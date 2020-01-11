15d53a4997790719c71a5c1dea8e08649a1e4de5
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-01-11 19:45:19 +0800

    feat: support document js polyfill
    Former-commit-id: f660945411846c2369c3b85857cb7dca2797731b [formerly f2ae9ec77089cc51d4f90a3d2728b89705dd0059]
    Former-commit-id: 6deb2390c57d53d4b9d40dcfc6db8dd6d124de7f

---
+++ bridge/polyfill/src/document/kraken.ts
@@ -0,0 +1,21 @@
+declare var __kraken_js_to_dart__: (data: string) => void;
+
+export function krakenCreateElement(type: string, id: number, props: any, events: string[]) {
+  return __kraken_js_to_dart__(`["createElement",[{"type":"${type.toUpperCase()}","id":${id},"props":${JSON.stringify(props)},"events":${JSON.stringify(events)}}]]`);
+}
+
+export function krakenCreateTextNode(id: number, nodeType: number, data: string) {
+  return __kraken_js_to_dart__(`["createTextNode",[{"id":${id},"nodeType":${nodeType},"data":"${data}"}]]`);
+}
+
+export function krakenInsertAdjacentNode(parentNodeId: number, position: string, nodeId: number) {
+  return __kraken_js_to_dart__(`["insertAdjacentNode",[${parentNodeId},"${position}",${nodeId}]]`);
+}
+
+export function krakenRemoveNode(id: number) {
+  return __kraken_js_to_dart__(`["removeNode",[${id}]]`);
+}
+
+export function krakenSetProperty(id: number, key: string, value: string) {
+  return __kraken_js_to_dart__(`["setProperty",[${id},"${key}",${value}]]`);
+}
