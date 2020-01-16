d9f4aec1c15e8e3a7049eefa19dfbed5629661ce
卓凌 <zhuoling.lcl@alibaba-inc.com>
2020-01-16 17:31:17 +0800

    feat: support touch event
    Former-commit-id: c2ec865210f3685a0a6ba3323ee738d895625f95 [formerly 0f57b9cd1c60619a02c4e8b5cfa47ca82cb4dcdb]
    Former-commit-id: 001676052c073811497473ac544ee91704b3d58c

--- dartfm/lib/src/element/event.dart
+++ dartfm/lib/src/element/event.dart
@@ -78,3 +78,112 @@ class InputEvent extends Event {
   }) : super('input',
             EventInit(bubbles: false, cancelable: true, composed: true));
 }
+
+/// reference: https://w3c.github.io/touch-events/#touchevent-interface
+class TouchEvent extends Event {
+  TouchEvent(String type) : super(type,
+      EventInit(bubbles: true, cancelable: true, composed: true ));
+
+  TouchList touches = TouchList();
+  TouchList targetTouches = TouchList();
+  TouchList changedTouches = TouchList();
+
+  bool altKey = false;
+  bool metaKey = false;
+  bool ctrlKey = false;
+  bool shiftKey = false;
+
+  Map toJson() {
+    Map eventMap = super.toJson();
+
+    eventMap['touches'] = touches.toJson();
+    eventMap['targetTouches'] = touches.toJson();
+    eventMap['changedTouches'] = touches.toJson();
+    eventMap['altKey'] = altKey;
+    eventMap['metaKey'] = metaKey;
+    eventMap['ctrlKey'] = ctrlKey;
+    eventMap['shiftKey'] = shiftKey;
+
+    return eventMap;
+  }
+}
+
+enum TouchType {
+  direct,
+  stylus,
+}
+
+/// reference: https://w3c.github.io/touch-events/#dom-touch
+class Touch {
+  final int identifier;
+  final EventTarget target;
+  final double clientX;
+  final double clientY;
+  final double screenX;
+  final double screenY;
+  final double pageX;
+  final double pageY;
+  final double radiusX;
+  final double radiusY;
+  final double rotationAngle;
+  final double force;
+  final double altitudeAngle;
+  final double azimuthAngle;
+  final TouchType touchType;
+
+  Touch({
+    this.identifier,
+    this.target,
+    this.clientX = 0,
+    this.clientY = 0,
+    this.screenX = 0,
+    this.screenY = 0,
+    this.pageX = 0,
+    this.pageY = 0,
+    this.radiusX = 0,
+    this.radiusY = 0,
+    this.rotationAngle = 0,
+    this.force = 0,
+    this.altitudeAngle = 0,
+    this.azimuthAngle = 0,
+    this.touchType = TouchType.direct,
+  });
+
+  Map toJson() {
+    return {
+      'identifier': identifier,
+//      'target': target, // target id?
+      'clientX': clientX,
+      'clientY': clientY,
+      'screenX': screenX,
+      'screenY': screenY,
+      'pageX': pageX,
+      'pageY': pageY,
+      'radiusX': radiusX,
+      'radiusY': radiusY,
+      'rotationAngle': rotationAngle,
+      'force': force,
+      'altitudeAngle': altitudeAngle,
+      'azimuthAngle': azimuthAngle,
+      'touchType': touchType == TouchType.direct ? 'direct' : 'stylus',
+    };
+  }
+}
+
+/// reference: https://w3c.github.io/touch-events/#touchlist-interface
+class TouchList {
+  List<Touch> items = [];
+  int get length => items.length;
+
+  Touch item(int index) {
+    return items[index];
+  }
+
+  List<Map> toJson() {
+    List<Map> ret = [];
+    for (Touch touch in items) {
+      ret.add(touch.toJson());
+    }
+    return ret;
+  }
+}
