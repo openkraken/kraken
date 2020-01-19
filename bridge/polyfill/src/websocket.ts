c02def8f638b9d8e9e82b5769780edf7e648b04d
卓凌 <zhuoling.lcl@alibaba-inc.com>
2020-01-19 16:31:08 +0800

    feat: polyfill websocket event handlder
    Former-commit-id: 00b8ff0351a65545eabe4dc1789c7dc55ac662ae [formerly d3a655f677e4f605664835bb7e50847c47848488]
    Former-commit-id: a5d664a867129fd57031a23b71b9aa13d07e03c6

--- bridge/polyfill/src/websocket.ts
+++ bridge/polyfill/src/websocket.ts
@@ -59,10 +59,6 @@ class WebSocket extends EventTarget {
   public extensions: string = ''; // TODO add extensions support
   public protocol: string = ''; // TODO add protocol support
   public binaryType: BinaryType = BinaryType.blob;
-  public onopen: OnOpenFunc = notImpl('onopen');
-  public onclose: OnCloseFunc = notImpl('onclose');
-  public onerror: OnErrorFunc = notImpl('onerror');
-  public onmessage: OnMessageFunc = notImpl('onmessage');
 
   private _onMessage = (message: string) => {
     this.dispatchEvent({
@@ -118,6 +114,60 @@ class WebSocket extends EventTarget {
     });
     this.readyState = ReadyState.CLOSED;
   };
+
+  public _onopen: any = null;
+  public _onmessage: any = null
+  public _onclose: any = null
+  public _onerror: any = null
+
+  public set onopen(messageHandler: any) {
+    if (this._onopen) {
+      this.removeEventListener('open', this._onopen);
+    }
+    this._onopen = messageHandler;
+    this.addEventListener('open', messageHandler);
+  }
+
+  public get onopen() {
+    return this._onopen;
+  }
+
+  public set onclose(messageHandler: any) {
+    if (this._onclose) {
+      this.removeEventListener('close', this._onclose);
+    }
+    this._onclose = messageHandler;
+    this.addEventListener('close', messageHandler);
+  }
+
+  public get onclose() {
+    return this._onclose;
+  }
+
+
+  public set onmessage(messageHandler: any) {
+    if (this._onmessage) {
+      this.removeEventListener('message', this._onmessage);
+    }
+    this._onmessage = messageHandler;
+    this.addEventListener('message', messageHandler);
+  }
+
+  public get onmessage() {
+    return this._onmessage;
+  }
+
+  public set onerror(messageHandler: any) {
+    if (this._onerror) {
+      this.removeEventListener('error', this._onerror);
+    }
+    this._onerror = messageHandler;
+    this.addEventListener('error', messageHandler);
+  }
+
+  public get onerror() {
+    return this._onerror;
+  }
 
   constructor(url: string, protocol: string | string[]) {
     super();
