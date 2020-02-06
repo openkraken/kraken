fee4b542635c8348c5027d250c89f3b87515c6ca
元彦 <yuanyan.cao@alibaba-inc.com>
2020-02-06 23:44:58 +0800

    chore: rework files
    Former-commit-id: f802f003bac085140f1bc2aaf6b253827337e498 [formerly cbca9b2f5134729391a6c4089cfcca42d317d48b]
    Former-commit-id: 8633ecc2899a4347aaabac3efb7372783b3b1c1d

---
+++ bridge/polyfill/src/document/bridge.ts
@@ -0,0 +1,42 @@
+import { krakenJSToDart } from '../kraken';
+
+const tickMessageQueue:string[] = [];
+
+export function frameTick() {
+  if (tickMessageQueue.length > 0) {
+    krakenJSToDart('["batchUpdate",[' + tickMessageQueue.join(',') + ']]');
+    tickMessageQueue.length = 0;
+  }
+}
+
+export function createElement(type: string, id: number, props: any, events: any) {
+  tickMessageQueue.push(`["createElement", [{"id":${id},"type":"${type}","props":${JSON.stringify(props)},"events":${JSON.stringify(events)}}]]`);
+}
+
+export function createTextNode(id: number, nodeType: number, data: string) {
+  tickMessageQueue.push(`["createTextNode",[{"id":${id},"nodeType":${nodeType},"data":"${data}"}]]`);
+}
+
+export function insertAdjacentNode(parentNodeId: number, position: string, nodeId: number) {
+  tickMessageQueue.push(`["insertAdjacentNode",[${parentNodeId},"${position}",${nodeId}]]`);
+}
+
+export function removeNode(id: number) {
+  tickMessageQueue.push(`["removeNode",[${id}]]`);
+}
+
+export function setProperty(id: number, key: string, value: any) {
+  tickMessageQueue.push(`["setProperty",[${id},"${key}","${value}"]]`);
+}
+
+export function setStyle(id: number, key: string, value: string) {
+  tickMessageQueue.push(`["setStyle",[${id},"${key}","${value}"]]`);
+}
+
+export function addEvent(id: number, eventName: string) {
+  tickMessageQueue.push(`["addEvent",[${id},"${eventName}"]]`);
+}
+
+export function removeEvent(id: number, eventName: string) {
+  tickMessageQueue.push(`["removeEvent",[${id},"${eventName}"]]`);
+}
