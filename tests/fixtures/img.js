894c9ea482aca73c20a3e9bcfb3be0d2648b0521
卓凌 <zhuoling.lcl@alibaba-inc.com>
2020-02-07 15:53:21 +0800

    fix: img tag support empty src
    Former-commit-id: dd40898127512cef30248210767ed6d00a43bf84 [formerly c4bf33c6b4936b2d1c3a1ed195bcbecf957deef9]
    Former-commit-id: 0a44ed2432f6f45ccbbb7194c6b6c8083dbe1b2b

---
+++ tests/fixtures/img.js
@@ -0,0 +1,5 @@
+var img = document.createElement('img');
+document.body.appendChild(img);
+
+img.style.width = '60px';
+img.setAttribute('src', '//gw.alicdn.com/tfs/TB1MRC_cvb2gK0jSZK9XXaEgFXa-1701-1535.png');
