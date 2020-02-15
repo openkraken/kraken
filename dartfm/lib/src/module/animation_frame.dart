517523773279ed2132b4a692483f3d078114cd7c
元彦 <yuanyan.cao@alibaba-inc.com>
2020-02-15 18:38:33 +0800

    fix: add file
    Former-commit-id: 47984fd6f1988f2615931e3e63d61d03ba001c4b [formerly 1355fe24d5a15025896799cdb1656299608e8a0a]
    Former-commit-id: 98b510e07c64cb3c7040793a13bbbd7328607eb0

---
+++ dartfm/lib/src/module/animation_frame.dart
@@ -0,0 +1,22 @@
+import 'package:kraken/element.dart';
+
+int _frameId = 1;
+Map<int, bool> _animationFrameCallbackValidateMap = {};
+
+int requestAnimationFrame(Function callback) {
+  int id = _frameId++;
+  _animationFrameCallbackValidateMap[id] = true;
+  ElementsBinding.instance.scheduleFrameCallback((Duration timeStamp) {
+    if (_animationFrameCallbackValidateMap[id] == true) {
+      callback();
+    }
+  });
+  ElementsBinding.instance.scheduleFrame();
+  return id;
+}
+
+void cancelAnimationFrame(int frameId) {
+  if (_animationFrameCallbackValidateMap.containsKey(frameId)) {
+    _animationFrameCallbackValidateMap[frameId] = false;
+  }
+}
