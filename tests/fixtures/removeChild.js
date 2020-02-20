490515aefa702c39f933afec0c8ae11b8e40b139
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2020-02-20 22:31:03 +0800

    feat: add removeChild in renderObject tree & add test
    Former-commit-id: 7fc7b0affdacd00b10ca7d3af2cffa9649aef911 [formerly 5898ba7568c87abe7de89eb0b341f529bfeb1549]
    Former-commit-id: 915e2e8e012f6bbfc9ecbc9d41892ebe514ff5bf

---
+++ tests/fixtures/removeChild.js
@@ -0,0 +1,23 @@
+function setStyle(dom, object) {
+  for (let key in object) {
+    if (object.hasOwnProperty(key)) {
+      dom.style[key] = object[key];
+    }
+  }
+}
+
+const block1 = document.createElement('p');
+block1.appendChild(document.createTextNode('text 1'));
+setStyle(block1, {
+  backgroundColor: '#999', textAlign: "center",
+});
+
+const block2 = document.createElement('div');
+block2.appendChild(document.createTextNode('text 2'));
+setStyle(block2, {
+  backgroundColor: '#f40', textAlign: "center",
+});
+
+document.body.appendChild(block1);
+document.body.appendChild(block2);
+document.body.removeChild(block1);
