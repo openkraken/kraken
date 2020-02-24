e7f5644fc8becb0c586404d43e59be6f8d0ffa5c
lzl <lei.lzl@alibaba-inc.com>
2020-02-24 22:56:02 +0800

    chore: add comment for boderMargin
    Former-commit-id: 6e0104cf8d7522003c0304928e495403d42c6c3a [formerly b4f3c4b50a89315a012850c8c419f6d1067c3326]
    Former-commit-id: 716343a6d0e98f34bef6fa644ce7738996127a1c

--- kraken/lib/src/style/box_decoration.dart
+++ kraken/lib/src/style/box_decoration.dart
@@ -27,6 +27,10 @@ mixin RenderDecoratedBoxMixin on BackgroundImageMixin {
     if (element != null) {
       element.cropBorderWidth = (margin.left ?? 0) + (margin.right ?? 0);
     }
+    /// cause flutter Border width is inside the element
+    /// but w3c border is outside the element
+    /// so use margin to fix it
+
     renderBorderMargin = RenderMargin(
         margin: margin,
         child: renderObject
