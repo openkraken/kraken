a79f2245b555346321713f936cca4491f9a1ff3a
卓凌 <zhuoling.lcl@alibaba-inc.com>
2020-03-05 17:31:16 +0800

    fix: canvas test
    Former-commit-id: c4dbdd11071c3505fca25cfa5cc3df7b042e5979 [formerly 88a82a0bedd96421847647eab2c926319e9f2adc]
    Former-commit-id: 672c5242e9ed6d42f59286f9e9ac24e6cb68a957

--- tests/integration/fixtures/canvas.js
+++ tests/integration/fixtures/canvas.js
@@ -1,6 +1,6 @@
 var div = document.createElement('div');
 div.style.width = div.style.height = '300px';
-div.style.backgroundColro = '#eee';
+div.style.backgroundColor = '#eee';
 
 var canvas = document.createElement('canvas');
 canvas.style.width = canvas.style.height = '200px';
@@ -9,12 +9,12 @@ canvas.style.width = canvas.style.height = '200px';
 requestAnimationFrame(() => {
   var context = canvas.getContext('2d');
 
-  context.fillStyle = WebColor.green;
+  context.fillStyle = 'green';
   context.fillRect(10, 10, 50, 50);
   context.clearRect(15, 15, 30, 30);
-  context.strokeStyle = WebColor.red;
+  context.strokeStyle = 'red';
   context.strokeRect(40, 40, 100, 100);
-  context.fillStyle = WebColor.blue;
+  context.fillStyle = 'blue';
   context.fillText('Hello World', 5.0, 5.0);
   context.strokeText('Hello World', 5.0, 25.0);
 });
