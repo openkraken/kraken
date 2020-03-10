d7913e6e9d2a764a157e3cbd9a7b1e4e5b03a41d
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-03-10 20:39:40 +0800

    chore: merge setTimeout, setInterval into timer.js
    Former-commit-id: 3d07fbb7aee24df11a5a1878949ad7dda2f9b5ac [formerly f58af4baaa89899e781739e65129fcfaa592d25f]
    Former-commit-id: 945cc87ae6e13217800928f26e3008c2f8f1c11d

---
+++ tests/unit/js_api/src/timer.js
@@ -0,0 +1,77 @@
+describe('setTimeout', function () {
+  it('resolve after 1 seconds', () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 100);
+    });
+  });
+
+  it('stop before resolved', () => {
+    return new Promise((resolve, reject) => {
+      let timer = setTimeout(() => {
+        reject();
+      }, 100);
+
+      setTimeout(() => {
+        resolve();
+      }, 120);
+
+      setTimeout(() => {
+        clearTimeout(timer);
+      }, 50);
+    });
+  });
+
+  it('first param not function will throw', () => {
+    try {
+      setTimeout(null);
+      throw new Error('setTimeout: parameter 1 is not an function should to throw');
+    } catch (e) {
+    }
+  });
+
+  it('second param not number will throw', () => {
+    try {
+      setTimeout(() => {
+      }, []);
+      throw new Error('setTimeout: parameter 2 is not an number should be throw');
+    } catch (e) {
+    }
+  });
+});
+
+describe('setInterval', function () {
+  it('trigger 5 times and stop', () => {
+    return new Promise((resolve, reject) => {
+      let count = 0;
+      let timer = setInterval(() => {
+        count++;
+        if (count > 5) {
+          clearTimeout(timer);
+          resolve();
+        }
+      }, 10);
+      setTimeout(() => {
+        reject('setInterval execute time out!');
+      }, 100);
+    });
+  });
+
+  it('first param not function will throw', () => {
+    try {
+      setInterval(null);
+      throw new Error('setInterval: parameter 1 is not an function should to throw');
+    } catch (e) {
+    }
+  });
+
+  it('second param not number will throw', () => {
+    try {
+      setInterval(() => {
+      }, []);
+      throw new Error('setInterval: parameter 2 is not an number should be throw');
+    } catch (e) {
+    }
+  });
+});
