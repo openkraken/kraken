3dd09525d4c96a0ff822262705d7113ad3b1e419
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-03-10 21:59:12 +0800

    feat: use kraken_test as entry and add js error listener
    Former-commit-id: 11251b8eefdf3c4e5e56ac696a962fc83fa613f8 [formerly 7af52a9125e8dd65415deb3a2367f8c6a435302c]
    Former-commit-id: 00e6850434f925419407cfc023155a60aab9a8e5

--- tests/integration/app.dart
+++ tests/integration/app.dart
@@ -3,14 +3,26 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:typed_data';
 import 'package:flutter/rendering.dart';
+import 'package:colorize/colorize.dart';
 import 'package:flutter/foundation.dart' show debugDefaultTargetPlatformOverride, TargetPlatform;
 import 'package:kraken/element.dart';
 import 'package:kraken/kraken.dart';
 import 'package:kraken/style.dart';
+import 'package:kraken/src/bridge/from_native.dart';
 import 'package:flutter_driver/driver_extension.dart';
+import '../bridge/from_native.dart';
+import '../bridge/to_native.dart';
 
 void main() {
-  initBridge();
+  initTestFramework();
+  registerDartTestMethodsToCpp();
+  registerDartMethodsToCpp();
+  addJSErrorListener((String errmsg) {
+    Colorize color = Colorize(errmsg);
+    color.red();
+    print(color);
+  });
+
   if (Platform.isMacOS) debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
 
   // Set render font family AlibabaPuHuiTi to resolve rendering difference.
@@ -29,10 +41,7 @@ void main() {
         shouldInitializeBinding: false,
         enableDebug: true,
         afterConnected: () {
-          evaluateScripts(payload, caseName, 0);
-          // Force wait to execute async ops.
-          sleep(const Duration(microseconds: 200));
-
+          evaluateTestScripts(payload, url: caseName);
           RendererBinding.instance.addPostFrameCallback((Duration timeout) async {
             BodyElement body = ElementManager().getRootElement();
             Uint8List bodyImage = await body.toBlob(devicePixelRatio: 1.0);
