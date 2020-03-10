f6318b9a8069fa3df726e145f9edcbc46ceca756
卓凌 <zhuoling.lcl@alibaba-inc.com>
2020-03-10 20:14:31 +0800

    chore: remove ignore files
    Former-commit-id: 9f02ec2f8140578c7d7ccc89d1e2c79ca76125d0 [formerly 6b6910e9aba2be288334d200366f13ed1358c527]
    Former-commit-id: ab1017377b7a453f8b9f54d46fddb49b7e726ee1

--- plugins/geolocation/macos/Flutter/ephemeral/flutter_export_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/zhuoling/workspace/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/zhuoling/workspace/kraken_workspace/kraken/plugins/geolocation"
-export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_FRAMEWORK_DIR=/Users/zhuoling/workspace/flutter/bin/cache/artifacts/engine/darwin-x64"
-export "FLUTTER_BUILD_NAME=2.5.3"
-export "FLUTTER_BUILD_NUMBER=2.5.3"
