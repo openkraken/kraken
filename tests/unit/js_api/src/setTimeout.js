93c65c4402d8b3dbe2e0cfe4773e980f287ebdb5
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-03-10 19:44:13 +0800

    test: add setTimeout unit test cases
    Former-commit-id: 06a3625c4473cd6c17665649c4781ea838bf5bb6 [formerly 0fd5d3acb3766ec4b3139360727a4a926fa035f8]
    Former-commit-id: 105ecfe21a517466a700bd555553e78d1e4f124e

--- tests/unit/js_api/src/setTimeout.js
+++ tests/unit/js_api/src/setTimeout.js
@@ -3,7 +3,37 @@ describe('setTimeout', function () {
     return new Promise((resolve) => {
       setTimeout(() => {
         resolve();
-      }, 1000);
+      }, 100);
     });
+  });
+
+  it('stop before resolved', () => {
+    return new Promise((resolve, reject) => {
+      let timer = setTimeout(() => {
+        reject();
+      }, 100);
+
+      setTimeout(() => {
+        resolve();
+      }, 120);
+
+      setTimeout(() => {
+        clearTimeout(timer);
+      }, 50);
+    });
+  });
+
+  it('first param not function will throw', () => {
+    try {
+      setTimeout(null);
+      throw new Error('setTimeout: parameter 1 is not an function should to throw');
+    } catch (e) {}
+  });
+
+  it('second param not number will throw', () => {
+    try {
+      setTimeout(() => {}, []);
+      throw new Error('setTimeout: parameter 2 is not an number should be throw');
+    } catch (e) {}
   });
 });
