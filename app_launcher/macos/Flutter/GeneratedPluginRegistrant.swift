dd6a118035d9218266af4d83a58b6a4f09a3ec11
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-03-10 16:28:30 +0800

    chore: rename geolocation to kraken_geolocation
    Former-commit-id: 7f579f220cd8a6f05cf322946eb3506766f1d8df [formerly 079a8585228a2220dfac464b02955fe3fde47209]
    Former-commit-id: 02c8186cf8dbdf81cafe7ffbdf6e580a3fbc71b1

--- app_launcher/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app_launcher/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,16 +7,16 @@ import Foundation
 
 import connectivity_macos
 import kraken_audioplayers
+import kraken_geolocation
 import kraken_video_player
-import location
 import path_provider_macos
 import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
-  FLTVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FLTVideoPlayerPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  FLTVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FLTVideoPlayerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
--- plugins/kraken_geolocation/ios/kraken_geolocation.podspec
+++ plugins/kraken_geolocation/ios/kraken_geolocation.podspec
@@ -2,7 +2,7 @@
 # To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html
 #
 Pod::Spec.new do |s|
-  s.name             = 'location'
+  s.name             = 'kraken_geolocation'
   s.version          = '0.0.1'
   s.summary          = 'A new flutter plugin project.'
   s.description      = <<-DESC
