724151a75fe985a729af4caf62ccc22e242afabe
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2020-03-13 21:55:08 +0800

    fix: tests cases
    Former-commit-id: 5860ffeadbdb7a851303a50915071446fab1aed4 [formerly d1163ddb874785b9c4e4e70d31aa61fb41d587e7]
    Former-commit-id: fac26754f76c96a4eedbeafa02b7717a13a6c707

--- tests/integration/fixtures/position_relative.js
+++ tests/integration/fixtures/position_relative.js
@@ -1,31 +1,33 @@
-function setStyle(dom, object) {
-  for (let key in object) {
-    if (object.hasOwnProperty(key)) {
-      dom.style[key] = object[key];
+it('position_relative', () => {
+  function setStyle(dom, object) {
+    for (let key in object) {
+      if (object.hasOwnProperty(key)) {
+        dom.style[key] = object[key];
+      }
     }
   }
-}
 
-const div1 = document.createElement('div');
-setStyle(div1, {
-  width: '100px',
-  height: '100px',
-  backgroundColor: '#666',
-  position: 'relative',
-  top: '50px',
-  left: '50px',
-});
-div1.appendChild(document.createTextNode('relative top & left'));
-document.body.appendChild(div1);
+  const div1 = document.createElement('div');
+  setStyle(div1, {
+    width: '100px',
+    height: '100px',
+    backgroundColor: '#666',
+    position: 'relative',
+    top: '50px',
+    left: '50px',
+  });
+  div1.appendChild(document.createTextNode('relative top & left'));
+  document.body.appendChild(div1);
 
-const div2 = document.createElement('div');
-setStyle(div2, {
-  width: '100px',
-  height: '100px',
-  backgroundColor: '#999',
-  position: 'relative',
-  bottom: '-50px',
-  right: '-50px',
+  const div2 = document.createElement('div');
+  setStyle(div2, {
+    width: '100px',
+    height: '100px',
+    backgroundColor: '#999',
+    position: 'relative',
+    bottom: '-50px',
+    right: '-50px',
+  });
+  div2.appendChild(document.createTextNode('relative bottom & right'));
+  document.body.appendChild(div2);
 });
-div2.appendChild(document.createTextNode('relative bottom & right'));
-document.body.appendChild(div2);
--- tests/integration/fixtures/position_static.js
+++ tests/integration/fixtures/position_static.js
@@ -1,20 +1,22 @@
-function setStyle(dom, object) {
-  for (let key in object) {
-    if (object.hasOwnProperty(key)) {
-      dom.style[key] = object[key];
+it('position_static', () => {
+  function setStyle(dom, object) {
+    for (let key in object) {
+      if (object.hasOwnProperty(key)) {
+        dom.style[key] = object[key];
+      }
     }
   }
-}
 
-const div1 = document.createElement('div');
-setStyle(div1, {
-  width: '100px',
-  height: '100px',
-  backgroundColor: '#666',
-  position: 'static',
-  top: '100px',
-  left: '100px',
+  const div1 = document.createElement('div');
+  setStyle(div1, {
+    width: '100px',
+    height: '100px',
+    backgroundColor: '#666',
+    position: 'static',
+    top: '100px',
+    left: '100px',
+  });
+  div1.appendChild(document.createTextNode('static element'));
+
+  document.body.appendChild(div1);
 });
-div1.appendChild(document.createTextNode('static element'));
-
-document.body.appendChild(div1);
