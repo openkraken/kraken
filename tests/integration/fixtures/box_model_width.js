35d6d217da6f5add97fdd73d3373b6440c6691da
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2020-03-13 15:12:45 +0800

    chore: update tests syntax
    Former-commit-id: d85ed287c9417061875174f096c1900b9c22ae8a [formerly dbd14a2eebf0403e5434db7f75545952c61db509]
    Former-commit-id: d0dd4dae07fa2834170acc2ebda152950689f23c

--- tests/integration/fixtures/box_model_width.js
+++ tests/integration/fixtures/box_model_width.js
@@ -1,20 +1,22 @@
-function setStyle(dom, object) {
-  for (let key in object) {
-    if (object.hasOwnProperty(key)) {
-      dom.style[key] = object[key];
+it('box_model_width', () => {
+  function setStyle(dom, object) {
+    for (let key in object) {
+      if (object.hasOwnProperty(key)) {
+        dom.style[key] = object[key];
+      }
     }
   }
-}
 
-const div = document.createElement('div');
-setStyle(div, {
-  width: '100px',
-  height: '100px',
-  backgroundColor: '#666',
-});
+  const div = document.createElement('div');
+  setStyle(div, {
+    width: '100px',
+    height: '100px',
+    backgroundColor: '#666',
+  });
 
-document.body.appendChild(div);
+  document.body.appendChild(div);
 
-requestAnimationFrame(() => {
-  div.style.width = '200px';
+  requestAnimationFrame(() => {
+    div.style.width = '200px';
+  });
 });
