38e621b4cd8cf384d5eb6bc49dd2c3b72c0ea87a
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-03-15 08:50:07 +0800

    refactor: remove legacy test API
    Former-commit-id: 8652fc3daa19fc9695dea50359564ab975cb17df [formerly 1514ff42d034376b9671e1f3d9002a27116fa7cc]
    Former-commit-id: 140ec0e83dd887d438cbf6f0e25709141297a8c1

--- bridge/include/bridge_test_export.h
+++ bridge/include/bridge_test_export.h
@@ -10,18 +10,6 @@
 #include <cstdint>
 #define KRAKEN_EXPORT extern "C" __attribute__((visibility("default"))) __attribute__((used))
 
-using TestCallback = void (*)(void *);
-typedef void *(*Describe)(const char *, void *context, TestCallback callback);
-
-using ItCallback = void (*)(void*, int32_t);
-typedef void *(*It)(const char *, void *context, ItCallback callback);
-typedef void *(*ItDone)(int32_t, const char*);
-
-typedef void *(*BeforeEach)(void *context, TestCallback callback);
-typedef void *(*BeforeAll)(void *context, TestCallback callback);
-typedef void *(*AfterEach)(void *context, TestCallback callback);
-typedef void *(*AfterAll)(void *context, TestCallback callback);
-
 KRAKEN_EXPORT
 void initTestFramework();
 KRAKEN_EXPORT
