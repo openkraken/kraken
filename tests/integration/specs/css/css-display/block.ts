90c865cef5f3857ac5993eff69426dd41a5fe65d
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2020-03-17 15:20:54 +0800

    feat: add css-display tests
    Former-commit-id: 8e3c755e13e8de7cbd935c1cf87afe3700f2e6d0 [formerly cf00afc0cfde0d711e780732f88e89538bb4bf73]
    Former-commit-id: 81c40b7f6fc5fbf340e580c15cbbf70b50ea4334

---
+++ tests/integration/specs/css/css-display/block.ts
@@ -0,0 +1,18 @@
+describe('Display block', () => {
+  it('should work with basic samples', async () => {
+
+    const container = document.createElement('div');
+    setStyle(container, {
+      width: '100px',
+      height: '100px',
+      display: 'block',
+      backgroundColor: '#666',
+    });
+    container.appendChild(document.createTextNode('block'));
+
+    document.body.appendChild(container);
+    document.body.appendChild(document.createTextNode('This text should wrap into next line from the box.'));
+
+    await expectAsync(document.body.toBlob(1)).toMatchImageSnapshot();
+  });
+});
