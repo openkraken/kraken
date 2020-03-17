5991b4471c1ffff756a79bcf636fa36e21429564
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-03-18 00:35:17 +0800

    test: rollback static to nostatic
    Former-commit-id: 8ea0e9b54874044ae2fce5bd4b6398e9059878d7 [formerly c745cdd18ec48ec03919230495e2d550ed128f9f]
    Former-commit-id: e42ba1fc578f3e7e30118a39e6649907dcc67184

--- tests/integration/specs/css/css-position/static-to-nostatic.ts
+++ tests/integration/specs/css/css-position/static-to-nostatic.ts
@@ -1,62 +1,62 @@
-// describe('Position static', () => {
-//   it('to non-static', async () => {
-//     const container = document.createElement('div');
-//     setStyle(container, {
-//       width: '400px',
-//       height: '400px',
-//       backgroundColor: '#999',
-//       position: 'relative',
-//     });
-//     document.body.appendChild(container);
+describe('Position static', () => {
+  it('to non-static', async () => {
+    const container = document.createElement('div');
+    setStyle(container, {
+      width: '400px',
+      height: '400px',
+      backgroundColor: '#999',
+      position: 'relative',
+    });
+    document.body.appendChild(container);
 
-//     const div1 = document.createElement('div');
-//     setStyle(div1, {
-//       width: '100px',
-//       height: '100px',
-//       backgroundColor: 'red',
-//       top: '200px',
-//       left: '100px',
-//     });
-//     div1.appendChild(document.createTextNode('absolute to static'));
-//     container.appendChild(div1);
+    const div1 = document.createElement('div');
+    setStyle(div1, {
+      width: '100px',
+      height: '100px',
+      backgroundColor: 'red',
+      top: '200px',
+      left: '100px',
+    });
+    div1.appendChild(document.createTextNode('absolute to static'));
+    container.appendChild(div1);
 
-//     const div2 = document.createElement('div');
-//     setStyle(div2, {
-//       width: '100px',
-//       height: '100px',
-//       backgroundColor: 'blue',
-//       top: '50px',
-//       left: '100px',
-//     });
-//     div2.appendChild(document.createTextNode('relative to static'));
-//     container.appendChild(div2);
+    const div2 = document.createElement('div');
+    setStyle(div2, {
+      width: '100px',
+      height: '100px',
+      backgroundColor: 'blue',
+      top: '50px',
+      left: '100px',
+    });
+    div2.appendChild(document.createTextNode('relative to static'));
+    container.appendChild(div2);
 
-//     const div3 = document.createElement('div');
-//     setStyle(div3, {
-//       width: '100px',
-//       height: '100px',
-//       backgroundColor: 'green',
-//       top: '200px',
-//       left: '200px',
-//     });
-//     div3.appendChild(document.createTextNode('fixed to static'));
-//     container.appendChild(div3);
+    const div3 = document.createElement('div');
+    setStyle(div3, {
+      width: '100px',
+      height: '100px',
+      backgroundColor: 'green',
+      top: '200px',
+      left: '200px',
+    });
+    div3.appendChild(document.createTextNode('fixed to static'));
+    container.appendChild(div3);
 
-//     const div4 = document.createElement('div');
-//     setStyle(div4, {
-//       width: '100px',
-//       height: '100px',
-//       backgroundColor: 'yellow',
-//       top: '50px',
-//     });
-//     div4.appendChild(document.createTextNode('sticky to static'));
-//     container.appendChild(div4);
+    const div4 = document.createElement('div');
+    setStyle(div4, {
+      width: '100px',
+      height: '100px',
+      backgroundColor: 'yellow',
+      top: '50px',
+    });
+    div4.appendChild(document.createTextNode('sticky to static'));
+    container.appendChild(div4);
 
-//     div1.style.position = 'absolute';
-//     div2.style.position = 'relative';
-//     div3.style.position = 'fixed';
-//     div4.style.position = 'sticky';
+    div1.style.position = 'absolute';
+    div2.style.position = 'relative';
+    div3.style.position = 'fixed';
+    div4.style.position = 'sticky';
 
-//     await matchScreenshot();
-//   });
-// });
+    await matchScreenshot();
+  });
+});
