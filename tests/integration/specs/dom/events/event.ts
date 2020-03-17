54bd50572b4e4724c8b3f58d724b43341990b2e9
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2020-03-17 17:58:26 +0800

    test: add event test
    Former-commit-id: 86bfb3bde853df5e56509985fd2279ba88cc3471 [formerly e750f77ea662a14484a55b5078e01920efef5939]
    Former-commit-id: 01dd58774665afa58838500ec921d55d93c30368

---
+++ tests/integration/specs/dom/events/event.ts
@@ -0,0 +1,41 @@
+describe('Event trigger order', () => {
+  it('should work', async () => {
+    return new Promise(async (resolve, reject) => {
+      const container1 = document.createElement('div');
+      document.body.appendChild(container1);
+      setStyle(container1, {
+        padding: '20px', backgroundColor: '#999', margin: '40px'
+      });
+      container1.appendChild(document.createTextNode('DIV 1'));
+
+      const container2 = document.createElement('div');
+      setStyle(container2, {
+        padding: '20px', height: '100px', backgroundColor: '#f40', margin: '40px'
+      });
+      container2.appendChild(document.createTextNode('DIV 2'));
+
+      container1.appendChild(container2);
+
+      document.body.addEventListener('click', () => {
+        wrapper.appendChild(document.createTextNode('BODY clicked, '));
+      });
+      container1.addEventListener('click', () => {
+        wrapper.appendChild(document.createTextNode('DIV 1 clicked, '));
+      });
+      container2.addEventListener('click', () => {
+        wrapper.appendChild(document.createTextNode('DIV 2 clicked, '));
+      });
+
+      const wrapper = document.createElement('div');
+      document.body.appendChild(wrapper);
+      wrapper.appendChild(document.createTextNode('Click DIV 2: '));
+
+      requestAnimationFrame(() => {
+        container2.click();
+        await expectAsync(div.toBlob(1.0)).toMatchImageSnapshot();
+        resolve();
+      });
+
+    });
+  });
+});
--- tests/integration/specs/dom/nodes/event_target.ts
+++ tests/integration/specs/dom/nodes/event_target.ts
@@ -6,30 +6,33 @@
  */
 describe('DOM EventTarget', () => {
   it('should work', async () => {
-    let clickTime = 0;
-    const div = document.createElement('div');
-    div.appendChild(document.createTextNode('Click: ' + clickTime + 'times.'));
+    return new Promise(async (resolve, reject) => {
+      let clickTime = 0;
+      const div = document.createElement('div');
+      div.appendChild(document.createTextNode('Click: ' + clickTime + 'times.'));
 
-    const clickHandler = () => {
-      clickTime++;
-      div.replaceChild(
-        document.createTextNode('Click: ' + clickTime + 'times.'),
-        div.firstChild
-      );
-    };
-    div.addEventListener('click', clickHandler);
+      const clickHandler = () => {
+        clickTime++;
+        div.replaceChild(
+          document.createTextNode('Click: ' + clickTime + 'times.'),
+          div.firstChild
+        );
+      };
+      div.addEventListener('click', clickHandler);
 
-    document.body.appendChild(div);
-    div.click();
-    div.click();
+      document.body.appendChild(div);
+      div.click();
+      div.click();
 
-    div.removeEventListener('click', clickHandler);
-    div.click(); // Should be `2`.
+      div.removeEventListener('click', clickHandler);
+      div.click(); // Should be `2`.
 
-    setTimeout(() => {
-      expect(clickTime === 2).toBe(true);
-      await matchScreenshot();
-    }, 100);
+      setTimeout(() => {
+        expect(clickTime === 2).toBe(true);
+        await matchScreenshot();
+        resolve();
+      }, 100);
 
+    });
   });
 });
