5cbf781fcf76f6455242bb0148f3e232f87a6be8
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2020-03-17 17:49:37 +0800

    test: add dom tests
    Former-commit-id: f651b98c1130f1e49d485508ec3368e49b1e3f1d [formerly 0cf754fc736db4618dfc7e7061bfa647c52a6e5e]
    Former-commit-id: a06d58006e2f9e8923743d897d126025424d6207

---
+++ tests/integration/specs/dom/nodes/remove_child.ts
@@ -0,0 +1,21 @@
+describe('Document api', () => {
+  it('removeChild', async () => {
+    const block1 = document.createElement('p');
+    block1.appendChild(document.createTextNode('text 1'));
+    setStyle(block1, {
+      backgroundColor: '#999', textAlign: "center",
+    });
+
+    const block2 = document.createElement('div');
+    block2.appendChild(document.createTextNode('text 2'));
+    setStyle(block2, {
+      backgroundColor: '#f40', textAlign: "center",
+    });
+
+    document.body.appendChild(block1);
+    document.body.appendChild(block2);
+    document.body.removeChild(block1);
+
+    await matchScreenshot();
+  });
+});
