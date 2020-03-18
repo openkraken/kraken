50830240b4ce49f2dd6e7f346a44a333b274c552
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2020-03-18 20:31:04 +0800

    feat: add more css-display  tests
    Former-commit-id: 7c0d383704e0f7e1e8054e27833908ff019f9471 [formerly cf428867f4708883920818971f0796c4eac31164]
    Former-commit-id: e15069370bc84356d01563f9ade27ca4a21952c9

---
+++ tests/integration/specs/css/css-display/block-in-inline.ts
@@ -0,0 +1,118 @@
+describe('Display block in inline', () => {
+  it('simple', async () => {
+    var div1 = document.createElement('div');
+    setStyle(div1, {
+      color: 'green',
+      display: 'block',
+    });
+    var div2 = document.createElement('div');
+    setStyle(div2, {
+      background: 'red',
+      color: 'red',
+      display: 'inline',
+    });
+    var div3 = document.createElement('div');
+    setStyle(div3, {
+      color: 'green',
+      display: 'block',
+    });
+
+    document.body.appendChild(div1);
+    div1.appendChild(div2);
+    div2.appendChild(div3);
+    div3.appendChild(document.createTextNode('There should be no red.'))
+
+    await matchScreenshot();
+  });
+
+  it('style changes 001', async () => {
+    var div1 = document.createElement('div');
+    setStyle(div1, {
+      display: 'block',
+    });
+    var div2 = document.createElement('div');
+    setStyle(div2, {
+      display: 'inline',
+    });
+    div2.appendChild(document.createTextNode('This text should all collapse'));
+    var div3 = document.createElement('div');
+    setStyle(div3, {
+      display: 'block',
+    });
+    div3.appendChild(document.createTextNode(' into one line of text when '));
+    div2.appendChild(div3);
+    div2.appendChild(document.createTextNode('you click on the text.'));
+
+    document.body.appendChild(div1);
+    div1.appendChild(div2);
+
+    div1.addEventListener('click', () => {
+      div3.style.display = div3.style.display == 'inline' ? 'block' : 'inline';
+    });
+    await matchScreenshot();
+  });
+
+  it('style changes 002', async () => {
+    var div1 = document.createElement('div');
+    setStyle(div1, {
+      display: 'block',
+    });
+    var div2 = document.createElement('div');
+    setStyle(div2, {
+      display: 'inline',
+    });
+    div2.appendChild(document.createTextNode('This text should all split into'));
+    var div3 = document.createElement('div');
+    setStyle(div3, {
+      display: 'inline',
+    });
+    div3.appendChild(document.createTextNode(' three separate lines when '));
+    div2.appendChild(div3);
+    div2.appendChild(document.createTextNode('you click on the text.'));
+
+    document.body.appendChild(div1);
+    div1.appendChild(div2);
+
+    div1.addEventListener('click', () => {
+      div2.style.display = div2.style.display == 'inline' ? 'block' : 'inline';
+      div3.style.display = div3.style.display == 'inline' ? 'block' : 'inline';
+    });
+    await matchScreenshot();
+  });
+
+  it('relative positioning', async () => {
+    var p = document.createElement('p');
+    document.body.appendChild(p);
+    p.appendChild(document.createTextNode('There should be no red.'));
+
+    var div1 = document.createElement('div');
+    setStyle(div1, {
+      backgroundColor: 'red',
+      height: '50px',
+      width: '50px',
+    });
+    document.body.appendChild(div1);
+
+    var div2 = document.createElement('div');
+    document.body.appendChild(div2);
+
+    var div3 = document.createElement('div');
+    setStyle(div3, {
+      display: 'inline',
+    });
+    div2.appendChild(div3);
+
+    var div4 = document.createElement('div');
+    setStyle(div4, {
+      display: 'block',
+      backgroundColor: 'green',
+      height: '50px',
+      width: '50px',
+      position: 'relative',
+      top: '-50px',
+    });
+    div3.appendChild(div4);
+
+    await matchScreenshot();
+  });
+});
