51ef584a7e393ccaa6204dd67a23badd8244aab0
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-03-19 19:06:44 +0800

    test: add background color and background repeat tests
    Former-commit-id: 713572595c5f1c20148979b00d995b5fc8ac990f [formerly 8ace8d94329c14e492e55ffd23852b28c7e5eea3]
    Former-commit-id: 027c0f01ce144267e144e7f749466f105a4fbdf5

--- tests/integration/specs/dom/tags/img.ts
+++ tests/integration/specs/dom/tags/img.ts
@@ -1,16 +1,13 @@
-describe('Tags img', async () => {
+describe('Tags img',  () => {
   it('basic', done => {
     const img = document.createElement('img');
-    img.addEventListener('load', () => {
+    img.addEventListener('load', async () => {
+      await matchScreenshot(img);
       done();
     });
     img.style.width = '60px';
     img.setAttribute('src', '//gw.alicdn.com/tfs/TB1MRC_cvb2gK0jSZK9XXaEgFXa-1701-1535.png');
 
     document.body.appendChild(img);
-    img.onload = async () => {
-      await matchScreenshot();
-      done();
-    };
   });
 });
