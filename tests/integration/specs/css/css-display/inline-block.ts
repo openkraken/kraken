b935e12f0fa533b5309bc1e97323fb1af1bce81f
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-03-19 13:37:56 +0800

    test: add inline-block test specs
    Former-commit-id: 63e7a5880d3e8e492b9a46cc1aca81733b8ba199 [formerly 3e5b347ed8cfdd893a18eb4e6c3a79d87a05b52c]
    Former-commit-id: 90f2b343b070430e263dec9bfdb9b5e1487e9b2f

--- tests/integration/specs/css/css-display/inline-block.ts
+++ tests/integration/specs/css/css-display/inline-block.ts
@@ -13,4 +13,56 @@ describe('Display inline-block', () => {
 
     await matchScreenshot();
   });
+
+  xit('inline-block box constraint is tight', async () => {
+    let magenta = create('div', {
+      border: '5px solid magenta',
+      display: 'inline-block',
+    });
+    // append(wrapper, magenta);
+    let box = create('div', {
+      border: '10px solid cyan',
+      padding: '15px',
+      margin: '20px 0px',
+      backgroundColor: 'yellow',
+      display: 'inline-flex',
+    });
+    append(magenta, box);
+    append(BODY, magenta);
+    await matchScreenshot(BODY);
+  });
+
+  xit('inline-block nest inline-block should behavior like inline-block', async () => {
+    let magenta = create('div', {
+      border: '5px solid magenta',
+      display: 'inline-block',
+    });
+    let box = create('div', {
+      border: '10px solid cyan',
+      padding: '15px',
+      margin: '20px 0px',
+      backgroundColor: 'yellow',
+      display: 'inline-block'
+    });
+    append(magenta, box);
+    append(BODY, magenta);
+    await matchScreenshot(magenta);
+  });
+
+  xit('inline-block nest block should behavior like inline-block', async () => {
+    let magenta = create('div', {
+      border: '5px solid magenta',
+      display: 'inline-block',
+    });
+    let box = create('div', {
+      border: '10px solid cyan',
+      padding: '15px',
+      margin: '20px 0px',
+      backgroundColor: 'yellow',
+      display: 'block'
+    });
+    append(magenta, box);
+    append(BODY, magenta);
+    await matchScreenshot(magenta);
+  });
 });
