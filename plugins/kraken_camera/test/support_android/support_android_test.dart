1069fd884b28c684853c797fcace28eb078ac43a
卓凌 <zhuoling.lcl@alibaba-inc.com>
2020-03-20 15:58:59 +0800

    chore: bump to pub
    Former-commit-id: 3b7a4f8aae5a28a27c311ceaf6acb9edd85cc2ea [formerly 096329d914a2416b0d5184e8c9e69fdc6106f506]
    Former-commit-id: 02316833f428140bcd4758c8940c2b439ac3338e

--- plugins/kraken_camera/test/support_android/support_android_test.dart
+++ plugins/kraken_camera/test/support_android/support_android_test.dart
@@ -2,11 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:camera/new/src/support_android/camera_info.dart';
-import 'package:camera/new/src/support_android/camera.dart';
+import 'package:kraken_camera/new/src/support_android/camera_info.dart';
+import 'package:kraken_camera/new/src/support_android/camera.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:camera/new/src/camera_testing.dart';
+import 'package:kraken_camera/new/src/camera_testing.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
