03f08f58c98aa395157b2e707ec195a040c998c6
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2020-03-24 22:23:47 +0800

    feat: window extend EventTarget
    Former-commit-id: 5f550ab54e6149a273c1cfb218af0c1c127d4fd3 [formerly 0d496ce6596ecec632f6bd94db02d0cd3bb9acec]
    Former-commit-id: 33d12281e41302094c695ee48461b9ef9941d111

--- bridge/polyfill/src/window.ts
+++ bridge/polyfill/src/window.ts
@@ -1,24 +1,54 @@
-import { krakenWindow } from './kraken';
+import { EventTarget } from 'event-target-shim';
+import { krakenWindow, KrakenLocation } from './kraken';
 
-Object.defineProperty(global, 'onload', {
-  enumerable: true,
-  configurable: false,
-  set(fn: any) {
-    krakenWindow.onLoad = fn;
-  },
-});
 
-Object.defineProperty(global, 'devicePixelRatio', {
-  enumerable: true,
-  configurable: false,
-  get() {
+function bindLegacyListeners(eventTarget: EventTarget, events: string[]) {
+  events.forEach((event: string) => {
+    Object.defineProperty(eventTarget, 'on' + event, {
+      enumerable: true,
+      configurable: false,
+      set(fn: any) {
+        if (this['_on' + event]) this.removeEventListener(event, this['_on' + event]);
+        this['_on' + event] = fn;
+        this.addEventListener(event, fn);
+      },
+      get() {
+        return this['_on' + event];
+      },
+    });
+  });
+}
+
+
+class Window extends EventTarget {
+
+  constructor() {
+    super();
+    bindLegacyListeners(this, ['load', 'colorschemechange']);
+    krakenWindow.onLoad = this.dispatchEvent;
+    krakenWindow.onColorSchemeChange = this.dispatchEvent;
+  }
+
+  public get colorScheme() : string {
+    return krakenWindow.colorScheme;
+  }
+
+
+  public get devicePixelRatio() : number {
     return krakenWindow.devicePixelRatio;
-  },
-});
+  }
+
+  public get location() : KrakenLocation {
+    return krakenWindow.location;
+  }
+}
+
+
+const window = new Window();
 
 Object.defineProperty(global, 'window', {
   enumerable: true,
   writable: false,
   configurable: false,
-  value: global,
+  value: window,
 });
