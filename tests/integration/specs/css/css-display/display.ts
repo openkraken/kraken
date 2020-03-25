c4d2b2f06b5be1f894a081ca8bffbda1d09488c0
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-03-25 20:37:25 +0800

    test: add display specs
    Former-commit-id: 100d19677c5171a4b5ec8c6a208487e60591e623 [formerly b442d015f7da26370d49b3cb93513e645cf5183d]
    Former-commit-id: 16753b4de013b700be192284b1fb808f5e7cc7b4

---
+++ tests/integration/specs/css/css-display/display.ts
@@ -0,0 +1,77 @@
+fdescribe('display', () => {
+  it('001', async () => {
+    let divStyle = {
+      display: 'inline'
+    };
+    let element = create('div', {}, [
+      create('div', divStyle, createText('Filter text')),
+      create('div', divStyle, createText('Filter text'))
+    ]);
+    append(BODY, element);
+    await matchScreenshot();
+  });
+
+  it('002', async () => {
+    let divStyle = {
+      display: 'block'
+    };
+    let element = create('div', {}, [
+      create('div', divStyle, createText('Filter text')),
+      create('div', divStyle, createText('Filter text')),
+    ]);
+    append(BODY, element);
+    await matchScreenshot();
+  });
+  it('005', async () => {
+    let divdivStyle = {
+      display: 'inline-block'
+    };
+    let element = create('div', {}, [
+      createText('Filter text'),
+      create('div', divdivStyle, createText('Filter text'))
+    ]);
+    append(BODY, element);
+    await matchScreenshot();
+  });
+  it('016', async () => {
+    let divStyle = {
+      color: 'red',
+      display: 'none'
+    };
+    let element = create('div', divStyle, createText('FAIL'));
+    append(BODY, element);
+    await matchScreenshot();
+  });
+  it('applies-to-001', async () => {
+    let spanStyle = {
+      display: 'inline'
+    };
+    let element = create('div', {}, [
+      createText('Filter text'),
+      create('span', spanStyle, createText('Filter Text')),
+      createText('Filter text')
+    ]);
+    append(BODY, element);
+    await matchScreenshot();
+  });
+  it('none-001', async () => {
+    let divStyle = {
+      backgroundColor: 'red',
+      display: 'none',
+      position: 'absolute'
+    };
+    let element = create('div', divStyle, createText('Filter Text'));
+    append(BODY, element);
+    await matchScreenshot();
+  });
+  it('none-002', async () => {
+    let divStyle = {
+      backgroundColor: 'red',
+      display: 'none',
+      position: 'fixed'
+    };
+    let element = create('div', divStyle, createText('Filter Text'));
+    append(BODY, element);
+    await matchScreenshot();
+  });
+});
