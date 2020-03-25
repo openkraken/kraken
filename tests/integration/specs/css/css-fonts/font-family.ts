d6f6fc3ef5b857adbc9d1b6192e1814d776ce7b5
卓凌 <zhuoling.lcl@alibaba-inc.com>
2020-03-25 21:56:20 +0800

    test(refactor): add test cases for font
    Former-commit-id: e5829ab86e61d01458f63f3d7cd7a577c385b66d [formerly 09194fd1e1150fab04b0f5cdd15be584e53f8224]
    Former-commit-id: a9d3e6d8d62dd48f37c579d3b880c944aa74f0dc

---
+++ tests/integration/specs/css/css-fonts/font-family.ts
@@ -0,0 +1,31 @@
+describe('FontFamily', () => {
+  it('should works in english', () => {
+    const p1 = create('p', {
+      fontFamily: 'SimSun',
+      fontSize: '32px',
+    }, createText('These two lines should use the same font.'));
+    const p2 = create('p', {
+      fontFamily: 'SimSun',
+      fontSize: '32px',
+    }, createText('These two lines should use the same font.'));
+    append(BODY, p1);
+    append(BODY, p2);
+
+    return matchScreenshot();
+  });
+
+  it('should works in chinese', () => {
+    const p1 = create('p', {
+      fontFamily: 'SimSun',
+      fontSize: '32px',
+    }, createText('字体文本测试。'));
+    const p2 = create('p', {
+      fontFamily: 'SimSun',
+      fontSize: '32px',
+    }, createText('字体文本测试。'));
+    append(BODY, p1);
+    append(BODY, p2);
+
+    return matchScreenshot();
+  });
+});
