345ceeca88246efd97031ad935adbd799644b9e0
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-04-02 17:59:03 +0800

    test: add method-channel.ts specs.
    Former-commit-id: 9d5ed31acbca2a5728fccbbe8284ecb507fd9059 [formerly 14156aa7d9405aaa7e64bdb93fe6f78c28e02204]
    Former-commit-id: 2723a49b77b0a8168eae476ad9489b84e0612edb

--- tests/macos/Runner/MainFlutterWindow.swift
+++ tests/macos/Runner/MainFlutterWindow.swift
@@ -1,5 +1,6 @@
 import Cocoa
 import FlutterMacOS
+import kraken_method_channel
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
@@ -7,9 +8,15 @@ class MainFlutterWindow: NSWindow {
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
-
+    
     RegisterGeneratedPlugins(registry: flutterViewController)
-
+    
+    func handler(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) {
+      KrakenMethodChannel.shared.invokeMethod(method: call.method, arguments: call.arguments)
+      result("method: " + call.method)
+    }
+    
+    KrakenMethodChannel.shared.setMessageHandler(handler)
     super.awakeFromNib()
   }
 }
