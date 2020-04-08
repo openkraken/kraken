c5af4e3e7aec4fa46e859fbecf89fedd9476c416
卓凌 <zhuoling.lcl@alibaba-inc.com>
2020-04-08 11:43:18 +0800

    feat: performance
    Former-commit-id: 276de972e1e9c1a4ff4a4ab833a0226ac5f98406 [formerly 4ce5786d1b9cfb142f92e1eb166944f8e5e7d451]
    Former-commit-id: 6183e8f17f0934c44e321d814145189f30f78b13

--- bridge/polyfill/src/performance.ts
+++ bridge/polyfill/src/performance.ts
@@ -1,15 +1,34 @@
-import {krakenInvokeModule} from "./bridge";
+import { krakenInvokeModule } from './bridge';
+import { EventTarget} from 'event-target-shim';
 
-const performance = {
-  now() {
+// https://www.w3.org/TR/hr-time-2/#the-performance-interface
+class Performance extends EventTarget {
+  private _timeOrigin: number;
+
+  constructor() {
+    super();
+    this._timeOrigin = this.now();
+  }
+
+  public get timeOrigin() : number {
+    return this._timeOrigin;
+  }
+
+  public now() : number {
     const timeStamp = krakenInvokeModule('["Performance","now"]');
     return parseFloat(timeStamp);
   }
-};
+
+  public toJSON() {
+    return {
+      timeOrigin: this._timeOrigin,
+    };
+  }
+}
 
 Object.defineProperty(global, 'performance', {
   enumerable: true,
   writable: false,
-  value: performance,
+  value: new Performance(),
   configurable: false
 });
