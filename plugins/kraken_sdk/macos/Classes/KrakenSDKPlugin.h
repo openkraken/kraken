d69b780b16132f1e3239356c4a3b569e239f3b26
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-04-08 15:24:11 +0800

    fix: fix KrakenSDK MethodChannel Crashs.
    Former-commit-id: c6a978910edd6af0a8567f66a49d9e050411b441 [formerly 6b55fa5f200ae0819b32a17248d6faba9693331c]
    Former-commit-id: 7ee19349ad9d4a27d44e820a0fc3ad7287854485

--- plugins/kraken_sdk/macos/Classes/KrakenSDKPlugin.h
+++ plugins/kraken_sdk/macos/Classes/KrakenSDKPlugin.h
@@ -3,7 +3,10 @@
 @interface KrakenSDKPlugin : NSObject<FlutterPlugin>
 
 @property NSObject<FlutterPluginRegistrar> *registrar;
+@property FlutterMethodChannel *channel;
 
 - (instancetype) initWithRegistrar: (NSObject<FlutterPluginRegistrar>*)registrar;
+
++ (FlutterMethodChannel *) getMethodChannel;
 
 @end
--- tests/macos/Runner/MainFlutterWindow.swift
+++ tests/macos/Runner/MainFlutterWindow.swift
@@ -8,16 +8,15 @@ class MainFlutterWindow: NSWindow {
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
-
+    
     RegisterGeneratedPlugins(registry: flutterViewController)
     
-    var kraken = Kraken.init(flutterEngine: flutterViewController.engine)
-
-    func handler(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) {
-      kraken?.invokeMethod(call.method, arguments: call.arguments)
-      result("method: " + call.method)
-    }
-    kraken?.setValue(handler, forKey: "methodHandler")
+    let kraken = Kraken.init(flutterEngine: flutterViewController.engine)
+    
+    kraken.registerMethodCallHandler({ (call: FlutterMethodCall, result: FlutterResult) in
+      kraken.invokeMethod(call.method, arguments: call.arguments)
+    })
+    
     super.awakeFromNib()
   }
 }
