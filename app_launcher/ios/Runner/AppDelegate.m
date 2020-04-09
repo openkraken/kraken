3d4425198c8aea485b3d408a07ce431d496e8967
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-04-09 13:54:06 +0800

    fix: xcode shell scripts can change kraken_bridge.framework archs
    Former-commit-id: 1fd3fef6d1d7c01365ead964be2f42a45f6a64bc [formerly 867452fafcfe3c9d7e49a770828eefca11ee800c]
    Former-commit-id: d5b4ff8e5b2d6c8078667bcc1cfeb87abb380cec

--- app_launcher/ios/Runner/AppDelegate.m
+++ app_launcher/ios/Runner/AppDelegate.m
@@ -1,11 +1,15 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
+#import <kraken/Kraken.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
+  FlutterEngine *engine = ((FlutterViewController*)self.window.rootViewController).engine;
+  Kraken *kraken = [[Kraken alloc] initWithFlutterEngine:engine];
+  [kraken loadUrl:@"https://dev.g.alicdn.com/kraken/kraken-demos/richtext/build/kraken/index.js"];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
