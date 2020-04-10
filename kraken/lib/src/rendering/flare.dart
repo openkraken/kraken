c9fc01ebaa9e200df7b0c5642c42662c64c47d65
元彦 <yuanyan.cao@alibaba-inc.com>
2020-04-11 00:47:38 +0800

    chore: style fix and file rename
    Former-commit-id: 83481fe867eecf2d1a7fcce4e4e8d336b3c888a4 [formerly f5b523b05bceae1f0c8b4aadc970c1ecad44c7c3]
    Former-commit-id: 0004fa13c8bac84b2b7f9e0c3bc27d8a15b6bf45

--- kraken/lib/src/rendering/flare.dart
+++ kraken/lib/src/rendering/flare.dart
@@ -1,5 +1,6 @@
+import 'dart:ui';
+
 import 'package:flare_flutter/flare_actor.dart';
-import 'package:flutter/material.dart';
 import 'package:kraken/style.dart';
 
 class FlareRenderObject extends FlareActorRenderObject with ElementStyleMixin {
