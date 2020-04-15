916bba26d46b8c05357c117450c93a69774b42a6
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2020-04-15 21:24:40 +0800

    feat: add css-sizing test
    Former-commit-id: 1f2c0e2446eef0027c735221041083e0e378449d [formerly eb35c7df8ea83c5af42ae1a67d1cb57a1dea7932]
    Former-commit-id: 228328ab2b7d69177975940b9f83674082f75bd5

--- tests/integration/specs/css/css-sizing/height.ts
+++ tests/integration/specs/css/css-sizing/height.ts
@@ -1,5 +1,5 @@
 describe('Height', () => {
-  it('001', async () => {
+  it('basic example', async () => {
     const div = document.createElement('div');
     setStyle(div, {
       width: '100px',
@@ -10,5 +10,197 @@ describe('Height', () => {
     document.body.appendChild(div);
     div.style.height = '200px';
     await expectAsync(div.toBlob(1)).toMatchImageSnapshot('');
+  });
+
+  describe('element style has height', () => {
+    it('element is inline', async () => {
+      let element = create('div', {
+        display: 'inline',
+        height: '100px',
+        backgroundColor: '#999',
+      }, [
+        createText('foobar'),
+      ]);
+      append(BODY, element);
+      await matchScreenshot();
+    });
+
+    it('element is inline-block', async () => {
+      let element = create('div', {
+        display: 'inline-block',
+        height: '100px',
+        backgroundColor: '#999',
+      }, [
+        createText('foobar'),
+      ]);
+      append(BODY, element);
+      await matchScreenshot();
+    });
+
+    it('element is inline-flex', async () => {
+      let element = create('div', {
+        display: 'inline-block',
+        height: '100px',
+        backgroundColor: '#999',
+      }, [
+        createText('foobar'),
+      ]);
+      append(BODY, element);
+      await matchScreenshot();
+    });
+
+    it('element is block', async () => {
+      let element = create('div', {
+        display: 'inline-block',
+        height: '100px',
+        backgroundColor: '#999',
+      }, [
+        createText('foobar'),
+      ]);
+      append(BODY, element);
+      await matchScreenshot();
+    });
+
+    it('element is flex', async () => {
+      let element = create('div', {
+        display: 'inline-block',
+        height: '100px',
+        backgroundColor: '#999',
+      }, [
+        createText('foobar'),
+      ]);
+      append(BODY, element);
+      await matchScreenshot();
+    });
+  });
+
+  describe('element style has no height', () => {
+    it('when parent is flex with height and align-items stretch', async () => {
+      const container = document.createElement('div');
+      setStyle(container, {
+        width: '200px',
+        height: '200px',
+        display: 'flex',
+        backgroundColor: '#666',
+        flexDirection: 'column',
+        alignItems: 'stretch',
+      });
+
+      document.body.appendChild(container);
+
+      const child1 = document.createElement('div');
+      setStyle(child1, {
+        height: '50px',
+        backgroundColor: 'blue',
+      });
+      container.appendChild(child1);
+      child1.appendChild(document.createTextNode('block with no width'));
+
+      const child2 = document.createElement('div');
+      setStyle(child2, {
+        width: '50px',
+        height: '50px',
+        backgroundColor: 'red',
+      });
+      container.appendChild(child2);
+
+      const child3 = document.createElement('div');
+      setStyle(child3, {
+        width: '50px',
+        height: '50px',
+        backgroundColor: 'green',
+      });
+      container.appendChild(child3);
+
+      await matchScreenshot();
+    });
+
+    it('when parent is flex with no height and align-items stretch', async () => {
+      const container = document.createElement('div');
+      setStyle(container, {
+        width: '200px',
+        display: 'flex',
+        backgroundColor: '#666',
+        flexDirection: 'column',
+        alignItems: 'stretch',
+      });
+
+      document.body.appendChild(container);
+
+      const child1 = document.createElement('div');
+      setStyle(child1, {
+        height: '50px',
+        backgroundColor: 'blue',
+      });
+      container.appendChild(child1);
+      child1.appendChild(document.createTextNode('block with no width'));
+
+      const child2 = document.createElement('div');
+      setStyle(child2, {
+        width: '50px',
+        height: '50px',
+        backgroundColor: 'red',
+      });
+      container.appendChild(child2);
+
+      const child3 = document.createElement('div');
+      setStyle(child3, {
+        width: '50px',
+        height: '50px',
+        backgroundColor: 'green',
+      });
+      container.appendChild(child3);
+
+      await matchScreenshot();
+    });
+
+    it('when nested in flex parents with align-items stretch', async () => {
+      const container0 = document.createElement('div');
+      setStyle(container0, {
+        width: '300px',
+        height: '300px',
+        display: 'flex',
+        backgroundColor: '#aaa',
+        flexDirection: 'row',
+        alignItems: 'stretch',
+      });
+
+      document.body.appendChild(container0);
+
+      const container = document.createElement('div');
+      setStyle(container, {
+        width: '200px',
+        display: 'flex',
+        backgroundColor: '#666',
+        flexDirection: 'row',
+        alignItems: 'stretch',
+      });
+
+      container0.appendChild(container);
+
+      const child1 = document.createElement('div');
+      setStyle(child1, {
+        width: '50px',
+        backgroundColor: 'blue',
+      });
+      container.appendChild(child1);
+      child1.appendChild(document.createTextNode('block with no height'));
+
+      const child2 = document.createElement('div');
+      setStyle(child2, {
+        width: '50px',
+        height: '100px',
+        backgroundColor: 'red',
+      });
+      container.appendChild(child2);
+
+      const child3 = document.createElement('div');
+      setStyle(child3, {
+        width: '50px',
+        height: '50px',
+        backgroundColor: 'green',
+      });
+      container.appendChild(child3);
+    });
   });
 });
