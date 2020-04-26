758f4915cd3d263a8c0a5ceda750f8bbdb8a9d4a
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-04-26 23:06:48 +0800

    test: add align specs.
    Former-commit-id: e5078373654c36de530866c40d9f5a6f20e96515 [formerly cc173a0eac3463b85d10ddf215b934b2c955d2fb]
    Former-commit-id: 49008c98c06c92ebd856f0d9a2c010dbfd33165a

---
+++ tests/integration/specs/css/css-flexbox/align.ts
@@ -0,0 +1,137 @@
+/*auto generated*/
+describe('align', () => {
+  it('content_center', async () => {
+    let test01;
+    let test02;
+    let test03;
+    let test;
+    test = createElement(
+      'div',
+      {
+        style: {
+          'background-color': '#ff0000',
+          height: '200px',
+          width: '80px',
+          display: 'flex',
+          'flex-wrap': 'wrap',
+          'align-content': 'center',
+          'box-sizing': 'border-box',
+        },
+      },
+      [
+        (test01 = createElement(
+          'div',
+          {
+            style: {
+              width: '50px',
+              height: '50px',
+              'text-align': 'center',
+              'font-size': '20px',
+              'background-color': '#7FFF00',
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`1`)]
+        )),
+        (test02 = createElement(
+          'div',
+          {
+            style: {
+              width: '50px',
+              height: '50px',
+              'text-align': 'center',
+              'font-size': '20px',
+              'background-color': '#00FFFF',
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`2`)]
+        )),
+        (test03 = createElement(
+          'div',
+          {
+            style: {
+              width: '50px',
+              height: '50px',
+              'text-align': 'center',
+              'font-size': '20px',
+              'background-color': '#4169E1',
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`3`)]
+        )),
+      ]
+    );
+    BODY.appendChild(test);
+
+    await matchScreenshot();
+  });
+  xit('content_stretch', async () => {
+    let test01;
+    let test02;
+    let test03;
+    let test;
+    test = createElement(
+      'div',
+      {
+        style: {
+          'background-color': '#ff0000',
+          height: '200px',
+          width: '80px',
+          display: 'flex',
+          'flex-wrap': 'wrap',
+          'align-content': 'stretch',
+          'box-sizing': 'border-box',
+        },
+      },
+      [
+        (test01 = createElement(
+          'div',
+          {
+            style: {
+              width: '50px',
+              height: '50px',
+              'text-align': 'center',
+              'font-size': '20px',
+              'background-color': '#7FFF00',
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`1`)]
+        )),
+        (test02 = createElement(
+          'div',
+          {
+            style: {
+              width: '50px',
+              height: '50px',
+              'text-align': 'center',
+              'font-size': '20px',
+              'background-color': '#00FFFF',
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`2`)]
+        )),
+        (test03 = createElement(
+          'div',
+          {
+            style: {
+              width: '50px',
+              height: '50px',
+              'text-align': 'center',
+              'font-size': '20px',
+              'background-color': '#4169E1',
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`3`)]
+        )),
+      ]
+    );
+    BODY.appendChild(test);
+
+    await matchScreenshot();
+  });
+});
