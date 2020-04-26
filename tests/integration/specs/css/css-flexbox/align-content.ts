e961731ab56be3284e56d6414344ad887d3eb1a4
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-04-26 19:47:59 +0800

    test: add align-content specs.
    Former-commit-id: 297b8de510cea0bceeb4c8ddcbdb3a893fa79b74 [formerly 23c6d329e284ec37cd4373545f7ecd5d07ce4ea7]
    Former-commit-id: d2f3961c9aef1ab4ad024aa41bd50d81983d7e1f

---
+++ tests/integration/specs/css/css-flexbox/align-content.ts
@@ -0,0 +1,686 @@
+/*auto generated*/
+describe('align-content', () => {
+  it('001', async () => {
+    let flexbox;
+    flexbox = createElement(
+      'div',
+      {
+        style: {
+          'background-color': 'red',
+          'align-content': 'center',
+          display: 'flex',
+          'flex-direction': 'row',
+          'flex-wrap': 'wrap',
+          height: '100px',
+          width: '300px',
+          'box-sizing': 'border-box',
+        },
+      },
+      [
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '26px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '26px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '26px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '26px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+      ]
+    );
+    BODY.appendChild(flexbox);
+
+    await matchScreenshot();
+  });
+  it('002', async () => {
+    let flexbox;
+    flexbox = createElement(
+      'div',
+      {
+        style: {
+          'background-color': 'red',
+          'align-content': 'flex-start',
+          display: 'flex',
+          'flex-wrap': 'wrap',
+          height: '100px',
+          width: '300px',
+          'box-sizing': 'border-box',
+        },
+      },
+      [
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '26px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '26px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '26px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '26px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+      ]
+    );
+    BODY.appendChild(flexbox);
+
+    await matchScreenshot();
+  });
+  it('003', async () => {
+    let p;
+    let flexbox;
+    p = createElement(
+      'p',
+      {
+        style: {
+          'box-sizing': 'border-box',
+        },
+      },
+      [createText(`Test passes if there is no red visible on the page.`)]
+    );
+    flexbox = createElement(
+      'div',
+      {
+        style: {
+          'background-color': 'red',
+          'align-content': 'flex-end',
+          display: 'flex',
+          'flex-wrap': 'wrap',
+          height: '100px',
+          width: '300px',
+          'box-sizing': 'border-box',
+        },
+      },
+      [
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '26px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '26px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '26px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '26px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+      ]
+    );
+    BODY.appendChild(p);
+    BODY.appendChild(flexbox);
+
+    await matchScreenshot();
+  });
+  it('004', async () => {
+    let p;
+    let flexbox;
+    p = createElement(
+      'p',
+      {
+        style: {
+          'box-sizing': 'border-box',
+        },
+      },
+      [createText(`Test passes if there is no red visible on the page.`)]
+    );
+    flexbox = createElement(
+      'div',
+      {
+        style: {
+          'background-color': 'red',
+          'align-content': 'space-between',
+          display: 'flex',
+          'flex-wrap': 'wrap',
+          height: '100px',
+          width: '300px',
+          'box-sizing': 'border-box',
+        },
+      },
+      [
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '21px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '21px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '21px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '21px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+      ]
+    );
+    BODY.appendChild(p);
+    BODY.appendChild(flexbox);
+
+    await matchScreenshot();
+  });
+  it('005', async () => {
+    let p;
+    let flexbox;
+    p = createElement(
+      'p',
+      {
+        style: {
+          'box-sizing': 'border-box',
+        },
+      },
+      [createText(`Test passes if there is no red visible on the page.`)]
+    );
+    flexbox = createElement(
+      'div',
+      {
+        style: {
+          'background-color': 'red',
+          'align-content': 'space-around',
+          display: 'flex',
+          'flex-wrap': 'wrap',
+          height: '100px',
+          width: '300px',
+          'box-sizing': 'border-box',
+        },
+      },
+      [
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '22px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '22px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '22px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            height: '22px',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+      ]
+    );
+    BODY.appendChild(p);
+    BODY.appendChild(flexbox);
+
+    await matchScreenshot();
+  });
+  xit('006', async () => {
+    let p;
+    let flexbox;
+    p = createElement(
+      'p',
+      {
+        style: {
+          'box-sizing': 'border-box',
+        },
+      },
+      [createText(`Test passes if there is no red visible on the page.`)]
+    );
+    flexbox = createElement(
+      'div',
+      {
+        style: {
+          'background-color': 'red',
+          'align-content': 'stretch',
+          display: 'flex',
+          'flex-wrap': 'wrap',
+          height: '100px',
+          width: '300px',
+          'box-sizing': 'border-box',
+        },
+      },
+      [
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+        createElement('div', {
+          style: {
+            'background-color': 'green',
+            width: '150px',
+            'box-sizing': 'border-box',
+          },
+        }),
+      ]
+    );
+    BODY.appendChild(p);
+    BODY.appendChild(flexbox);
+
+    await matchScreenshot();
+  });
+  xit('wrap-001', async () => {
+    let log;
+    let box;
+    let box_1;
+    let box_2;
+    let box_3;
+    let box_4;
+    let box_5;
+    let box_6;
+    log = createElement('div', {
+      style: {
+        'box-sizing': 'border-box',
+      },
+    });
+    box = createElement(
+      'div',
+      {
+        style: {
+          display: 'flex',
+          'flex-wrap': 'wrap',
+          position: 'relative',
+          height: '70px',
+          border: '1px solid red',
+          margin: '5px',
+          'box-sizing': 'border-box',
+          'align-content': 'flex-start',
+        },
+      },
+      [
+        createElement(
+          'div',
+          {
+            style: {
+              height: '20px',
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`This text should be at the top of its container`)]
+        ),
+      ]
+    );
+    box_1 = createElement(
+      'div',
+      {
+        style: {
+          display: 'flex',
+          'flex-wrap': 'wrap',
+          position: 'relative',
+          height: '70px',
+          border: '1px solid red',
+          margin: '5px',
+          'box-sizing': 'border-box',
+          'align-content': 'flex-end',
+        },
+      },
+      [
+        createElement(
+          'div',
+          {
+            style: {
+              height: '20px',
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`This text should be at the bottom of its container`)]
+        ),
+      ]
+    );
+    box_2 = createElement(
+      'div',
+      {
+        style: {
+          display: 'flex',
+          'flex-wrap': 'wrap',
+          position: 'relative',
+          height: '70px',
+          border: '1px solid red',
+          margin: '5px',
+          'box-sizing': 'border-box',
+          'align-content': 'center',
+        },
+      },
+      [
+        createElement(
+          'div',
+          {
+            style: {
+              height: '20px',
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`This text should be centered in its container`)]
+        ),
+      ]
+    );
+    box_3 = createElement(
+      'div',
+      {
+        style: {
+          display: 'flex',
+          'flex-wrap': 'wrap',
+          position: 'relative',
+          height: '70px',
+          border: '1px solid red',
+          margin: '5px',
+          'box-sizing': 'border-box',
+          'align-content': 'space-between',
+        },
+      },
+      [
+        createElement(
+          'div',
+          {
+            style: {
+              height: '20px',
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`This text should be at the top of its container`)]
+        ),
+      ]
+    );
+    box_4 = createElement(
+      'div',
+      {
+        style: {
+          display: 'flex',
+          'flex-wrap': 'wrap',
+          position: 'relative',
+          height: '70px',
+          border: '1px solid red',
+          margin: '5px',
+          'box-sizing': 'border-box',
+          'align-content': 'space-around',
+        },
+      },
+      [
+        createElement(
+          'div',
+          {
+            style: {
+              height: '20px',
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`This text should be centered in its container`)]
+        ),
+      ]
+    );
+    box_5 = createElement(
+      'div',
+      {
+        style: {
+          display: 'flex',
+          'flex-wrap': 'wrap',
+          position: 'relative',
+          height: '70px',
+          border: '1px solid red',
+          margin: '5px',
+          'box-sizing': 'border-box',
+          'align-content': 'space-evenly',
+        },
+      },
+      [
+        createElement(
+          'div',
+          {
+            style: {
+              height: '20px',
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`This text should be centered in its container`)]
+        ),
+      ]
+    );
+    box_6 = createElement(
+      'div',
+      {
+        style: {
+          display: 'flex',
+          'flex-wrap': 'wrap',
+          position: 'relative',
+          height: '70px',
+          border: '1px solid red',
+          margin: '5px',
+          'box-sizing': 'border-box',
+          'align-content': 'stretch',
+        },
+      },
+      [
+        createElement(
+          'div',
+          {
+            style: {
+              height: '20px',
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`This text should be at the top of its container`)]
+        ),
+      ]
+    );
+    BODY.appendChild(log);
+    BODY.appendChild(box);
+    BODY.appendChild(box_1);
+    BODY.appendChild(box_2);
+    BODY.appendChild(box_3);
+    BODY.appendChild(box_4);
+    BODY.appendChild(box_5);
+    BODY.appendChild(box_6);
+
+    await matchScreenshot();
+  });
+  it('wrap-002', async () => {
+    let log;
+    let p;
+    let content1Horizontal;
+    let itemHorizontal;
+    let itemHorizontal_1;
+    let itemHorizontal_2;
+    let content2Horizontal;
+    let content3Horizontal;
+    let flexHorizontal;
+    log = createElement('div', {
+      style: {},
+    });
+    p = createElement(
+      'p',
+      {
+        style: {},
+      },
+      [
+        createText(
+          `Test for crbug.com/362848: Flex box word-wrap is not adhering to spec`
+        ),
+      ]
+    );
+    flexHorizontal = createElement(
+      'div',
+      {
+        style: {
+          width: '300px',
+          display: 'flex',
+          height: '100px',
+          'background-color': 'gray',
+          'margin-bottom': '100px',
+        },
+      },
+      [
+        (itemHorizontal = createElement(
+          'div',
+          {
+            style: {
+              width: '50px',
+              'background-color': 'yellow',
+              margin: '10px',
+              flex: 'none',
+            },
+          },
+          [
+            (content1Horizontal = createElement('div', {
+              style: {
+                width: '30px',
+                height: '50px',
+                'background-color': 'red',
+              },
+            })),
+          ]
+        )),
+        (itemHorizontal_1 = createElement(
+          'div',
+          {
+            style: {
+              width: '50px',
+              'background-color': 'yellow',
+              margin: '10px',
+              flex: 'none',
+            },
+          },
+          [
+            (content2Horizontal = createElement('div', {
+              style: {
+                width: '30px',
+                height: '100px',
+                'background-color': 'red',
+              },
+            })),
+          ]
+        )),
+        (itemHorizontal_2 = createElement(
+          'div',
+          {
+            style: {
+              width: '50px',
+              'background-color': 'yellow',
+              margin: '10px',
+              flex: 'none',
+            },
+          },
+          [
+            (content3Horizontal = createElement('div', {
+              style: {
+                width: '30px',
+                height: '50px',
+                'background-color': 'red',
+              },
+            })),
+          ]
+        )),
+      ]
+    );
+    BODY.appendChild(log);
+    BODY.appendChild(p);
+    BODY.appendChild(flexHorizontal);
+
+    await matchScreenshot();
+  });
+});
