8c75ca488d1a1199d33b1ce643917750d0d704f0
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-04-27 23:29:23 +0800

    test: add dynamic-change specs.
    Former-commit-id: 846a1493834c615c835a48faf14c1fd1731a8baa [formerly 350b0a0e185e9bf306eb0fefc40c0666c848dfbd]
    Former-commit-id: 065129089d5b0ead8887fdfeff2ac45c08e9add5

---
+++ tests/integration/specs/css/css-flexbox/dynamic-change.ts
@@ -0,0 +1,123 @@
+/*auto generated*/
+describe('dynamic-change', () => {
+  xit('simplified-layout-002', async () => {
+    let target;
+    let div;
+    div = createElement(
+      'div',
+      {
+        style: {
+          'box-sizing': 'border-box',
+          display: 'flex',
+          width: '100px',
+          height: '100px',
+          'background-color': 'red',
+        },
+      },
+      [
+        createElement(
+          'div',
+          {
+            style: {
+              'box-sizing': 'border-box',
+              contain: 'layout size',
+              height: '100px',
+              flex: '1',
+              'background-color': 'green',
+            },
+          },
+          [
+            (target = createElement('div', {
+              id: 'target',
+              style: {
+                'box-sizing': 'border-box',
+              },
+            })),
+          ]
+        ),
+      ]
+    );
+    BODY.appendChild(div);
+
+    // document.body.offsetTop;
+    // document.getElementById('target').style.width = '1px';
+
+    await matchScreenshot();
+
+    await matchScreenshot();
+  });
+  it('simplified-layout', async () => {
+    let child;
+    let it1;
+    let it2;
+    let flex;
+    let div;
+    div = createElement(
+      'div',
+      {
+        style: {
+          'box-sizing': 'border-box',
+          position: 'relative',
+        },
+      },
+      [
+        (flex = createElement(
+          'div',
+          {
+            id: 'flex',
+            style: {
+              display: 'flex',
+              'flex-direction': 'column',
+              'flex-wrap': 'wrap',
+              position: 'absolute',
+              top: '20px',
+              width: '100px',
+              'box-sizing': 'border-box',
+            },
+          },
+          [
+            (it1 = createElement(
+              'div',
+              {
+                id: 'it1',
+                style: {
+                  'background-color': 'green',
+                  flex: 'none',
+                  height: '100px',
+                  'min-height': '0',
+                  position: 'relative',
+                  'box-sizing': 'border-box',
+                },
+              },
+              [
+                (child = createElement('div', {
+                  id: 'child',
+                  style: {
+                    position: 'absolute',
+                    top: '0',
+                    left: '0',
+                    'box-sizing': 'border-box',
+                  },
+                })),
+              ]
+            )),
+            (it2 = createElement('div', {
+              id: 'it2',
+              style: {
+                'box-sizing': 'border-box',
+              },
+            })),
+          ]
+        )),
+      ]
+    );
+    BODY.appendChild(div);
+    await matchScreenshot();
+
+    it2.style.width = '50px';
+    flex.style.top = '0px';
+    child.style.top = '1px';
+
+    await matchScreenshot();
+  });
+});
