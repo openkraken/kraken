f7707bda31a074fa69c3f1dcd645664f828dcead
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-04-27 22:21:59 +0800

    test: add anoymous-flex specs
    Former-commit-id: 4f155509bf1b967d28165cd240628720acd54753 [formerly 51f3be7e5b6931a754e59623e5d8527224207c28]
    Former-commit-id: 754fd2a4e9d034168c106f5aa709eab7c8a47921

--- tests/integration/specs/css/css-flexbox/anonymous-flex.ts
+++ tests/integration/specs/css/css-flexbox/anonymous-flex.ts
@@ -1,197 +1,266 @@
-// /*auto generated*/
-// describe('anonymous-flex', () => {
-//   it('item-001', async () => {
-//     let p;
-//     let div;
-//     p = createElement(
-//       'p',
-//       {
-//         style: {
-//           'box-sizing': 'border-box',
-//         },
-//       },
-//       [createText(`There should be a space between "two" and "words" below.`)]
-//     );
-//     div = createElement(
-//       'div',
-//       {
-//         style: {
-//           'box-sizing': 'border-box',
-//           display: 'flex',
-//         },
-//       },
-//       [createText(`two words`)]
-//     );
-//     BODY.appendChild(p);
-//     BODY.appendChild(div);
+/*auto generated*/
+describe('anonymous-flex', () => {
+  xit('item-001', async () => {
+    let p;
+    let spanRemove;
+    let div;
+    p = createElement(
+      'p',
+      {
+        style: {
+          'box-sizing': 'border-box',
+        },
+      },
+      [createText(`There should be a space between "two" and "words" below.`)]
+    );
+    div = createElement(
+      'div',
+      {
+        style: {
+          'box-sizing': 'border-box',
+          display: 'flex',
+        },
+      },
+      [
+        createText(`two `),
+        (spanRemove = createElement('span', {
+          style: {
+            'box-sizing': 'border-box',
+          },
+        })),
+        createText(`words`),
+      ]
+    );
+    BODY.appendChild(p);
+    BODY.appendChild(div);
 
-//     document.body.offsetTop;
-//     spanRemove.remove();
+    document.body.offsetTop;
 
-//     await matchScreenshot();
-//   });
-//   it('item-002', async () => {
-//     let p;
-//     let div;
-//     p = createElement(
-//       'p',
-//       {
-//         style: {
-//           'box-sizing': 'border-box',
-//         },
-//       },
-//       [createText(`There should be a space between "two" and "words" below.`)]
-//     );
-//     div = createElement(
-//       'div',
-//       {
-//         style: {
-//           'box-sizing': 'border-box',
-//           display: 'flex',
-//         },
-//       },
-//       [
-//         createText(`two `),
-//         createElement('span', {
-//           style: {
-//             'box-sizing': 'border-box',
-//             display: 'none',
-//           },
-//         }),
-//         createText(`words`),
-//       ]
-//     );
-//     BODY.appendChild(p);
-//     BODY.appendChild(div);
+    spanRemove.remove();
 
-//     await matchScreenshot();
-//   });
-//   it('item-003', async () => {
-//     let p;
-//     let noneSpan;
-//     let div;
-//     p = createElement(
-//       'p',
-//       {
-//         style: {
-//           'box-sizing': 'border-box',
-//         },
-//       },
-//       [createText(`There should be a space between "two" and "words" below.`)]
-//     );
-//     div = createElement(
-//       'div',
-//       {
-//         style: {
-//           'box-sizing': 'border-box',
-//           display: 'flex',
-//         },
-//       },
-//       [
-//         createText(`two `),
-//         (noneSpan = createElement('span', {
-//           style: {
-//             'box-sizing': 'border-box',
-//             display: 'none',
-//           },
-//         })),
-//         createText(`words`),
-//       ]
-//     );
-//     BODY.appendChild(p);
-//     BODY.appendChild(div);
+    await matchScreenshot();
+  });
+  it('item-002', async () => {
+    let p;
+    let div;
+    p = createElement(
+      'p',
+      {
+        style: {
+          'box-sizing': 'border-box',
+        },
+      },
+      [createText(`There should be a space between "two" and "words" below.`)]
+    );
+    div = createElement(
+      'div',
+      {
+        style: {
+          'box-sizing': 'border-box',
+          display: 'flex',
+        },
+      },
+      [
+        createText(`two `),
+        createElement('span', {
+          style: {
+            'box-sizing': 'border-box',
+            display: 'none',
+          },
+        }),
+        createText(`words`),
+      ]
+    );
+    BODY.appendChild(p);
+    BODY.appendChild(div);
 
-//     document.body.offsetTop;
-//     noneSpan.style.display = 'none';
+    await matchScreenshot();
+  });
+  xit('item-003', async () => {
+    let p;
+    let noneSpan;
+    let div;
+    p = createElement(
+      'p',
+      {
+        style: {
+          'box-sizing': 'border-box',
+        },
+      },
+      [createText(`There should be a space between "two" and "words" below.`)]
+    );
+    div = createElement(
+      'div',
+      {
+        style: {
+          'box-sizing': 'border-box',
+          display: 'flex',
+        },
+      },
+      [
+        createText(`two `),
+        (noneSpan = createElement('span', {
+          style: {
+            'box-sizing': 'border-box',
+          },
+        })),
+        createText(`words`),
+      ]
+    );
+    BODY.appendChild(p);
+    BODY.appendChild(div);
 
-//     await matchScreenshot();
-//   });
-//   it('item-004', async () => {
-//     let p;
-//     let div;
-//     p = createElement(
-//       'p',
-//       {
-//         style: {
-//           'box-sizing': 'border-box',
-//         },
-//       },
-//       [
-//         createText(
-//           `The words "Two" and "lines" should not be on the same line.`
-//         ),
-//       ]
-//     );
-//     div = createElement(
-//       'div',
-//       {
-//         style: {
-//           'box-sizing': 'border-box',
-//           display: 'flex',
-//           'flex-direction': 'column',
-//         },
-//       },
-//       [
-//         createText(`Two `),
-//         createElement('span', {
-//           style: {
-//             'box-sizing': 'border-box',
-//             position: 'absolute',
-//           },
-//         }),
-//         createText(`lines`),
-//       ]
-//     );
-//     BODY.appendChild(p);
-//     BODY.appendChild(div);
+    document.body.offsetTop;
 
-//     await matchScreenshot();
-//   });
-//   it('item-005', async () => {
-//     let p;
-//     let absSpan;
-//     let div;
-//     p = createElement(
-//       'p',
-//       {
-//         style: {
-//           'box-sizing': 'border-box',
-//         },
-//       },
-//       [
-//         createText(
-//           `The words "Two" and "lines" should not be on the same line.`
-//         ),
-//       ]
-//     );
-//     div = createElement(
-//       'div',
-//       {
-//         style: {
-//           'box-sizing': 'border-box',
-//           display: 'flex',
-//           'flex-direction': 'column',
-//         },
-//       },
-//       [
-//         createText(`Two`),
-//         (absSpan = createElement('span', {
-//           style: {
-//             'box-sizing': 'border-box',
-//           },
-//         })),
-//         createText(`lines`),
-//       ]
-//     );
-//     BODY.appendChild(p);
-//     BODY.appendChild(div);
+    await matchScreenshot();
+  });
+  it('item-004', async () => {
+    let p;
+    let div;
+    p = createElement(
+      'p',
+      {
+        style: {
+          'box-sizing': 'border-box',
+        },
+      },
+      [
+        createText(
+          `The words "Two" and "lines" should not be on the same line.`
+        ),
+      ]
+    );
+    div = createElement(
+      'div',
+      {
+        style: {
+          'box-sizing': 'border-box',
+          display: 'flex',
+          'flex-direction': 'column',
+        },
+      },
+      [
+        createText(`Two `),
+        createElement('span', {
+          style: {
+            'box-sizing': 'border-box',
+            position: 'absolute',
+          },
+        }),
+        createText(`lines`),
+      ]
+    );
+    BODY.appendChild(p);
+    BODY.appendChild(div);
 
-//     await matchScreenshot();
+    await matchScreenshot();
+  });
+  xit('item-005', async () => {
+    let p;
+    let absSpan;
+    let div;
+    p = createElement(
+      'p',
+      {
+        style: {
+          'box-sizing': 'border-box',
+        },
+      },
+      [
+        createText(
+          `The words "Two" and "lines" should not be on the same line.`
+        ),
+      ]
+    );
+    div = createElement(
+      'div',
+      {
+        style: {
+          'box-sizing': 'border-box',
+          display: 'flex',
+          'flex-direction': 'column',
+        },
+      },
+      [
+        createText(`Two `),
+        (absSpan = createElement(
+          'span',
+          {
+            style: {
+              'box-sizing': 'border-box',
+              display: 'none',
+            },
+          },
+          [createText(`1`)]
+        )),
+        createText(`lines`),
+      ]
+    );
+    BODY.appendChild(p);
+    BODY.appendChild(div);
 
-//     absSpan.style.display = 'none';
-//     absSpan.style.position = 'absolute';
-//     absSpan.style.display = 'inline';
+    absSpan.style.display = 'none';
+    // document.body.offsetTop;
+    // absSpan.style.position = "absolute";
+    // absSpan.style.display = "inline";
 
-//     await matchScreenshot();
-//   });
-// });
+    await matchScreenshot();
+  });
+  it('item-006', async () => {
+    let p;
+    let abs;
+    let abs_1;
+    let spanRemove;
+    let div;
+    p = createElement(
+      'p',
+      {
+        style: {
+          'box-sizing': 'border-box',
+        },
+      },
+      [
+        createText(
+          `The words "Two" and "lines" should not be on the same line.`
+        ),
+      ]
+    );
+    div = createElement(
+      'div',
+      {
+        style: {
+          'box-sizing': 'border-box',
+          display: 'flex',
+          'flex-direction': 'column',
+        },
+      },
+      [
+        createText(`Two `),
+        (abs = createElement('span', {
+          style: {
+            position: 'absolute',
+            'box-sizing': 'border-box',
+          },
+        })),
+        (spanRemove = createElement('span', {
+          style: {
+            'box-sizing': 'border-box',
+          },
+        })),
+        (abs_1 = createElement('span', {
+          style: {
+            position: 'absolute',
+            'box-sizing': 'border-box',
+          },
+        })),
+        createText(`lines`),
+      ]
+    );
+    BODY.appendChild(p);
+    BODY.appendChild(div);
+
+    spanRemove.remove();
+
+    await matchScreenshot();
+  });
+});
