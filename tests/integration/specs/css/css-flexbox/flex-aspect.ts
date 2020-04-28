f064e07a0bd67b10d4d44504066aa6c9f26f89ae
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-04-28 13:30:23 +0800

    test: add flex aspect specs.
    Former-commit-id: 70e6cb3b9443737e85b481c8d1493411b6f0686e [formerly 1c59838f720e9d4cc00aee83bcc72fe400895afd]
    Former-commit-id: 5de75d13e602f32ed339cb877b9e50b87548fe5b

---
+++ tests/integration/specs/css/css-flexbox/flex-aspect.ts
@@ -0,0 +1,71 @@
+/*auto generated*/
+describe('flex-aspect', () => {
+  it('ratio-img-column-001', async () => {
+    let p;
+    let referenceOverlappedRed;
+    let testFlexItemOverlappingGreen;
+    let constrainedFlex;
+    p = createElement(
+      'p',
+      {
+        style: {
+          'box-sizing': 'border-box',
+        },
+      },
+      [
+        createText(`Test passes if there is a filled green square and `),
+        createElement(
+          'strong',
+          {
+            style: {
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`no red`)]
+        ),
+        createText(`.`),
+      ]
+    );
+    referenceOverlappedRed = createElement('div', {
+      id: 'reference-overlapped-red',
+      style: {
+        position: 'absolute',
+        'background-color': 'red',
+        width: '100px',
+        height: '100px',
+        'z-index': '-1',
+        'box-sizing': 'border-box',
+      },
+    });
+    constrainedFlex = createElement(
+      'div',
+      {
+        id: 'constrained-flex',
+        style: {
+          display: 'flex',
+          'flex-direction': 'column',
+          height: '10px',
+          'box-sizing': 'border-box',
+        },
+      },
+      [
+        (testFlexItemOverlappingGreen = createElement('img', {
+          id: 'test-flex-item-overlapping-green',
+          src: 'support/100x100-green.png',
+          style: {
+            'min-width': '0',
+            'min-height': '0',
+            flex: 'none',
+            width: '100px',
+            'box-sizing': 'border-box',
+          },
+        })),
+      ]
+    );
+    BODY.appendChild(p);
+    BODY.appendChild(referenceOverlappedRed);
+    BODY.appendChild(constrainedFlex);
+
+    await matchScreenshot();
+  });
+});
