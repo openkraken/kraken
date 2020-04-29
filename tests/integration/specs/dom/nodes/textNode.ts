156db46b54c0d3ae0b6ee6de667d1f9ebaa0d12a
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2020-04-29 11:57:50 +0800

    test: add test for text style update
    Former-commit-id: ea09db8145526920ca45c2c79b5064aada3b562a [formerly b0ed0b09a195dcb689799588f44bb487094eee0d]
    Former-commit-id: 07f0612f64a8ce35fc234d78e6be77a1e50e8c87

--- tests/integration/specs/dom/nodes/textNode.ts
+++ tests/integration/specs/dom/nodes/textNode.ts
@@ -20,4 +20,38 @@ describe('TextNode', () => {
 
     await matchScreenshot();
   });
+
+  it('should work with style update of non empty text', async () => {
+    const div = document.createElement('div');
+    const child = document.createElement('div');
+    setElementStyle(child, {
+      backgroundColor: '#f40',
+      width: '100px',
+      height: '100px',
+    });
+    const text = document.createTextNode('Hello world');
+    div.appendChild(child);
+    div.appendChild(text);
+    div.style.color = 'blue';
+    document.body.appendChild(div);
+
+    await matchScreenshot();
+  });
+
+  it('should work with style update of empty text', async () => {
+    const div = document.createElement('div');
+    const child = document.createElement('div');
+    setElementStyle(child, {
+      backgroundColor: '#f40',
+      width: '100px',
+      height: '100px',
+    });
+    const text = document.createTextNode('');
+    div.appendChild(child);
+    div.appendChild(text);
+    div.style.color = 'blue';
+    document.body.appendChild(div);
+
+    await matchScreenshot();
+  });
 });
