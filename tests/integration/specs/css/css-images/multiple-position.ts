3a5f12307ebd686ac9dd8ee156f32047b196b91c
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-05-08 22:36:23 +0800

    test: add multiple-position specs.
    Former-commit-id: 1a56e8a3b4b99ccb2713430de412b896c9f5dc0b [formerly 983fdfc5c41fa7c0224139643cb9c8b2f20e80ae]
    Former-commit-id: c17d9e9d757fe672faf5a9ed627cdf33cf484201

---
+++ tests/integration/specs/css/css-images/multiple-position.ts
@@ -0,0 +1,146 @@
+/*auto generated*/
+describe('multiple-position', () => {
+  it('color-stop-conic-2-ref', async () => {
+    let div;
+    div = createElement('div', {
+      style: {
+        'box-sizing': 'border-box',
+        background:
+          'conic-gradient(red 0%, red 25%, blue 25%, blue 75%, green 75%, green 100%)',
+        width: '100px',
+        height: '100px',
+      },
+    });
+    BODY.appendChild(div);
+
+    await matchScreenshot();
+
+    await matchScreenshot();
+  });
+  xit('color-stop-conic-2', async () => {
+    let div;
+    div = createElement('div', {
+      style: {
+        'box-sizing': 'border-box',
+        'background-image':
+          'conic-gradient(red 0% 25%, blue 25% 75%, green 75% 100%)',
+        'background-color': 'orange',
+        width: '100px',
+        height: '100px',
+      },
+    });
+    BODY.appendChild(div);
+
+    await matchScreenshot();
+  });
+  xit('color-stop-conic', async () => {
+    let target;
+    target = createElement('div', {
+      id: 'target',
+      style: {
+        width: '100px',
+        height: '100px',
+        'background-color': 'red',
+        'background-image': 'conic-gradient(green 0% 180deg, blue 180deg)',
+        'box-sizing': 'border-box',
+      },
+    });
+    BODY.appendChild(target);
+
+    await matchScreenshot();
+  });
+  it('color-stop-linear-2-ref', async () => {
+    let div;
+    div = createElement('div', {
+      style: {
+        'box-sizing': 'border-box',
+        background:
+          'linear-gradient(to bottom, red 0%, red 25%, blue 25%, blue 75%, red 75%, red 100%)',
+        width: '100px',
+        height: '100px',
+      },
+    });
+    BODY.appendChild(div);
+
+    await matchScreenshot();
+  });
+  xit('color-stop-linear-2', async () => {
+    let div;
+    div = createElement('div', {
+      style: {
+        'box-sizing': 'border-box',
+        background:
+          'linear-gradient(to bottom, red 0% 25%, blue 25% 75%, red 75% 100%)',
+        width: '100px',
+        height: '100px',
+      },
+    });
+    BODY.appendChild(div);
+
+    await matchScreenshot();
+  });
+  it('color-stop-linear', async () => {
+    let target;
+    target = createElement('div', {
+      id: 'target',
+      style: {
+        width: '100px',
+        height: '100px',
+        'background-color': 'red',
+        'background-image': 'linear-gradient(to right, blue 0% 50%, green 50%)',
+        'box-sizing': 'border-box',
+      },
+    });
+    BODY.appendChild(target);
+
+    await matchScreenshot();
+  });
+  xit('color-stop-radial-2-ref', async () => {
+    let div;
+    div = createElement('div', {
+      style: {
+        'box-sizing': 'border-box',
+        background:
+          'radial-gradient(red 0%, red 25%, blue 25%, blue 75%, red 75%, red 100%)',
+        width: '100px',
+        height: '100px',
+      },
+    });
+    BODY.appendChild(div);
+
+    await matchScreenshot();
+  });
+  xit('color-stop-radial-2', async () => {
+    let div;
+    div = createElement('div', {
+      '<': '',
+      div: '',
+      style: {
+        'box-sizing': 'border-box',
+        background: 'radial-gradient(red 0% 25%, blue 25% 75%, red 75% 100%)',
+        width: '100px',
+        height: '100px',
+      },
+    });
+    BODY.appendChild(div);
+
+    await matchScreenshot();
+  });
+  xit('color-stop-radial', async () => {
+    let target;
+    target = createElement('div', {
+      id: 'target',
+      style: {
+        width: '100px',
+        height: '100px',
+        'background-color': 'red',
+        'background-image':
+          'radial-gradient(ellipse 50px 10000px at 0px 50px, blue 0% 50px, green 50px)',
+        'box-sizing': 'border-box',
+      },
+    });
+    BODY.appendChild(target);
+
+    await matchScreenshot();
+  });
+});
