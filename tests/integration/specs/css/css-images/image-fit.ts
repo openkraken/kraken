6b42845917f73ec0fc3052fd277187011b420fc5
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-05-08 22:35:38 +0800

    test: add image-fit specs.
    Former-commit-id: 52f69f0d7d5f761645344141515e245f47dcff69 [formerly 9f9256c5b726227f9b66363b078b029ed97ae493]
    Former-commit-id: e66f668dc4573943c7f03e61c5938cecbd69dc4b

---
+++ tests/integration/specs/css/css-images/image-fit.ts
@@ -0,0 +1,81 @@
+/*auto generated*/
+describe('image-fit', () => {
+  it('001', async () => {
+    let div;
+    let div_1;
+    div = createElement(
+      'div',
+      {
+        xmlns: 'http://www.w3.org/1999/xhtml',
+        style: {},
+      },
+      [
+        createText(
+          `A green block appears below. There must be no red on the page.`
+        ),
+      ]
+    );
+    div_1 = createElement(
+      'div',
+      {
+        xmlns: 'http://www.w3.org/1999/xhtml',
+        style: {},
+      },
+      [
+        createElement('img', {
+          src: 'assets/swatch-green.png',
+          alt: 'Failed: image missing',
+          style: {
+            background: 'red',
+            height: '100px',
+            width: '50px',
+          },
+        }),
+      ]
+    );
+    BODY.appendChild(div);
+    BODY.appendChild(div_1);
+
+    await matchScreenshot();
+  });
+  xit('006', async () => {
+    let div;
+    let div_1;
+    div = createElement(
+      'div',
+      {
+        style: {},
+      },
+      [
+        createText(
+          `The image below should fill the blue border with no red between the border and the image.`
+        ),
+      ]
+    );
+    div_1 = createElement(
+      'div',
+      {
+        style: {},
+      },
+      [
+        createElement('img', {
+          src: 'assets/intrinsic-size.png',
+          alt: 'Failed: image missing',
+          style: {
+            border: '5px solid blue',
+            height: '50px',
+            width: '100px',
+            'object-fit': 'fill',
+            background: 'red',
+          },
+        }),
+      ]
+    );
+    BODY.appendChild(div);
+    BODY.appendChild(div_1);
+
+    await matchScreenshot();
+
+    await matchScreenshot();
+  });
+});
