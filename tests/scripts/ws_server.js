caddd798ddf9f09550e60adf3a3da226b53ed28a
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-05-12 14:23:01 +0800

    test: add more unexpected scenarios.
    Former-commit-id: 5ecd20946a471e42eb3749b387da6e55026abf3f [formerly 492da0a04248fb5e6689743ba7d07110d77d7e55]
    Former-commit-id: fbf9a29573374970b513294bb9c543fa164bb91d

--- tests/scripts/ws_server.js
+++ tests/scripts/ws_server.js
@@ -1,8 +1,24 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8399 });
+const simpleServer = new WebSocket.Server({ port: 8399 });
 
-wss.on('connection', function connection(ws) {
+let unstableServer;
+
+// auto closed and recreate ws server.
+// used to test failed connection
+setInterval(() => {
+  if (!unstableServer) {
+    console.log('start server at 8400');
+    unstableServer = new WebSocket.Server({ port: 8400 });
+  } else {
+    // trigger server close and notify all clients.
+    console.log('trigger server close');
+    unstableServer.close();
+    unstableServer = null;
+  }
+}, 200);
+
+simpleServer.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     ws.send(`receive: ${message}`);
   });
@@ -14,9 +30,12 @@ wss.on('connection', function connection(ws) {
   ws.send('something');
 });
 
-process.on('uncaughtException', () => {
-  wss.close();
-});
-process.on('exit', () => {
-  wss.close();
-});
+function closeSimpleServer(e) {
+  console.log(e);
+  simpleServer.close();
+  process.exit(0);
+}
+
+process.on('uncaughtException', closeSimpleServer);
+process.on('exit', () => closeSimpleServer);
+process.on('disconnect', () => closeSimpleServer);
