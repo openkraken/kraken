63941fad276f147c93222330f3801ddd2e7ad076
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2020-05-26 20:26:23 +0800

    feat: add back image fit and alignment
    Former-commit-id: 75871602052d0372aa3d102ff330767a93757ce0 [formerly e82e83e03874de92aef2f61ec41002f195896737]
    Former-commit-id: ddb3473f0cbeb145b558153d99f37d98fa4ac856

--- kraken/lib/src/element/tags/img.dart
+++ kraken/lib/src/element/tags/img.dart
@@ -127,9 +127,86 @@ class ImageElement extends Element {
     imageBox.height = height;
   }
 
+  BoxFit _getBoxFit(CSSStyleDeclaration style) {
+    String fit = style['objectFit'];
+    switch (fit) {
+      case 'contain':
+        return BoxFit.contain;
+
+      case 'cover':
+        return BoxFit.cover;
+
+      case 'none':
+        return BoxFit.none;
+
+      case 'scaleDown':
+      case 'scale-down':
+        return BoxFit.scaleDown;
+
+      case 'fitWidth':
+      case 'fit-width':
+        return BoxFit.fitWidth;
+
+      case 'fitHeight':
+      case 'fit-height':
+        return BoxFit.fitHeight;
+
+      case 'fill':
+      default:
+        return BoxFit.fill;
+    }
+  }
+
+  Alignment _getAlignment(CSSStyleDeclaration style) {
+    // Syntax: object-position: <position>
+    // position: From one to four values that define the 2D position of the element. Relative or absolute offsets can be used.
+    // <position> = [ [ left | center | right ] || [ top | center | bottom ] | [ left | center | right | <length-percentage> ] [ top | center | bottom | <length-percentage> ]? | [ [ left | right ] <length-percentage> ] && [ [ top | bottom ] <length-percentage> ] ]
+    String objectPosition = style['objectPosition'];
+    List<String> splitted = CSSSizingMixin.getShortedProperties(objectPosition);
+    if (splitted.length == 1) {
+      double value = _getAlignmentValueFromString(splitted.first);
+      return Alignment(value, value);
+    } else if (splitted.length > 1) {
+      return Alignment(_getAlignmentValueFromString(splitted[0]), _getAlignmentValueFromString(splitted[1]));
+    } else {
+      // The default value for object-position is 50% 50%
+      return Alignment.center;
+    }
+  }
+
+  double _getAlignmentValueFromString(String value) {
+    assert(value != null);
+
+    // Support percentage
+    if (value.endsWith('%')) {
+      // 0% equal to -1.0
+      // 50% equal to 0.0
+      // 100% equal to 1.0
+      return double.tryParse(value.substring(0, value.length - 1)) / 50 - 1;
+    }
+
+    switch (value) {
+      case 'top':
+      case 'left':
+        return -1;
+
+      case 'bottom':
+      case 'right':
+        return 1;
+
+      case 'center':
+      default:
+        return 0;
+    }
+  }
+
   RenderImage getRenderImageBox(CSSStyleDeclaration style, ImageProvider image) {
+    BoxFit fit = _getBoxFit(style);
+    Alignment alignment = _getAlignment(style);
     return RenderImage(
       image: _imageInfo?.image,
+      fit: fit,
+      alignment: alignment,
     );
   }
 
