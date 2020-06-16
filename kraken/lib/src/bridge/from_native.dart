6f88446a81aa5825008df3c36819cb1bc62944e6
卓凌 <zhuoling.lcl@alibaba-inc.com>
2020-06-16 16:33:41 +0800

    :art: Make vibration works
    Fix a problem in iOS, and it works.
    
    
    Former-commit-id: 551740b12cc7d75f81f7acb9fdbce597b66fb287 [formerly aaecd3de43cd2d276cdc1184a3d4ba4f844609a2]
    Former-commit-id: 9e3312c05e6029304ae84081faa08429b32eec6c

--- kraken/lib/src/bridge/from_native.dart
+++ kraken/lib/src/bridge/from_native.dart
@@ -295,8 +295,20 @@ String invokeModule(String json, DartAsyncModuleCallback callback, Pointer<Void>
     } else if (module == 'Navigator') {
       String method = args[1];
       if (method == 'vibrate') {
-        List<int> methodArgs = args[2];
-        Vibration.vibrate(pattern: methodArgs);
+        List methodArgs = args[2];
+        if (methodArgs.length == 1) {
+          int duration = methodArgs[0];
+          Vibration.vibrate(duration: duration);
+        } else {
+          List<int> filteredArgs = [];
+          for (var number in methodArgs) {
+            if (number is double) filteredArgs.add(number.floor());
+            else if (number is int) filteredArgs.add(number);
+          }
+          // Pattern must have even number of elements, default duration to 500ms.
+          if (filteredArgs.length.isOdd) filteredArgs.add(500);
+          Vibration.vibrate(pattern: filteredArgs);
+        }
       } else if (method == 'cancelVibrate') {
         Vibration.cancel();
       }
