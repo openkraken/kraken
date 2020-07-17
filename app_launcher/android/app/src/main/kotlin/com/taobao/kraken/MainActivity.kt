7fc328402837a6c59efbadfb20e32d5f0436d0c0
tylorvan.fzm <tylorvan.fzm@alibaba-inc.com>
2020-07-17 11:59:10 +0800

    test
    Former-commit-id: e6dc30b0f0b1c1c4cd9e4cc445e2604724c43380 [formerly 897d3980ffd7602f5455c58a3a5b8c9e93c3655b]
    Former-commit-id: 9d78936c37c25ff8d8d0b54a533c00505cb7648b

--- app_launcher/android/app/src/main/kotlin/com/taobao/kraken/MainActivity.kt
+++ app_launcher/android/app/src/main/kotlin/com/taobao/kraken/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.taobao.kraken.app_launcher
 
-import androidx.annotation.NonNull;
+import android.os.Bundle
+import androidx.annotation.NonNull
+import com.taobao.kraken.Kraken
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
+
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    // getFlutterEngine 方法必须在 super.onCreate() 之后
+    // getFlutterEngine 方法必须在 super.onCreate() 之后
+    val kraken = Kraken(flutterEngine)
+    // 这里的 url 可以换成业务 bundle
+    // 这里的 url 可以换成业务 bundle
+    val url = "https://dev.g.alicdn.com/kraken/kraken-demos/richtext/build/kraken/index.js"
+    kraken.loadUrl(url)
+  }
+
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
 }
--- app_launcher/lib/main.dart
+++ app_launcher/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:kraken/kraken.dart' as kraken;
 
 void main() {
-  kraken.launch();
+  kraken.launch(bundleURLOverride: 'https://dev.g.alicdn.com/kraken/kraken-demos/richtext/build/kraken/index.js');
 }
