3b5c5ed6bee2e4a084d20da215f290cece145981
ranmo.cy <ranmo.cy@alibaba-inc.com>
2020-07-21 20:25:00 +0800

    feat: add assert
    Former-commit-id: 778c5ee383b70d1a46361652be8dfa9b73899539 [formerly 2a19285e27a308a1de3c4527af5d60854be9e791]
    Former-commit-id: ca74210531ba0dbc19c49f7539e151448b4fe625

--- kraken/lib/src/rendering/margin.dart
+++ kraken/lib/src/rendering/margin.dart
@@ -126,6 +126,41 @@ class RenderMargin extends RenderShiftedBox {
 
   @override
   bool hitTest(BoxHitTestResult result, { @required Offset position }) {
+    assert(() {
+      if (!hasSize) {
+        if (debugNeedsLayout) {
+          throw FlutterError.fromParts(<DiagnosticsNode>[
+            ErrorSummary('Cannot hit test a render box that has never been laid out.'),
+            describeForError('The hitTest() method was called on this RenderBox'),
+            ErrorDescription(
+                "Unfortunately, this object's geometry is not known at this time, "
+                    'probably because it has never been laid out. '
+                    'This means it cannot be accurately hit-tested.'
+            ),
+            ErrorHint(
+                'If you are trying '
+                    'to perform a hit test during the layout phase itself, make sure '
+                    "you only hit test nodes that have completed layout (e.g. the node's "
+                    'children, after their layout() method has been called).'
+            ),
+          ]);
+        }
+        throw FlutterError.fromParts(<DiagnosticsNode>[
+          ErrorSummary('Cannot hit test a render box with no size.'),
+          describeForError('The hitTest() method was called on this RenderBox'),
+          ErrorDescription(
+              'Although this node is not marked as needing layout, '
+                  'its size is not set.'
+          ),
+          ErrorHint(
+              'A RenderBox object must have an '
+                  'explicit size before it can be hit-tested. Make sure '
+                  'that the RenderBox in question sets its size during layout.'
+          ),
+        ]);
+      }
+      return true;
+    }());
     if (hitTestChildren(result, position: position) || hitTestSelf(position)) {
       result.add(BoxHitTestEntry(this, position));
       return true;
