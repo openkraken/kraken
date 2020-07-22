ae62e827017229593e1561570196f1ba1815b112
ranmo.cy <ranmo.cy@alibaba-inc.com>
2020-07-22 15:22:00 +0800

    feat: del invalid layout
    Former-commit-id: cfbd5efcd7da7fb3d4a188fe8cc70a3ce05b641e [formerly cfa1acec627cf4882062a45d19b2e9fa890b365d]
    Former-commit-id: 50c2e2aa2d485e57f4873183b6824f9956b70b3b

--- kraken/lib/src/rendering/box.dart
+++ kraken/lib/src/rendering/box.dart
@@ -8,26 +8,6 @@ class KrakenRenderConstrainedBox extends RenderConstrainedBox {
   }) : super(child: child, additionalConstraints: additionalConstraints);
 
   @override
-  void layout(Constraints constraints, {bool parentUsesSize = false}) {
-    Constraints additional = additionalConstraints;
-    Constraints result = constraints;
-    if (constraints is BoxConstraints && additional is BoxConstraints) {
-      result = constraints.enforce(additional);
-    }
-    super.layout(result, parentUsesSize: parentUsesSize);
-  }
-
-  @override
-  void performLayout() {
-    if (child != null) {
-      child.layout(constraints, parentUsesSize: true);
-      size = child.size;
-    } else {
-      size = constraints.constrain(Size.zero);
-    }
-  }
-
-  @override
   bool hitTest(BoxHitTestResult result, { @required Offset position }) {
     assert(() {
       if (!hasSize) {
