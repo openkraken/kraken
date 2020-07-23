cdecb6163d048faf93b4929142493febcc83ba99
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-07-23 18:33:25 +0800

    feat: multiple view KrakenSDK API support android.
    Former-commit-id: 46fd12da89f57faa1596a0e401723273421a81b7 [formerly f7258fcaf6ecb9354dab12964ff1c342279e13df]
    Former-commit-id: dbae8123830456b049011acd7248825748d6cc73

--- kraken/android/src/main/java/com/taobao/kraken/KrakenSDKPlugin.java
+++ kraken/android/src/main/java/com/taobao/kraken/KrakenSDKPlugin.java
@@ -14,61 +14,67 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
  * KrakenBundlePlugin
  */
 public class KrakenSDKPlugin implements FlutterPlugin, MethodCallHandler {
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
-    public MethodChannel channel;
-    private FlutterEngine flutterEngine;
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  public MethodChannel channel;
 
-    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-    // plugin registration via this function while apps migrate to use the new Android APIs
-    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-    //
-    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-    // in the same class.
-    public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "kraken");
-        KrakenSDKPlugin plugin = new KrakenSDKPlugin();
-        channel.setMethodCallHandler(plugin);
+  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
+  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
+  // plugin registration via this function while apps migrate to use the new Android APIs
+  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
+  //
+  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
+  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
+  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
+  // in the same class.
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "kraken");
+    KrakenSDKPlugin plugin = new KrakenSDKPlugin();
+    channel.setMethodCallHandler(plugin);
+  }
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "kraken");
+    channel.setMethodCallHandler(this);
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+  }
+
+  public void reload() {
+    if (channel != null) {
+      channel.invokeMethod("reload", null);
+    }
+  }
+
+  @Override
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    String[] group = call.method.split(Kraken.NAME_METHOD_SPLIT);
+    String name = group[0];
+    String method = group[1];
+
+    Kraken kraken = Kraken.get(name);
+
+    if (kraken == null) {
+      result.success(null);
+      return;
     }
 
-    @Override
-    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-        channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "kraken");
-        flutterEngine = flutterPluginBinding.getFlutterEngine();
-        channel.setMethodCallHandler(this);
+    if (method.equals("getUrl")) {
+      result.success(kraken.getUrl());
+    } else if (method.equals("invokeMethod")) {
+      String m = call.argument("method");
+      Object args = call.argument("args");
+      assert m != null;
+      MethodCall callWrap = new MethodCall(m, args);
+      kraken._handleMethodCall(callWrap, result);
+    } else {
+      result.notImplemented();
     }
-
-    public void reload() {
-        if (channel != null) {
-            channel.invokeMethod("reload", null);
-        }
-    }
-
-    @Override
-    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-        Kraken kraken = Kraken.get(flutterEngine);
-        if (call.method.equals("getUrl")) {
-            result.success(kraken == null ? "" : kraken.getUrl());
-        } else if (call.method.equals("invokeMethod")) {
-            String method = call.argument("method");
-            Object args = call.argument("args");
-            assert method != null;
-            MethodCall callWrap = new MethodCall(method, args);
-            kraken._handleMethodCall(callWrap, result);
-        } else {
-            result.notImplemented();
-        }
-    }
-
-    @Override
-    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-        channel.setMethodCallHandler(null);
-        Kraken.get(flutterEngine).destory();
-        flutterEngine = null;
-    }
+  }
 }
