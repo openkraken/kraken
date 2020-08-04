9cef1958465a231dbef7f95630ce7bd91164620f
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-08-04 21:37:29 +0800

    test: add scrollTo specs.
    Former-commit-id: 9829c86ae83646375ea2c7a7a6484ae44d6eabdb [formerly a9b2da3205e603a7c1dec27d9ef7d65aed09d5fd]
    Former-commit-id: 617afe122086db5283db25fa41b0b46e06f7fe56

--- tests/integration/specs/css/css-box/overflow.ts
+++ tests/integration/specs/css/css-box/overflow.ts
@@ -56,4 +56,32 @@ describe('Overflow', () => {
 
     await matchScreenshot();
   });
+
+  it('scrollTo', async (done) => {
+    let container = document.createElement('div');
+    let div1 = document.createElement('div');
+    Object.assign(div1.style, {
+      overflowX: 'scroll',
+      overflowY: 'visible',
+      width: '100px',
+      height: '100px',
+    });
+
+    let inner1 = document.createElement('div');
+    Object.assign(inner1.style, {
+      width: '120px',
+      height: '120px',
+      background: 'conic-gradient(from -90deg, blue 0 25%, black 25% 50%, red 50% 75%, green 75% 100%)',
+    });
+
+    div1.appendChild(inner1);
+    container.appendChild(div1);
+
+    requestAnimationFrame(async () => {
+      div1.scroll(20, 20);
+      await matchScreenshot();
+      done();
+    });
+    document.body.appendChild(container);
+  });
 });
