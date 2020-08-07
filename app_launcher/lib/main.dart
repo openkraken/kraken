c23f927927d7ff8c87786efe76e5c5720aaec187
xiaolun.zxl <xiaolun.zxl@alibaba-inc.com>
2020-08-07 20:33:09 +0800

    feat:getElementById
    Former-commit-id: 8467c524c27d69428cf1cc94b96f7eb481d3c38e [formerly be35a51e31c852c401699eeab5f6e8cbeface95f]
    Former-commit-id: 36c19b5ff5813588c09af94be40b320469177723

--- app_launcher/lib/main.dart
+++ app_launcher/lib/main.dart
@@ -1,17 +1,15 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter/widgets.dart';
-// import 'package:kraken/widget.dart';
-// import 'dart:ui';
-import 'package:kraken/kraken.dart' as kraken;
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:kraken/kraken.dart';
+import 'package:kraken/widget.dart';
+import 'dart:ui';
 
 void main() {
-  kraken.launch(
-      bundleURLOverride: 'http://127.0.0.1:3300/kraken_debug_server.js');
-  // runApp(MaterialApp(
-  //     title: 'Loading Test',
-  //     debugShowCheckedModeBanner: false,
-  //     home: KrakenWidget(
-  //       'main',
-  //         window.physicalSize.width / window.devicePixelRatio, window.physicalSize.height / window.devicePixelRatio)
-  // ));
+  runApp(MaterialApp(
+      title: 'Loading Test',
+      debugShowCheckedModeBanner: false,
+      home: KrakenWidget(
+          'main',
+          window.physicalSize.width / window.devicePixelRatio,
+          window.physicalSize.height / window.devicePixelRatio)));
 }
