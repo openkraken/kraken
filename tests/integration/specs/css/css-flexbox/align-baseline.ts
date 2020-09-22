19dd1752aabb0f0b238a49388f977b1a2c02f1da
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2020-09-22 17:02:51 +0800

    test: migrate flex-box tests
    Former-commit-id: 5f5301b685aee79a82152cadcc5fc0b86a50f459 [formerly 09551ac06da2375590cf8e33889e0e4d119ec10c]
    Former-commit-id: ba35bd2bbc7335d4df1a043ea3bb5fa5b4ebe1e4

--- tests/integration/specs/css/css-flexbox/align-baseline.ts
+++ tests/integration/specs/css/css-flexbox/align-baseline.ts
@@ -1,5 +1,80 @@
 /*auto generated*/
 describe('align-baseline', () => {
+  xit("001", async () => {
+    let flexbox;
+    let flexbox_1;
+    flexbox = createElement(
+      'div',
+      {
+        class: 'flexbox column align-items-baseline',
+        style: {
+          display: 'flex',
+          'flex-direction': 'column',
+          'align-items': 'baseline',
+          'box-sizing': 'border-box',
+        },
+      },
+      [
+        createElement(
+          'div',
+          {
+            style: {
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`This text`)]
+        ),
+        createElement(
+          'p',
+          {
+            style: {
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`should be left aligned.`)]
+        ),
+      ]
+    );
+    flexbox_1 = createElement(
+      'div',
+      {
+        class: 'flexbox column align-items-baseline wrap-reverse',
+        style: {
+          display: 'flex',
+          'flex-direction': 'column',
+          'flex-wrap': 'wrap-reverse',
+          'align-items': 'baseline',
+          'box-sizing': 'border-box',
+        },
+      },
+      [
+        createElement(
+          'div',
+          {
+            style: {
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`This text`)]
+        ),
+        createElement(
+          'p',
+          {
+            style: {
+              'box-sizing': 'border-box',
+            },
+          },
+          [createText(`should be right aligned.`)]
+        ),
+      ]
+    );
+    BODY.appendChild(flexbox);
+    BODY.appendChild(flexbox_1);
+
+
+    await matchScreenshot();
+  });
+
   xit('ref', async () => {
     let flexbox;
     let flexbox_1;
