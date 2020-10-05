305bbcdb200a403aa21184c76e20413c41fff822
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-10-05 10:18:48 +0800

    revert: revert macos generated plugin
    Former-commit-id: fd50f3a172b60dde47cafee36978bec860449f67 [formerly 8211af849667ffdb76e78d11545cbc85ef8221f1]
    Former-commit-id: fcf03868eb0407a5046e28deeead5a5ad15c5b1c

--- app_launcher/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app_launcher/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_macos
-import flutter_jscore
 import kraken
 import kraken_audioplayers
 import kraken_geolocation
@@ -16,7 +15,6 @@ import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
-  FlutterJscorePlugin.register(with: registry.registrar(forPlugin: "FlutterJscorePlugin"))
   KrakenSDKPlugin.register(with: registry.registrar(forPlugin: "KrakenSDKPlugin"))
   AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
