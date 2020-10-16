65e65c5c220a8a6063e78d871a8c0851b5143292
卓凌 <zhuoling.lcl@alibaba-inc.com>
2020-10-16 18:55:46 +0800

    :white_check_mark: Add sliver tests.
    Former-commit-id: 821706af59cd72298d635b74cbc04563565f9bed [formerly cb86398458e31e28b5b656f6cdf48b211a7e2a22]
    Former-commit-id: 887be36a72c3b3addc776c8a2a5b624bb95d4c52

---
+++ tests/integration/specs/css/css-display/sliver.ts
@@ -0,0 +1,33 @@
+describe('display sliver', () => {
+  function createSliverBasicCase() {
+    var d = document.createElement('div');
+
+    for (var i = 0; i < 100; i ++) {
+      var e = document.createElement('div');
+      e.style.background = 'red';
+      e.style.width = e.style.height = '99px';
+      e.appendChild(document.createTextNode(i + ''));
+      d.appendChild(e);
+    }
+
+    d.style.display = 'sliver';
+    d.style.width = '100px';
+    d.style.height = '150px';
+
+    document.body.appendChild(d);
+
+    return d;
+  }
+
+  it('basic', async () => {
+    createSliverBasicCase();
+    await matchViewportSnapshot();
+  });
+
+  it('sliver-direction', async () => {
+    const container = createSliverBasicCase();
+    (container.style as any).sliverDirection = 'column';
+
+    await matchViewportSnapshot();
+  });
+});
