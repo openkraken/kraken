c0461c4282c20c71748a46ee20e6aa3d18ba6966
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-11-12 13:32:59 +0800

    fix: fix integration test crashes. feat: remove flutter driver
    Former-commit-id: 2cc86746f01a28cb93f65d67678182cb9a05eb89 [formerly f30b3bbd5142cf9004b4bf5f839dc1ef3187fc33]
    Former-commit-id: 6443fe34a6e156c0165feb4130ae34cb95ac5f3d

--- tests/integration/app.dart
+++ tests/integration/app.dart
@@ -1,14 +1,13 @@
-import 'dart:convert';
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:kraken/dom.dart';
-import 'package:kraken/kraken.dart';
 import 'package:kraken/module.dart';
 import 'package:kraken/widget.dart';
 import 'package:kraken/css.dart';
 import 'package:ansicolor/ansicolor.dart';
-import 'package:flutter_driver/driver_extension.dart';
+import 'package:path/path.dart' as path;
 import '../bridge/from_native.dart';
 import '../bridge/to_native.dart';
 import 'custom/custom_object_element.dart';
@@ -16,89 +15,100 @@ import 'custom/custom_object_element.dart';
 String pass = (AnsiPen()..green())('[TEST PASS]');
 String err = (AnsiPen()..red())('[TEST FAILED]');
 
-void main() {
+final Directory specsDirectory = Directory('./integration/.specs');
+final Directory snapshotsDirectory = Directory('./integration/snapshots');
+
+void main() async {
   // Set render font family AlibabaPuHuiTi to resolve rendering difference.
   CSSText.DEFAULT_FONT_FAMILY_FALLBACK = ['AlibabaPuHuiTi'];
   CSSText.DEFAULT_FONT_SIZE = 14.0;
   setObjectElementFactory(customObjectElementFactory);
 
+  List<FileSystemEntity> specs = specsDirectory.listSync(recursive: true);
+  List<Map<String, String>> mainTestPayload = [];
+  for (FileSystemEntity file in specs) {
+    if (file.path.endsWith('js')) {
+      String filename = path.basename(file.path);
+      String code = File(file.path).readAsStringSync();
+      mainTestPayload.add({
+        'filename': filename,
+        'filepath': file.path,
+        'code': code,
+      });
+    }
+  }
 
-  // This line enables the extension.
-  enableFlutterDriverExtension(handler: (String payload) async {
-    Completer<String> completer = Completer();
-    List allSpecsPayload = jsonDecode(payload);
+  List<List<Map<String, String>>> allSpecsPayload = [
+    mainTestPayload,
+    mainTestPayload.reversed.toList()
+  ];
+  List<Kraken> widgets = [];
 
-    List<Kraken> widgets = [];
+  for (int i = 0; i < 1; i ++) {
+    KrakenJavaScriptChannel javaScriptChannel = KrakenJavaScriptChannel();
+    javaScriptChannel.onMethodCall = (String method, dynamic arguments) async {
+      javaScriptChannel.invokeMethod(method, arguments);
+      return 'method: ' + method;
+    };
 
-    for (int i = 0; i < 2; i ++) {
-      KrakenJavaScriptChannel javaScriptChannel = KrakenJavaScriptChannel();
-      javaScriptChannel.onMethodCall = (String method, dynamic arguments) async {
-        javaScriptChannel.invokeMethod(method, arguments);
-        return 'method: ' + method;
-      };
-
-      Kraken widget = Kraken(
+    Kraken widget = Kraken(
         viewportWidth: 360,
         viewportHeight: 640,
         bundleContent: 'console.log("starting integration test")',
         disableViewportWidthAssertion: true,
         disableViewportHeightAssertion: true,
         javaScriptChannel: javaScriptChannel
-      );
-      widgets.add(widget);
-    }
+    );
+    widgets.add(widget);
+  }
 
-    runApp(MaterialApp(
-        title: 'Loading Test',
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
+  runApp(MaterialApp(
+      title: 'Loading Test',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
           appBar: AppBar(
-            title: Text('Kraken Integration Test')
+              title: Text('Kraken Integration Test')
           ),
           body: Wrap(
             children: widgets,
           )
-        )
-    ));
+      )
+  ));
 
-    WidgetsBinding.instance
-        .addPostFrameCallback((_) async {
-      registerDartTestMethodsToCpp();
+  WidgetsBinding.instance
+      .addPostFrameCallback((_) async {
+    registerDartTestMethodsToCpp();
 
-      List<Future<String>> testResults = [];
+    List<Future<String>> testResults = [];
 
-      for (int i = 0; i < widgets.length; i ++) {
-        int contextId = i;
-        initTestFramework(contextId);
-        addJSErrorListener(contextId, (String err) {
-          print(err);
-        });
+    for (int i = 0; i < widgets.length; i ++) {
+      int contextId = i;
+      initTestFramework(contextId);
+      addJSErrorListener(contextId, (String err) {
+        print(err);
+      });
 
-        List testPayload = allSpecsPayload[i];
+      List<Map<String, String>> testPayload = allSpecsPayload[i];
 
-        // Preload load test cases
-        for (Map spec in testPayload) {
-          String filename = spec['filename'];
-          String code = spec['code'];
-          evaluateTestScripts(contextId, code, url: filename);
-        }
-
-        testResults.add(executeTest(contextId));
+      // Preload load test cases
+      for (Map spec in testPayload) {
+        String filename = spec['filename'];
+        String code = spec['code'];
+        evaluateTestScripts(contextId, code, url: filename);
       }
 
-      List<String> results = await Future.wait(testResults);
+      testResults.add(executeTest(contextId));
+    }
 
-      for (int i = 0; i < results.length; i ++) {
-        String status = results[i];
-        if (status == 'failed') {
-          completer.complete('failed');
-          return;
-        }
+    List<String> results = await Future.wait(testResults);
+
+    for (int i = 0; i < results.length; i ++) {
+      String status = results[i];
+      if (status == 'failed') {
+        exit(1);
       }
+    }
 
-      completer.complete('success');
-    });
-
-    return completer.future;
+    exit(0);
   });
 }
