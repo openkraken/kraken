29a917ff7a4a60f61fb1ee480ebd721cfcc26bf0
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-12-17 18:47:28 +0800

    fix: hostObject add contextId property.
    Former-commit-id: b48a1c30914d45e90754e30b37625196f5504bd5 [formerly 85a08e821bfb2c8c11d6cffcc9942835df05938d]
    Former-commit-id: e32dab50c12fc713007d634487574fd69c47ba5c

--- bridge/bindings/jsc/host_object.cc
+++ bridge/bindings/jsc/host_object.cc
@@ -9,7 +9,7 @@
 namespace kraken::binding::jsc {
 
 HostObject::HostObject(JSContext *context, std::string name)
-  : context(context), name(std::move(name)), ctx(context->context()) {
+  : context(context), name(std::move(name)), ctx(context->context()), contextId(context->getContextId()) {
   JSClassDefinition hostObjectDefinition = kJSClassDefinitionEmpty;
   JSC_CREATE_HOST_OBJECT_DEFINITION(hostObjectDefinition, this->name.c_str(), HostObject);
   jsClass = JSClassCreate(&hostObjectDefinition);
