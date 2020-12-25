28fc35638d01d0950fd8f91311afa7c07c8d41af
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2020-12-25 16:20:00 +0800

    feat: improve bundle resolve performance.
    Former-commit-id: 51e339fe1a03600230fac5aeece6b43fdb00f5bb [formerly 382db2a2a56802da7e231d6a63cac0c7ef5f5112]
    Former-commit-id: bd99895cdd24303b6ba949aa5cb61398c099a734

--- kraken/lib/src/launcher/bundle.dart
+++ kraken/lib/src/launcher/bundle.dart
@@ -5,14 +5,9 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:kraken/bridge.dart';
-import 'package:crypto/crypto.dart';
 import 'package:kraken/module.dart';
-import 'package:path/path.dart' as path;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-import 'package:archive/archive.dart';
-import 'package:archive/archive_io.dart';
-import 'package:path_provider/path_provider.dart';
 
 import 'manifest.dart';
 
@@ -21,7 +16,6 @@ const String BUNDLE_PATH = 'KRAKEN_BUNDLE_PATH';
 const String ENABLE_DEBUG = 'KRAKEN_ENABLE_DEBUG';
 const String ENABLE_PERFORMANCE_OVERLAY = 'KRAKEN_ENABLE_PERFORMANCE_OVERLAY';
 const String DEFAULT_BUNDLE_PATH = 'assets/bundle.js';
-const int ZIP_FILE_MAGIC_NUMBER = 0x04034b50;
 
 String getBundleURLFromEnv() {
   return Platform.environment[BUNDLE_URL];
@@ -29,11 +23,6 @@ String getBundleURLFromEnv() {
 
 String getBundlePathFromEnv() {
   return Platform.environment[BUNDLE_PATH];
-}
-
-String _md5(Uint8List data) {
-  Digest digest = md5.convert(data);
-  return digest.toString();
 }
 
 abstract class KrakenBundle {
@@ -81,21 +70,6 @@ abstract class KrakenBundle {
     return bundle;
   }
 
-  Future<Directory> _getLocalBundleDirectory() async {
-    // https://developer.apple.com/library/archive/documentation/FileManagement/Conceptual/FileSystemProgrammingGuide/FileSystemOverview/FileSystemOverview.html
-    // Each app will have it's unique support directory, if sandbox is off, app
-    // will share a common support directory(eg: /Library/Application support/${bundleId}/ in macOS).
-    Directory support = await getApplicationSupportDirectory();
-    String localBundlePath = '${support.path}/Kraken/Applications';
-
-    // Make sure directory exists.
-    Directory localBundleDirectory = Directory(localBundlePath);
-    if (!localBundleDirectory.existsSync()) {
-      localBundleDirectory.createSync(recursive: true);
-    }
-    return localBundleDirectory;
-  }
-
   Future<void> run(int contextId) async {
     if (!isResolved) await resolve();
 
@@ -137,16 +111,7 @@ class NetworkBundle extends KrakenBundle with BundleMixin {
     bundle.httpClient.userAgent = getKrakenInfo().userAgent;
     String absoluteURL = url.toString();
     ByteData data = await bundle.load(absoluteURL);
-    Uint8List dataList = data.buffer.asUint8List();
-    // Test is zip file.
-    if (isZipFile(dataList)) {
-      bundleId = _md5(dataList);
-      Directory localBundleDirectory = await _getLocalBundleDirectory();
-      await _unArchive(dataList, Directory(path.join(localBundleDirectory.path, bundleId)));
-    } else {
-      content = await _resolveStringFromData(data, absoluteURL);
-    }
-
+    content = await _resolveStringFromData(data, absoluteURL);
     isResolved = true;
   }
 }
@@ -210,79 +175,15 @@ class AssetsBundle extends KrakenBundle with BundleMixin {
     manifest = AppManifest();
     String localPath = url.toString();
     ByteData data = await rootBundle.load(localPath);
-    Uint8List buffer = data.buffer.asUint8List();
-    if (isZipFile(buffer)) {
-      Directory localBundleDirectory = await _getLocalBundleDirectory();
-      await _unArchive(buffer, Directory(path.join(localBundleDirectory.path, _md5(buffer))));
-    } else {
-      content = await _resolveStringFromData(data, localPath);
-    }
-
+    content = await _resolveStringFromData(data, localPath);
     isResolved = true;
   }
 }
 
 mixin BundleMixin on KrakenBundle {
-  static String _utf8decode(ByteData data) {
-    return utf8.decode(data.buffer.asUint8List());
-  }
-
   Future<String> _resolveStringFromData(ByteData data, String key) async {
     if (data == null) throw FlutterError('Unable to load asset: $key');
-    if (data.lengthInBytes < 10 * 1024) {
-      // 10KB takes about 3ms to parse on a Pixel 2 XL.
-      // See: https://github.com/dart-lang/sdk/issues/31954
-      return utf8.decode(data.buffer.asUint8List());
-    }
-    return compute(_utf8decode, data, debugLabel: 'UTF8 decode for "$key"');
-  }
-
-  Future<void> _unArchive(Uint8List data, Directory dest) async {
-    Archive archive = ZipDecoder().decodeBytes(data.cast<int>());
-    for (ArchiveFile file in archive) {
-      String filename = file.name;
-      if (file.isFile) {
-        if (filename == 'index.js') {
-          content = utf8.decode(file.content);
-        } else if (filename == 'manifest.json') {
-          try {
-            Map<String, dynamic> manifestJson = jsonDecode(utf8.decode(file.content));
-            manifest = AppManifest.fromJson(manifestJson);
-          } catch (err, stack) {
-            print('Failed to parse manifest.json');
-            print('$err\n$stack');
-          }
-        } else {
-          // Treat as assets.
-          assets.add(filename);
-          File(path.join(dest.path, filename))
-            ..createSync(recursive: true)
-            ..writeAsBytesSync(file.content);
-        }
-      } else {
-        final dir = Directory(path.join(dest.path, filename));
-        dir.create(recursive: true);
-      }
-    }
-
-    if (content == null) {
-      if (kReleaseMode) {
-        content = '';
-      } else {
-        throw FlutterError('ZipBundle have no JS bundle.');
-      }
-    }
-  }
-
-  bool isZipFile(Uint8List buffer) {
-    if (buffer.length < 4) return false;
-
-    /// Read a 32-bit word from the buffer.
-    final b1 = buffer[0] & 0xff;
-    final b2 = buffer[1] & 0xff;
-    final b3 = buffer[2] & 0xff;
-    final b4 = buffer[3] & 0xff;
-    int signature = (b4 << 24) | (b3 << 16) | (b2 << 8) | b1;
-    return signature == ZIP_FILE_MAGIC_NUMBER;
+    // Utf8 decode is fast enough with dart 2.10
+    return utf8.decode(data.buffer.asUint8List());
   }
 }
