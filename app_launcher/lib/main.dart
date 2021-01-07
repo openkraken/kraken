5e655c54153f936f94c770d800dbf0a2d44a61fe
卓凌 <zhuoling.lcl@alibaba-inc.com>
2021-01-07 19:58:44 +0800

    :recycle: chore: restore.
    Former-commit-id: 73efa8bc3abe03e1a635c855f714a34fddd2999f [formerly a9d3fb077966d7c13c657cfcbb714c53a5816af7]
    Former-commit-id: 6353873bc89f0d2e004ea8f24e7dd09b661e8a3a

--- app_launcher/lib/main.dart
+++ app_launcher/lib/main.dart
@@ -1,11 +1,49 @@
-import 'dart:ui';
+import 'dart:async';
+import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:kraken/widget.dart';
+import 'dart:ui';
 
 void main() {
-  runApp(Kraken(
-    viewportWidth: window.physicalSize.width / window.devicePixelRatio,
-    viewportHeight: window.physicalSize.height / window.devicePixelRatio,
+  runApp(MaterialApp(
+    title: 'Kraken App Demo',
+    home: FirstRoute(),
   ));
+}
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Kraken App Demo'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Open Kraken Page'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Kraken kraken = Kraken(
+      viewportWidth: window.physicalSize.width / window.devicePixelRatio,
+      viewportHeight: window.physicalSize.height / window.devicePixelRatio,
+    );
+
+    return kraken;
+  }
 }
