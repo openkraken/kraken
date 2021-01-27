2fe9fc8e7ed4c1311415a9ada35847a954f6f753
元彦 <yuanyan.cao@alibaba-inc.com>
2021-01-27 21:49:38 +0800

    chore: move scrollable to gesture
    Former-commit-id: b17ecc61a9f5e0eab545d091430dd3a37b48832c [formerly 956ab6631f3409d0b69ef28acdba678cf653f051]
    Former-commit-id: 43a6471045ed8c77116d466448528c7277032c02

--- kraken/lib/src/rendering/box_model.dart
+++ kraken/lib/src/rendering/box_model.dart
@@ -14,6 +14,7 @@ import 'package:kraken/kraken.dart';
 import 'package:kraken/module.dart';
 import 'package:kraken/rendering.dart';
 import 'package:kraken/inspector.dart';
+import 'package:kraken/gesture.dart';
 
 class RenderLayoutParentData extends ContainerBoxParentData<RenderBox> {
   bool isPositioned = false;
