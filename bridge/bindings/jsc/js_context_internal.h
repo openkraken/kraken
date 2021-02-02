cfdea09340913c8d3b5d1fcfd111043b3276fc0f
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2021-02-02 20:24:45 +0800

    refactor: make symbol public.
    Former-commit-id: 5191490306802ac90a2463321f57397772e961da [formerly 38e3c677cc3ac3f3291ca57156a9530bc5f98028]
    Former-commit-id: a2797cddd9c68afee1d89b2b5bc05adb6d9017ff

--- bridge/bindings/jsc/js_context_internal.h
+++ bridge/bindings/jsc/js_context_internal.h
@@ -7,7 +7,6 @@
 #define KRAKENBRIDGE_JS_CONTEXT_INTERNAL_H
 
 #include "bindings/jsc/macros.h"
-#include "foundation/macros.h"
 #include "include/kraken_bridge.h"
 #include <chrono>
 #include <codecvt>
@@ -32,81 +31,14 @@
 
 namespace kraken::binding::jsc {
 
-class JSContext;
-
-class JSFunctionHolder {
-public:
-  JSFunctionHolder() = delete;
-  explicit JSFunctionHolder(JSContext *context, void *data, std::string name, JSObjectCallAsFunctionCallback callback);
-  explicit JSFunctionHolder(JSContext *context, std::string name, JSObjectCallAsFunctionCallback callback);
-  ~JSFunctionHolder();
-
-  JSObjectRef function();
-
-private:
-  JSObjectRef m_function{nullptr};
-  JSContext *context{nullptr};
-  void *m_data{nullptr};
-  std::string m_name;
-  JSObjectCallAsFunctionCallback m_callback{nullptr};
-  FML_DISALLOW_COPY_ASSIGN_AND_MOVE(JSFunctionHolder);
-};
-
-class JSStringHolder {
-public:
-  JSStringHolder() = delete;
-  explicit JSStringHolder(JSContext *context, const std::string &string);
-  ~JSStringHolder();
-
-  JSValueRef makeString();
-  JSStringRef getString();
-  std::string string();
-
-  const JSChar *ptr();
-  size_t utf8Size();
-  size_t size();
-  bool empty();
-
-  void setString(JSStringRef value);
-  void setString(NativeString *value);
-
-private:
-  JSContext *m_context;
-  JSStringRef m_string{nullptr};
-  FML_DISALLOW_COPY_ASSIGN_AND_MOVE(JSStringHolder);
-};
-
-class JSValueHolder {
-public:
-  JSValueHolder() = delete;
-  explicit JSValueHolder(JSContext *context, JSValueRef value);
-  ~JSValueHolder();
-  JSValueRef value();
-
-  void setValue(JSValueRef value);
-
-private:
-  JSContext *m_context;
-  JSValueRef m_value{nullptr};
-  FML_DISALLOW_COPY_ASSIGN_AND_MOVE(JSValueHolder);
-};
-
 static inline bool isNumberIndex(std::string &name) {
   if (name.empty()) return false;
   char f = name[0];
   return f >= '0' && f <= '9';
 }
 
-
 JSObjectRef makeObjectFunctionWithPrivateData(JSContext *context, void *data, const char *name,
                                               JSObjectCallAsFunctionCallback callback);
-
-void buildUICommandArgs(JSStringRef key, NativeString &args_01);
-void buildUICommandArgs(std::string &key, NativeString &args_01);
-void buildUICommandArgs(std::string &key, JSStringRef value, NativeString &args_01, NativeString &args_02);
-void buildUICommandArgs(std::string &key, std::string &value, NativeString &args_01, NativeString &args_02);
-
-void throwJSError(JSContextRef ctx, const char* msg, JSValueRef *exception);
 
 JSObjectRef JSObjectMakePromise(JSContext *context, void *data, JSObjectCallAsFunctionCallback callback,
                                 JSValueRef *exception);
@@ -126,9 +58,6 @@ inline bool objectHasProperty(JSContextRef ctx, const std::string key, JSObjectR
   JSStringRelease(keyRef);
   return result;
 }
-
-NativeString *stringToNativeString(std::string &string);
-NativeString *stringRefToNativeString(JSStringRef string);
 
 template <typename T> std::string toUTF8(const std::basic_string<T, std::char_traits<T>, std::allocator<T>> &source) {
   std::string result;
--- bridge/include/kraken_bridge_jsc.h
+++ bridge/include/kraken_bridge_jsc.h
@@ -2,7 +2,33 @@
 #include <chrono>
 #include <functional>
 
+#define EXPORT __attribute__((__visibility__("default")))
+
+#define KRAKEN_DISALLOW_COPY(TypeName) TypeName(const TypeName &) = delete
+
+#define KRAKEN_DISALLOW_ASSIGN(TypeName) TypeName &operator=(const TypeName &) = delete
+
+#define KRAKEN_DISALLOW_MOVE(TypeName)                                                                                 \
+  TypeName(TypeName &&) = delete;                                                                                      \
+  TypeName &operator=(TypeName &&) = delete
+
+#define FML_DISALLOW_COPY_AND_ASSIGN(TypeName)                                                                         \
+  TypeName(const TypeName &) = delete;                                                                                 \
+  TypeName &operator=(const TypeName &) = delete
+
+#define FML_DISALLOW_COPY_ASSIGN_AND_MOVE(TypeName)                                                                    \
+  TypeName(const TypeName &) = delete;                                                                                 \
+  TypeName(TypeName &&) = delete;                                                                                      \
+  TypeName &operator=(const TypeName &) = delete;                                                                      \
+  TypeName &operator=(TypeName &&) = delete
+
+#define FML_DISALLOW_IMPLICIT_CONSTRUCTORS(TypeName)                                                                   \
+  TypeName() = delete;                                                                                                 \
+  FML_DISALLOW_COPY_ASSIGN_AND_MOVE(TypeName)
+
 using JSExceptionHandler = std::function<void(int32_t contextId, const char *errmsg)>;
+
+class NativeString;
 
 namespace kraken::binding::jsc {
 
@@ -11,7 +37,7 @@ class JSFunctionHolder;
 class JSStringHolder;
 class JSValueHolder;
 
-class JSContext {
+class EXPORT JSContext {
 public:
   static std::vector<JSStaticFunction> globalFunctions;
   static std::vector<JSStaticValue> globalValue;
@@ -47,5 +73,72 @@ private:
   std::atomic<bool> ctxInvalid_{false};
   JSGlobalContextRef ctx_;
 };
+
+class EXPORT JSFunctionHolder {
+public:
+  JSFunctionHolder() = delete;
+  explicit JSFunctionHolder(JSContext *context, void *data, std::string name, JSObjectCallAsFunctionCallback callback);
+  explicit JSFunctionHolder(JSContext *context, std::string name, JSObjectCallAsFunctionCallback callback);
+  ~JSFunctionHolder();
+
+  JSObjectRef function();
+
+private:
+  JSObjectRef m_function{nullptr};
+  JSContext *context{nullptr};
+  void *m_data{nullptr};
+  std::string m_name;
+  JSObjectCallAsFunctionCallback m_callback{nullptr};
+  FML_DISALLOW_COPY_ASSIGN_AND_MOVE(JSFunctionHolder);
+};
+
+class EXPORT JSStringHolder {
+public:
+  JSStringHolder() = delete;
+  explicit JSStringHolder(JSContext *context, const std::string &string);
+  ~JSStringHolder();
+
+  JSValueRef makeString();
+  JSStringRef getString();
+  std::string string();
+
+  const JSChar *ptr();
+  size_t utf8Size();
+  size_t size();
+  bool empty();
+
+  void setString(JSStringRef value);
+  void setString(NativeString *value);
+
+private:
+  JSContext *m_context;
+  JSStringRef m_string{nullptr};
+  FML_DISALLOW_COPY_ASSIGN_AND_MOVE(JSStringHolder);
+};
+
+class EXPORT JSValueHolder {
+public:
+  JSValueHolder() = delete;
+  explicit JSValueHolder(JSContext *context, JSValueRef value);
+  ~JSValueHolder();
+  JSValueRef value();
+
+  void setValue(JSValueRef value);
+
+private:
+  JSContext *m_context;
+  JSValueRef m_value{nullptr};
+  FML_DISALLOW_COPY_ASSIGN_AND_MOVE(JSValueHolder);
+};
+
+void EXPORT buildUICommandArgs(JSStringRef key, NativeString &args_01);
+void EXPORT buildUICommandArgs(std::string &key, NativeString &args_01);
+void EXPORT buildUICommandArgs(std::string &key, JSStringRef value, NativeString &args_01, NativeString &args_02);
+void EXPORT buildUICommandArgs(std::string &key, std::string &value, NativeString &args_01, NativeString &args_02);
+
+void EXPORT throwJSError(JSContextRef ctx, const char *msg, JSValueRef *exception);
+
+EXPORT NativeString *stringToNativeString(std::string &string);
+EXPORT NativeString *stringRefToNativeString(JSStringRef string);
 
 } // namespace kraken::binding::jsc
