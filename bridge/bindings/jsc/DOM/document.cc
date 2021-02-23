ca15ac825bfaf58f9e34cf7dd9a8fbe5fefff1fc
ranmo.cy <ranmo.cy@alibaba-inc.com>
2021-02-23 17:25:07 +0800

    feat: modify gesture event
    Former-commit-id: 2a356069b0772a356751495392d1b224e2c99e53 [formerly 576aa07f02fc289b3a0c115986bfa3ae7c3abf52]
    Former-commit-id: 30b24e185d1446e4530f6b3dc43a9369959e01e2

--- bridge/bindings/jsc/DOM/document.cc
+++ bridge/bindings/jsc/DOM/document.cc
@@ -117,6 +117,18 @@ JSDocument::JSDocument(JSContext *context) : JSNode(context, "Document") {
     JSEvent::defineEvent(EVENT_TOUCH_CANCEL, [](JSContext *context, void *nativeEvent) -> EventInstance* {
       return new TouchEventInstance(JSTouchEvent::instance(context), reinterpret_cast<NativeTouchEvent *>(nativeEvent));
     });
+    JSEvent::defineEvent(EVENT_SWIPE, [](JSContext *context, void *nativeEvent) -> EventInstance* {
+      return new GestureEventInstance(JSGestureEvent::instance(context), reinterpret_cast<NativeGestureEvent *>(nativeEvent));
+    });
+    JSEvent::defineEvent(EVENT_PAN, [](JSContext *context, void *nativeEvent) -> EventInstance* {
+      return new GestureEventInstance(JSGestureEvent::instance(context), reinterpret_cast<NativeGestureEvent *>(nativeEvent));
+    });
+    JSEvent::defineEvent(EVENT_LONG_PRESS, [](JSContext *context, void *nativeEvent) -> EventInstance* {
+      return new GestureEventInstance(JSGestureEvent::instance(context), reinterpret_cast<NativeGestureEvent *>(nativeEvent));
+    });
+    JSEvent::defineEvent(EVENT_SCALE, [](JSContext *context, void *nativeEvent) -> EventInstance* {
+      return new GestureEventInstance(JSGestureEvent::instance(context), reinterpret_cast<NativeGestureEvent *>(nativeEvent));
+    });
   }
   if (!document_registered) {
     document_registered = true;
