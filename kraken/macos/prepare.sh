996d4bea13d3e0cf0f592f3fb6d6f5a913498998
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2021-03-04 21:09:06 +0800

    fix: kraken module's README mirror to project's README.md fix: fix macOS and ios framework symbol link during development.
    Former-commit-id: 48716353d777f9cc0e46a3d64924afb8f4173662 [formerly a95c23b9facf0cb977ba1e278c145d855a57430d]
    Former-commit-id: ffb914460616cab1faf6c4970c97a112ef0a6763

--- kraken/macos/prepare.sh
+++ kraken/macos/prepare.sh
@@ -1,12 +1,14 @@
-
-
 read_version() {
   VERSION_STR=$(cat kraken.podspec | grep s.version | awk '{print $3}')
   END_POS=$(echo ${#VERSION_STR} - 2 | bc)
   export VERSION=${VERSION_STR:1:$END_POS}
 }
 
-if [ ! -d "libkraken_jsc.dylib" ]; then
+if [ -L "libkraken_jsc.dylib" ]; then
+  ROOT=$(pwd)
+  rm libkraken_jsc.dylib
+  ln -s $ROOT/../../sdk/build/macos/lib/x86_64/libkraken_jsc.dylib
+elif [ ! -d "libkraken_jsc.dylib" ]; then
   read_version
   curl -O https://kraken.oss-cn-hangzhou.aliyuncs.com/kraken_bridge/$VERSION/libkraken_jsc.dylib
 fi
