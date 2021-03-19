de7507a914e3c58d3d02a126e6d4711c7a8b2a5a
卓凌 <zhuoling.lcl@alibaba-inc.com>
2021-03-19 13:47:57 +0800

    :recycle: Chore: restore test.

--- scripts/run_test.js
+++ scripts/run_test.js
@@ -8,10 +8,6 @@ const { series } = require('gulp');
 const chalk = require('chalk');
 
 series(
-  'macos-dylib-clean',
-  'compile-polyfill',
-  'build-darwin-kraken-lib',
-
   'integration-test'
 )(() => {
   console.log(chalk.green('Test Success.'));
