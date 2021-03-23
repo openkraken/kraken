1fae028abe608b7819ce5fec6f8d3637c6c78451
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2021-03-23 18:29:45 +0800

    fix: fix method-channel test

--- integration_tests/specs/method-channel/method-channel.ts
+++ integration_tests/specs/method-channel/method-channel.ts
@@ -31,13 +31,17 @@ describe('MethodChannel', () => {
       handlerCount++;
       expect(method).toBe('helloworld');
       expect(args).toEqual(['abc', 1234, null, /* undefined will be converted to */ null, [], true, false, {name: 1}]);
-      done();
+      if(handlerCount == 2) {
+        done();
+      }
     });
     kraken.methodChannel.setMethodCallHandler((method: string, args: any[]) => {
       handlerCount++;
       expect(method).toBe('helloworld');
       expect(args).toEqual(['abc', 1234, null, /* undefined will be converted to */ null, [], true, false, {name: 1}]);
-      done();
+      if (handlerCount == 2) {
+        done();
+      }
     });
     let result = await kraken.methodChannel.invokeMethod('helloworld', 'abc', 1234, null, undefined, [], true, false, {name: 1});
     expect(result).toBe('method: helloworld');
