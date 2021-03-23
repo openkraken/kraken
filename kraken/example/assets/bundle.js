bcc3d3c94fd901b835a19a182a50652f2e18b9b5
董天成 <dongtiangche@outlook.com>
2021-03-23 14:26:50 +0800

    Update bundle.js

--- kraken/example/assets/bundle.js
+++ kraken/example/assets/bundle.js
@@ -1,5 +1,9 @@
 var text = document.createTextNode('Hello World!');
 var p = document.createElement('p');
 p.style.textAlign = 'center';
+p.style.width = '200px';
+p.style.height = '100px';
+p.style.border = '1px solid #000';
+p.style.margin = '0 auto';
 p.appendChild(text);
 document.body.appendChild(p);
