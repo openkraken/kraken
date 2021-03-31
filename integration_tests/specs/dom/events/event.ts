6b09ff083774abb9bde9e4324e2a35771d9d2afc
answershuto <answershuto@gmail.com>
2021-03-31 10:53:29 +0800

    test: modify test

--- integration_tests/specs/dom/events/event.ts
+++ integration_tests/specs/dom/events/event.ts
@@ -350,19 +350,19 @@ describe('Event', () => {
     div.click();
   });
 
-  it('createEvent set bubbles', async (done) => {
+  it('initEvent set bubbles', async (done) => {
     const e = document.createEvent('Event');
     e.initEvent('type', true, true);
     expect(e.bubbles).toBe(true);
   });
 
-  it('createEvent set cancelable', async (done) => {
+  it('initEvent set cancelable', async (done) => {
     const e = document.createEvent('Event');
     e.initEvent('type', true, true);
     expect(e.cancelable).toBe(true);
   });
 
-  it('createEvent set type', async (done) => {
+  it('initEvent set type', async (done) => {
     const type = 'customtype';
     const e = document.createEvent('Event');
     e.initEvent(type, true, true);
