b5574e675f22d2c922ab5d8546d194bd75fb09ab
元彦 <yuanyan.cao@gmail.com>
2021-04-20 20:15:44 +0800

    refactor: change snapshots dir (#162)

--- integration_tests/runtime/global.ts
+++ integration_tests/runtime/global.ts
@@ -180,12 +180,14 @@ function append(parent: HTMLElement, child: Node) {
   parent.appendChild(child);
 }
 
-async function snapshot(target: HTMLElement | number = 0.0) {
-  if (typeof target == 'number') {
-    await sleep(target);
-    await expectAsync(document.body.toBlob(1.0)).toMatchSnapshot();
+async function snapshot(target: any, filename ?: String) {
+  if (target && target.toBlob) {
+    await expectAsync(target.toBlob(1.0)).toMatchSnapshot(filename); 
   } else {
-    await expectAsync(target.toBlob(1.0)).toMatchSnapshot(); 
+    if (typeof target == 'number') {
+      await sleep(target);
+    }
+    await expectAsync(document.body.toBlob(1.0)).toMatchSnapshot(filename);
   }
 }
 
