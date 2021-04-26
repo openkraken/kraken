53f24f16e741cef8041c1a72302b5fd7b20a6298
元彦 <yuanyan.cao@alibaba-inc.com>
2021-04-26 19:24:06 +0800

    tests: add html test

---
+++ integration_tests/specs/dom/elements/html.ts
@@ -0,0 +1,10 @@
+describe('html', () => {
+  it('should exist', async () => {
+    expect(document.documentElement).toBeDefined();
+    expect(document.documentElement.appendChild).toBeDefined();
+  });
+
+  it('tagName is HTML', async () => {
+    expect(document.documentElement.tagName).toBe('HTML');
+  });
+});
