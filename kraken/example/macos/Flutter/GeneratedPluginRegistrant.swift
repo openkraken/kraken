50e654940301bf5f50c54814db75ac72b010dffd
卓凌 <zhuoling.lcl@alibaba-inc.com>
2021-04-30 18:33:41 +0800

    :recycle: Chore: add websocket as default plugin for example.

--- kraken/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ kraken/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import connectivity_macos
 import path_provider_macos
 import shared_preferences_macos
 import kraken
+import kraken_websocket
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   KrakenPlugin.register(with: registry.registrar(forPlugin: "KrakenPlugin"))
+  KrakenWebsocketPlugin.register(with: registry.registrar(forPlugin: "KrakenWebsocketPlugin"))
 }
