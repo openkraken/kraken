b835863fcbe8bf7cd3f44bac18d5405ebd8d1747
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2021-05-07 20:55:34 +0800

    test: change test spec order to cover this case.

--- integration_tests/specs/method-channel/method-channel.ts
+++ integration_tests/specs/method-channel/method-channel.ts
@@ -1,4 +1,14 @@
 describe('MethodChannel', () => {
+  it('setMethodCallHandler multi params', async (done) => {
+    kraken.methodChannel.setMethodCallHandler((method: string, args: any[]) => {
+      expect(method).toBe('helloworld');
+      expect(args).toEqual(['abc', 1234, null, /* undefined will be converted to */ null, [], true, false, {name: 1}]);
+      done();
+    });
+    let result = await kraken.methodChannel.invokeMethod('helloworld', 'abc', 1234, null, undefined, [], true, false, {name: 1});
+    expect(result).toBe('method: helloworld');
+  });
+
   it('invokeMethod', async () => {
     let result = await kraken.methodChannel.invokeMethod('helloworld', 'abc');
     // TEST App will return method string
@@ -12,16 +22,6 @@ describe('MethodChannel', () => {
       done();
     });
     let result = await kraken.methodChannel.invokeMethod('helloworld', 'abc');
-    expect(result).toBe('method: helloworld');
-  });
-
-  it('setMethodCallHandler multi params', async (done) => {
-    kraken.methodChannel.setMethodCallHandler((method: string, args: any[]) => {
-      expect(method).toBe('helloworld');
-      expect(args).toEqual(['abc', 1234, null, /* undefined will be converted to */ null, [], true, false, {name: 1}]);
-      done();
-    });
-    let result = await kraken.methodChannel.invokeMethod('helloworld', 'abc', 1234, null, undefined, [], true, false, {name: 1});
     expect(result).toBe('method: helloworld');
   });
 
