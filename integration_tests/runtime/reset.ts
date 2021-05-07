10cb43425f70454e66bde2fa75451f0fa779cbc1
元彦 <yuanyan.cao@alibaba-inc.com>
2021-05-07 20:51:00 +0800

    chore: set min-height when run tests

--- integration_tests/runtime/reset.ts
+++ integration_tests/runtime/reset.ts
@@ -1,2 +1,3 @@
 // For the snapshot image with white background, the default is transparent
+document.documentElement.style.minHeight = '100vh';
 document.documentElement.style.backgroundColor = 'white';
