495cc45ab8b820a23a47253b6700e621c8168281
元彦 <yuanyan.cao@alibaba-inc.com>
2021-05-10 21:37:09 +0800

    chore: temp disable sticky testcase

--- integration_tests/runtime/reset.ts
+++ integration_tests/runtime/reset.ts
@@ -1,3 +1,2 @@
 // For the snapshot image with white background, the default is transparent
-document.documentElement.style.minHeight = '100vh';
 document.documentElement.style.backgroundColor = 'white';
