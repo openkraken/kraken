05dac9f079b1ec6585292053205c1d86ba183924
ranmo.cy <ranmo.cy@alibaba-inc.com>
2021-05-13 22:15:02 +0800

    feat: move gestures to gesture manager

--- kraken/lib/src/gesture/gesture_manager.dart
+++ kraken/lib/src/gesture/gesture_manager.dart
@@ -4,8 +4,10 @@
  */
 
 import 'package:kraken/rendering.dart';
-import 'package:flutter/gestures.dart';
 import 'package:kraken/gesture.dart';
+import 'package:kraken/dom.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/gestures.dart';
 
 class GestureManager {
 
@@ -23,10 +25,6 @@ class GestureManager {
     renderBoxModelList = [];
   }
 
-  void triggerGesture() {
-
-  }
-
   void addPointer(PointerEvent event) {
     gestures.forEach((key, gesture) {
       gesture.addPointer(event);
@@ -40,12 +38,84 @@ class GestureManager {
     renderBoxModelList = [];
   }
 
+  void onSwipe(Event event) {
+    if (renderBoxModelList.length != 0) {
+      (renderBoxModelList[0] as RenderBoxModel).onSwipe(event);
+    }
+    renderBoxModelList = [];
+  }
+
+  void onPanStart(DragStartDetails details) {
+    if (renderBoxModelList.length != 0) {
+      (renderBoxModelList[0] as RenderBoxModel).onPan(GestureEvent(EVENT_PAN, GestureEventInit( state: EVENT_STATE_START, deltaX: details.globalPosition.dx, deltaY: details.globalPosition.dy )));
+    }
+    renderBoxModelList = [];
+  }
+
+  void onPanUpdate(DragUpdateDetails details) {
+    if (renderBoxModelList.length != 0) {
+      (renderBoxModelList[0] as RenderBoxModel).onPan(GestureEvent(EVENT_PAN, GestureEventInit( state: EVENT_STATE_UPDATE, deltaX: details.globalPosition.dx, deltaY: details.globalPosition.dy )));
+    }
+    renderBoxModelList = [];
+  }
+
+  void onPanEnd(DragEndDetails details) {
+    if (renderBoxModelList.length != 0) {
+      (renderBoxModelList[0] as RenderBoxModel).onPan(GestureEvent(EVENT_PAN, GestureEventInit( state: EVENT_STATE_END, velocityX: details.velocity.pixelsPerSecond.dx, velocityY: details.velocity.pixelsPerSecond.dy )));
+    }
+    renderBoxModelList = [];
+  }
+
+  void onScaleStart(ScaleStartDetails details) {
+    if (renderBoxModelList.length != 0) {
+      (renderBoxModelList[0] as RenderBoxModel).onScale(GestureEvent(EVENT_SCALE, GestureEventInit( state: EVENT_STATE_START )));
+    }
+    renderBoxModelList = [];
+  }
+
+  void onScaleUpdate(ScaleUpdateDetails details) {
+    if (renderBoxModelList.length != 0) {
+      (renderBoxModelList[0] as RenderBoxModel).onScale(GestureEvent(EVENT_SCALE, GestureEventInit( state: EVENT_STATE_UPDATE, rotation: details.rotation, scale: details.scale )));
+    }
+    renderBoxModelList = [];
+  }
+
+  void onScaleEnd(ScaleEndDetails details) {
+    if (renderBoxModelList.length != 0) {
+      (renderBoxModelList[0] as RenderBoxModel).onScale(GestureEvent(EVENT_SCALE, GestureEventInit( state: EVENT_STATE_END )));
+    }
+    renderBoxModelList = [];
+  }
+
+  void onLongPressEnd(LongPressEndDetails details) {
+    if (renderBoxModelList.length != 0) {
+      (renderBoxModelList[0] as RenderBoxModel).onLongPress(GestureEvent(EVENT_LONG_PRESS, GestureEventInit(deltaX: details.globalPosition.dx, deltaY: details.globalPosition.dy )));
+    }
+    renderBoxModelList = [];
+  }
+
   static _getInstance() {
     if (_instance == null) {
       _instance = GestureManager._internal();
 
       _instance.gestures[ClickGestureRecognizer] = ClickGestureRecognizer();
       (_instance.gestures[ClickGestureRecognizer] as ClickGestureRecognizer).onClick = _instance.onClick;
+
+      _instance.gestures[SwipeGestureRecognizer] = SwipeGestureRecognizer();
+      (_instance.gestures[SwipeGestureRecognizer] as SwipeGestureRecognizer).onSwipe = _instance.onSwipe;
+
+      _instance.gestures[PanGestureRecognizer] = PanGestureRecognizer();
+      (_instance.gestures[PanGestureRecognizer] as PanGestureRecognizer).onStart = _instance.onPanStart;
+      (_instance.gestures[PanGestureRecognizer] as PanGestureRecognizer).onUpdate = _instance.onPanUpdate;
+      (_instance.gestures[PanGestureRecognizer] as PanGestureRecognizer).onEnd = _instance.onPanEnd;
+
+      _instance.gestures[LongPressGestureRecognizer] = LongPressGestureRecognizer();
+      (_instance.gestures[LongPressGestureRecognizer] as LongPressGestureRecognizer).onLongPressEnd = _instance.onLongPressEnd;
+
+      _instance.gestures[ScaleGestureRecognizer] = ScaleGestureRecognizer();
+      (_instance.gestures[ScaleGestureRecognizer] as ScaleGestureRecognizer).onStart = _instance.onScaleStart;
+      (_instance.gestures[ScaleGestureRecognizer] as ScaleGestureRecognizer).onUpdate = _instance.onScaleUpdate;
+      (_instance.gestures[ScaleGestureRecognizer] as ScaleGestureRecognizer).onEnd = _instance.onScaleEnd;
     }
     return _instance;
   }
