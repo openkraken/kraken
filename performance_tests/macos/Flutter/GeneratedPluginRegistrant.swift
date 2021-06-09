d46fdf20806c05fe33c1c839ce86da77a45d495f
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2021-06-09 14:51:47 +0800

    feat: add benchmark project templates.

---
+++ performance_tests/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import connectivity_macos
+import path_provider_macos
+import shared_preferences_macos
+import kraken
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  KrakenPlugin.register(with: registry.registrar(forPlugin: "KrakenPlugin"))
+}
