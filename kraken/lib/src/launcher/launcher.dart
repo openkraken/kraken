a10e3051661db83adb2e9feadded24d56f9e332d
董天成 <dongtiangche@outlook.com>
2021-06-17 12:10:44 +0800

    feat: export HttpClientInterceptor api to launcher (#411)

--- kraken/lib/src/launcher/launcher.dart
+++ kraken/lib/src/launcher/launcher.dart
@@ -24,6 +24,7 @@ void launch({
   bool? debugEnableInspector,
   Color background = _white,
   DevToolsService? devToolsService,
+  HttpClientInterceptor? httpClientInterceptor
 }) async {
   // Bootstrap binding.
   ElementsFlutterBinding.ensureInitialized().scheduleWarmUpFrame();
@@ -32,10 +33,11 @@ void launch({
 
   Future<void> _initKrakenApp() async {
     KrakenController controller = KrakenController(null, window.physicalSize.width / window.devicePixelRatio, window.physicalSize.height / window.devicePixelRatio,
-        background: background,
-        showPerformanceOverlay: Platform.environment[ENABLE_PERFORMANCE_OVERLAY] != null,
-        methodChannel: KrakenNativeChannel(),
-        devToolsService: devToolsService
+      background: background,
+      showPerformanceOverlay: Platform.environment[ENABLE_PERFORMANCE_OVERLAY] != null,
+      methodChannel: KrakenNativeChannel(),
+      devToolsService: devToolsService,
+      httpClientInterceptor: httpClientInterceptor
     );
 
     controller.view.attachView(RendererBinding.instance!.renderView);
