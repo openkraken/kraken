ad046259401f7ab9a42c37672ce7f77f41e3dfc0
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2021-06-22 17:40:29 +0800

    test: add test for inset box-shadow

--- integration_tests/specs/css/css-box/box-shadow.ts
+++ integration_tests/specs/css/css-box/box-shadow.ts
@@ -134,4 +134,89 @@ describe('BoxShadow', () => {
       done();
     });
   });
+
+  fdescribe('inset', () => {
+    it('should works with positive box shadow offset', async () => {
+      let div;
+      div = createElement(
+        'div',
+        {
+          style: {
+            display: 'inline-block',
+            width: '200px',
+            height: '100px',
+            margin: '20px',
+            border: '1px solid black',
+            borderRadius: '10px',
+            boxShadow: 'inset 10px 5px 0px 0px red',
+          },
+        },
+      );
+      BODY.appendChild(div);
+
+      await snapshot();
+    });
+
+    it('should works with negative box shadow offset', async () => {
+      let div;
+      div = createElement(
+        'div',
+        {
+          style: {
+            display: 'inline-block',
+            width: '200px',
+            height: '100px',
+            margin: '20px',
+            borderRadius: '10px',
+            border: '1px solid black',
+            boxShadow: 'inset -10px -5px 0px 0px red',
+          },
+        },
+      );
+      BODY.appendChild(div);
+
+      await snapshot();
+    });
+
+    it('should works with blur radius and spread distance', async () => {
+      let div;
+      div = createElement(
+        'div',
+        {
+          style: {
+            display: 'inline-block',
+            border: '3px solid green',
+            width: '200px',
+            height: '100px',
+            margin: '20px',
+            borderRadius: '10px',
+            boxShadow: 'inset 10px 5px 10px 10px red',
+          },
+        },
+      );
+      BODY.appendChild(div);
+
+      await snapshot();
+    });
+
+    it('should works with multiple box shadows', async () => {
+      let div;
+      div = createElement(
+        'div',
+        {
+          style: {
+            display: 'inline-block',
+            width: '200px',
+            height: '100px',
+            margin: '20px',
+            borderRadius: '10px',
+            boxShadow: 'inset 10px 5px 0px 0px blue, inset -10px -5px 10px 10px red',
+          },
+        }
+      );
+      BODY.appendChild(div);
+
+      await snapshot();
+    });
+  });
 });
