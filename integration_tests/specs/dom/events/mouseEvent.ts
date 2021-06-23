5686c88c382fc32cd109a3cf3bdb47e28e0d6730
ranmo.cy <ranmo.cy@alibaba-inc.com>
2021-06-23 19:02:30 +0800

    test: add touch event test.

--- integration_tests/specs/dom/events/mouseEvent.ts
+++ integration_tests/specs/dom/events/mouseEvent.ts
@@ -19,46 +19,6 @@ describe('MouseEvent', () => {
     div.click();
   });
 
-  it('should work with element touch start', (done) => {
-    const div = document.createElement('div')
-    div.style.backgroundColor = 'red';
-    div.style.width = '100px';
-    div.style.height = '100px';
-    document.body.appendChild(div);
-
-    div.addEventListener('touchstart', e => {
-      done();
-    })
-
-    simulateSwipe(0, 0, 100, 100, 0.5);
-  });
-
-  it('should work with element touch move', (done) => {
-    const div = document.createElement('div')
-    div.style.backgroundColor = 'red';
-    div.style.width = '100px';
-    div.style.height = '100px';
-    document.body.appendChild(div);
-
-    div.addEventListener('touchmove', e => {
-      done();
-    })
-    simulateSwipe(0, 0, 100, 100, 0.5);
-  });
-
-  it('should work with element touch end', (done) => {
-    const div = document.createElement('div')
-    div.style.backgroundColor = 'red';
-    div.style.width = '100px';
-    div.style.height = '100px';
-    document.body.appendChild(div);
-
-    div.addEventListener('touchend', e => {
-      done();
-    })
-    simulateSwipe(0, 0, 100, 100, 0.5);
-  });
-
   it('should new MouseEvent', () => {
     let mouseEvent = new MouseEvent('mouseEvent');
     expect(mouseEvent.type).toEqual('mouseEvent');
