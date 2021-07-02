45845ddd6d60b294c7034ab36f99a10d7909b5e6
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2021-07-02 17:33:13 +0800

    feat: use quickjs engine as default javascript engine.

--- kraken/macos/prepare.sh
+++ kraken/macos/prepare.sh
@@ -8,7 +8,17 @@ if [ -L "libkraken_jsc.dylib" ]; then
   ROOT=$(pwd)
   rm libkraken_jsc.dylib
   ln -s $ROOT/../../bridge/build/macos/lib/x86_64/libkraken_jsc.dylib
-elif [ ! -e "libkraken_jsc.dylib" ]; then
-  read_version
-  curl -O https://kraken.oss-cn-hangzhou.aliyuncs.com/kraken_bridge/$VERSION/libkraken_jsc.dylib
+fi
+
+if [ -L "libkraken_quickjs.dylib" ]; then
+  ROOT=$(pwd)
+  rm libkraken_quickjs.dylib
+  ln -s $ROOT/../../bridge/build/macos/lib/x86_64/libkraken_quickjs.dylib
+fi
+
+
+if [ -L "libquickjs.dylib" ]; then
+  ROOT=$(pwd)
+  rm libquickjs.dylib
+  ln -s $ROOT/../../bridge/build/macos/lib/x86_64/libquickjs.dylib
 fi
