616a420e7bddf583a0cd3f4f34cb183076b0b5d6
ranmo.cy <ranmo.cy@alibaba-inc.com>
2021-07-08 17:22:05 +0800

    feat: support style.

--- bridge/bindings/jsc/html_parser.cc
+++ bridge/bindings/jsc/html_parser.cc
@@ -20,13 +20,57 @@ HTMLParser::HTMLParser(std::unique_ptr<JSContext> &context, const JSExceptionHan
 
 }
 
+void HTMLParser::splitStyle(std::string style) {
+
+}
+
 void HTMLParser::traverseHTML(GumboNode * node, ElementInstance* element) {
   const GumboVector* children = &node->v.element.children;
   for (int i = 0; i < children->length; ++i) {
-    GumboNode* child =(GumboNode*) children->data[i];
+    GumboNode* child = (GumboNode*) children->data[i];
+
     if (child->type == GUMBO_NODE_ELEMENT) {
       auto newElement = JSElement::buildElementInstance(m_context.get(), gumbo_normalized_tagname(child->v.element.tag));
       element->internalAppendChild(newElement);
+
+      GumboVector* attributes = &child->v.element.attributes;
+      for (int j = 0; j < attributes->length; ++j) {
+        GumboAttribute* attribute = (GumboAttribute*) attributes->data[j];
+//        KRAKEN_LOG(VERBOSE) << attribute->name;
+//        KRAKEN_LOG(VERBOSE) << attribute->value;
+
+        if (strcmp(attribute->name, "style") == 0) {
+          std::vector<std::string> output;
+          std::string::size_type prev_pos = 0, pos = 0;
+          std::string styles = attribute->value;
+
+          while((pos = styles.find(";", pos)) != std::string::npos)
+          {
+            std::string substring( styles.substr(prev_pos, pos-prev_pos) );
+
+            std::string::size_type position = substring.find(":");
+            if (position != substring.npos) {
+              KRAKEN_LOG(VERBOSE) << substring.substr(0, position);
+              KRAKEN_LOG(VERBOSE) << substring.substr(position + 1, substring.length());
+            }
+
+//            KRAKEN_LOG(VERBOSE) << substring;
+
+            prev_pos = ++pos;
+          }
+
+//          KRAKEN_LOG(VERBOSE) << styles.substr(prev_pos, pos-prev_pos);
+
+          JSStringRef propertyName = JSStringCreateWithUTF8CString("style");
+          JSValueRef exc = nullptr; // exception
+          JSValueRef styleRef = JSObjectGetProperty(m_context->context(), newElement->object, propertyName, &exc);
+          JSObjectRef style = JSValueToObject(m_context->context(), styleRef, nullptr);
+          auto styleDeclarationInstance = static_cast<StyleDeclarationInstance *>(JSObjectGetPrivate(style));
+          std::string strTextAlign = "text-align";
+          styleDeclarationInstance->internalSetProperty(strTextAlign, JSValueMakeString(m_context->context(),JSStringCreateWithUTF8CString("center")), nullptr);
+        }
+      }
+
       traverseHTML(child, newElement);
     } else if (child->type == GUMBO_NODE_TEXT) {
       auto newTextNodeInstance = new JSTextNode::TextNodeInstance(JSTextNode::instance(m_context.get()),
