ce30a49c1b9d91eafabafd5a5b3b49c886538752
ranmo.cy <ranmo.cy@alibaba-inc.com>
2021-08-11 14:07:48 +0800

    feat: content id is required.

--- kraken/lib/src/foundation/url.dart
+++ kraken/lib/src/foundation/url.dart
@@ -8,42 +8,38 @@ import 'package:kraken/foundation.dart';
 
 class UriParser {
   String _url = '';
-  int? _contextId;
+  int _contextId;
 
-  UriParser(int contextId) {
-    _contextId = contextId;
-  }
+  UriParser(contextId) : _contextId = contextId;
 
   Uri parse(Uri uri) {
     String path = uri.toString();
 
-    if(_contextId != null) {
-      KrakenController controller = KrakenController.getControllerOfJSContextId(_contextId)!;
-      HttpClientInterceptor? httpClientInterceptor = controller.httpClientInterceptor;
-      Uri uriHref = Uri.parse(controller.href);
-      String href = controller.href;
+    KrakenController controller = KrakenController.getControllerOfJSContextId(_contextId)!;
+    HttpClientInterceptor? httpClientInterceptor = controller.httpClientInterceptor;
+    Uri uriHref = Uri.parse(controller.href);
+    String href = controller.href;
 
-      // Treat empty scheme as https.
-      if (path.startsWith('//')) {
-        path = 'https:' + path;
-      }
+    // Treat empty scheme as https.
+    if (path.startsWith('//')) {
+      path = 'https:' + path;
+    }
 
-      RegExp exp = RegExp("^([a-z][a-z\d\+\-\.]*:)?\/\/");
-      if (!exp.hasMatch(path) && _contextId != null) {
-        // relative path.
-        if (path.startsWith('/')) {
-          path = uriHref.scheme + '://' + uriHref.host + ':' + uriHref.port.toString() + path;
-        } else {
-          int lastPath = href.lastIndexOf('/');
-          if (lastPath >= 0) {
-            path = href.substring(0, href.lastIndexOf('/')) + '/' + path;
-          }
+    RegExp exp = RegExp("^([a-z][a-z\d\+\-\.]*:)?\/\/");
+    if (!exp.hasMatch(path) && _contextId != null) {
+      // relative path.
+      if (path.startsWith('/')) {
+        path = uriHref.scheme + '://' + uriHref.host + ':' + uriHref.port.toString() + path;
+      } else {
+        int lastPath = href.lastIndexOf('/');
+        if (lastPath >= 0) {
+          path = href.substring(0, href.lastIndexOf('/')) + '/' + path;
         }
       }
+    }
 
-      if (httpClientInterceptor != null) {
-        path = httpClientInterceptor.customURLParser(path, uri.toString());
-      }
+    if (httpClientInterceptor != null) {
+      path = httpClientInterceptor.customURLParser(path, uri.toString());
     }
 
     return Uri.parse(path);
