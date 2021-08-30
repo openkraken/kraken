23938f7b6b246d4b7c007935ae4501fe0d6b4af0
ranmo.cy <ranmo.cy@alibaba-inc.com>
2021-08-30 16:24:39 +0800

    test: add test of document.createDocumentFragment.

--- integration_tests/specs/dom/elements/documentFragment.ts
+++ integration_tests/specs/dom/elements/documentFragment.ts
@@ -1,5 +1,5 @@
 describe('Tags documentFragment', () => {
-  it('support appendChild', async () => {
+  it('should work with appendChild', async () => {
     const list = document.createElement('div');
     document.body.appendChild(list);
 
@@ -20,7 +20,7 @@ describe('Tags documentFragment', () => {
     await snapshot();
   });
 
-  it('basic', async () => {
+  it('should work with insertBefore', async () => {
     const list = document.createElement('div');
     document.body.appendChild(list);
 
@@ -33,6 +33,33 @@ describe('Tags documentFragment', () => {
     const colors = ['red', 'green', 'blue', 'black'];
 
     const fragment = new DocumentFragment();
+
+    colors.forEach(color => {
+      const ele = document.createElement('div');
+      ele.style.width = '100px';
+      ele.style.height = '100px';
+      ele.style.backgroundColor = color;
+      fragment.appendChild(ele);
+    });
+
+    list.insertBefore(fragment, e);
+
+    await snapshot();
+  });
+
+  it('should work with createDocumentFragment', async () => {
+    const list = document.createElement('div');
+    document.body.appendChild(list);
+
+    const e = document.createElement('div');
+    e.style.width = '100px';
+    e.style.height = '100px';
+    e.style.backgroundColor = 'yellow';
+    list.appendChild(e);
+
+    const colors = ['red', 'green', 'blue', 'black'];
+
+    const fragment = document.createDocumentFragment();
 
     colors.forEach(color => {
       const ele = document.createElement('div');
