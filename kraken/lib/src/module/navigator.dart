9ea11b3fbc949526f362ceeacdd978bdd0bee9ba
卓凌 <zhuoling.lcl@alibaba-inc.com>
2021-09-03 16:56:43 +0800

    :sparkles: refactor: remove vibrate API, move to plugin.

--- kraken/lib/src/module/navigator.dart
+++ kraken/lib/src/module/navigator.dart
@@ -1,6 +1,5 @@
 import 'package:kraken/bridge.dart';
 import 'package:kraken/src/module/module_manager.dart';
-import 'package:vibration/vibration.dart';
 
 class NavigatorModule extends BaseModule {
   @override
@@ -13,25 +12,7 @@ class NavigatorModule extends BaseModule {
 
   @override
   String invoke(String method, dynamic params, callback) {
-    if (method == 'vibrate') {
-      List methodArgs = params;
-      if (methodArgs.length == 1) {
-        int duration = methodArgs[0];
-        Vibration.vibrate(duration: duration);
-      } else {
-        List<int> filteredArgs = [];
-        for (var number in methodArgs) {
-          if (number is double)
-            filteredArgs.add(number.floor());
-          else if (number is int) filteredArgs.add(number);
-        }
-        // Pattern must have even number of elements, default duration to 500ms.
-        if (filteredArgs.length.isOdd) filteredArgs.add(500);
-        Vibration.vibrate(pattern: filteredArgs);
-      }
-    } else if (method == 'cancelVibrate') {
-      Vibration.cancel();
-    } else if (method == 'getUserAgent') {
+    if (method == 'getUserAgent') {
       return getKrakenInfo().userAgent;
     }
     return '';
