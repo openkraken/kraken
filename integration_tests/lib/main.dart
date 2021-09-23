cc5f3a448d899cdc20717ced08b923ea76bee7ec
andycall <chenghuai.dtc@alibaba-inc.com>
2021-09-23 15:43:43 +0800

    feat: enable all plugins in integration tests.

--- integration_tests/lib/main.dart
+++ integration_tests/lib/main.dart
@@ -17,10 +17,10 @@ import 'custom/custom_object_element.dart';
 import 'custom/custom_element.dart';
 import 'package:kraken/gesture.dart';
 import 'package:kraken/foundation.dart';
-// import 'package:kraken_websocket/kraken_websocket.dart';
-// import 'package:kraken_animation_player/kraken_animation_player.dart';
-// import 'package:kraken_video_player/kraken_video_player.dart';
-// import 'package:kraken_webview/kraken_webview.dart';
+import 'package:kraken_websocket/kraken_websocket.dart';
+import 'package:kraken_animation_player/kraken_animation_player.dart';
+import 'package:kraken_video_player/kraken_video_player.dart';
+import 'package:kraken_webview/kraken_webview.dart';
 import 'local_http_server.dart';
 
 String? pass = (AnsiPen()..green())('[TEST PASS]');
@@ -70,10 +70,10 @@ class IntegrationTestUriParser extends UriParser {
 
 // By CLI: `KRAKEN_ENABLE_TEST=true flutter run`
 void main() async {
-  // KrakenWebsocket.initialize();
-  // KrakenAnimationPlayer.initialize();
-  // KrakenVideoPlayer.initialize();
-  // KrakenWebView.initialize();
+  KrakenWebsocket.initialize();
+  KrakenAnimationPlayer.initialize();
+  KrakenVideoPlayer.initialize();
+  KrakenWebView.initialize();
   defineKrakenCustomElements();
 
   // FIXME: This is a workaround for testcase
