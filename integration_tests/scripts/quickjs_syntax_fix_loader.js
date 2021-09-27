22867372dcd87c5af266ac57503e4f67f56a14b5
andycall <chenghuai.dtc@alibaba-inc.com>
2021-09-27 17:43:52 +0800

    fix: fix sleep in integration not working.

--- integration_tests/scripts/quickjs_syntax_fix_loader.js
+++ integration_tests/scripts/quickjs_syntax_fix_loader.js
@@ -4,6 +4,6 @@
 // https://github.com/bellard/quickjs/issues/77
 module.exports = function(source) {
   let replacePattern = /\n\s+(await.+);/g;
-  source = source.replace(replacePattern, ';($1);');
+  source = source.replace(replacePattern, '\n;($1);\n');
   return source;
 }
