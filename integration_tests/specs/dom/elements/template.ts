4dde1263fece5f6a8575cc2f0251f6ad297202f4
answershuto <answershuto@gmail.com>
2021-10-08 18:57:56 +0800

    test: add test for template element.

---
+++ integration_tests/specs/dom/elements/template.ts
@@ -0,0 +1,18 @@
+describe('Tags template', () => {
+  it('should work with content', async () => {
+    const t = document.createElement('template')
+    const template = t.content;
+    template.appendChild(document.createTextNode('template text'))
+    document.body.appendChild(template);
+
+    await snapshot();
+  });
+
+  it('should work with innerHTML', async () => {
+    const t = document.createElement('template')
+    t.innerHTML = '<div>template text</div>';
+    document.body.appendChild(t.content);
+
+    await snapshot();
+  });
+});
