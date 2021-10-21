b53702ca2783049c8dc83abdff16fff151279bc0
元彦 <yuanyan.cao@alibaba-inc.com>
2021-10-21 23:56:41 +0800

    chore: fix lint

--- kraken/lib/src/css/offset.dart
+++ kraken/lib/src/css/offset.dart
@@ -1,10 +1,7 @@
-
-
 /*
  * Copyright (C) 2019-present Alibaba Inc. All rights reserved.
  * Author: Kraken Team.
  */
-import 'package:flutter/rendering.dart';
 import 'package:kraken/rendering.dart';
 import 'package:kraken/css.dart';
 
