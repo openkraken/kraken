efea623ad1b3bc12ec764d12329827d9de47de81
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2021-11-02 19:16:27 +0800

    fix: remove addEvent on document

--- kraken/lib/src/dom/document.dart
+++ kraken/lib/src/dom/document.dart
@@ -15,17 +15,8 @@ class Document extends Node {
   Document(int targetId, Pointer<NativeEventTarget> nativeEventTarget, ElementManager elementManager, this.documentElement)
       : super(NodeType.DOCUMENT_NODE, targetId, nativeEventTarget, elementManager);
 
-  void _handleEvent(Event event) {
-    emitUIEvent(elementManager.controller.view.contextId, nativeEventTargetPtr, event);
-  }
-
   @override
   String get nodeName => '#document';
-
-  @override
-  void addEvent(String eventType) {
-    documentElement.addEventListener(eventType, _handleEvent);
-  }
 
   @override
   RenderObject? get renderer => throw FlutterError('Document did\'t have renderObject.');
