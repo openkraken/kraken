a019a11eff63ef2e402cfcaeda2001288f18270a
元彦 <yuanyan.cao@alibaba-inc.com>
2021-11-24 22:28:34 +0800

    refactor: painting order

--- kraken/lib/src/rendering/flex.dart
+++ kraken/lib/src/rendering/flex.dart
@@ -2410,48 +2410,6 @@ class RenderFlexLayout extends RenderLayoutBox {
   }
 
   @override
-  int sortSiblingsByZIndex(RenderObject prev, RenderObject next) {
-    // z-index values other than auto of flex-item create a stacking context even if position is static
-    // (behaving exactly as if position were relative)
-    // https://drafts.csswg.org/css-flexbox-1/#painting
-
-    // z-index descending order is as follows:
-    // 1. element has z-index
-    // 2. element has no z-index and position is non static
-    // 3. element has no z-index and position is static
-    CSSPositionType prevPosition = prev is RenderBoxModel
-      ? prev.renderStyle.position
-      : CSSPositionType.static;
-    CSSPositionType nextPosition = next is RenderBoxModel
-      ? next.renderStyle.position
-      : CSSPositionType.static;
-    int? prevZIndex =
-    prev is RenderBoxModel ? prev.renderStyle.zIndex : null;
-    int? nextZIndex =
-    next is RenderBoxModel ? next.renderStyle.zIndex : null;
-
-    if (prevZIndex != null && nextZIndex != null) {
-      return prevZIndex - nextZIndex;
-    } else if (prevZIndex != null && nextZIndex == null) {
-      return 1;
-    } else if (prevZIndex == null && nextZIndex != null) {
-      return -1;
-    } else {
-      if ((prevPosition != CSSPositionType.static &&
-        nextPosition != CSSPositionType.static) ||
-        (prevPosition == CSSPositionType.static &&
-          nextPosition == CSSPositionType.static)) {
-        return 0;
-      } else if (prevPosition == CSSPositionType.static &&
-          nextPosition != CSSPositionType.static) {
-        return -1;
-      } else {
-        return 1;
-      }
-    }
-  }
-
-  @override
   void performPaint(PaintingContext context, Offset offset) {
     for (int i = 0; i < paintingOrder.length; i++) {
       RenderObject child = paintingOrder[i];
