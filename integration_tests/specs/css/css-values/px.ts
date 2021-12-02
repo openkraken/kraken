6ed502e970bdc3957340f26aa4c48ab411cc675a
站稳 <zhanwen.zw@alibaba-inc.com>
2021-12-02 15:44:46 +0800

    fix: add negative length validation for non negative css value (#938)
    * fix: add negative length validation for non negative css value
    
    * fix: margin parse
    
    * fix: negative zero
    
    * test: add test for invalid negative css value
    
    * fix: delete margin non negative assert
    
    * chore: extract common str in length regular expressions

--- integration_tests/specs/css/css-values/px.ts
+++ integration_tests/specs/css/css-values/px.ts
@@ -46,4 +46,26 @@ describe('px', () => {
       }, 600);
     });
   });
+
+  it('negative css length value should not work', async () => {
+    const container = createElement('div', {
+      style: {
+        background: 'yellow',
+        width: '-100px',
+        height: '-100px',
+        minWidth: '-100px',
+        maxWidth: '-200px',
+        minHeight: '-100px',
+        maxHeight: '-200px',
+        padding: '-50px',
+        border: '-10px solid green',
+      }
+    }, [
+      createText('foo')
+    ]);
+    
+    document.body.appendChild(container);
+
+    await snapshot();
+  });
 });
