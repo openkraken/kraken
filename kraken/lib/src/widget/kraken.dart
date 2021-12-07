8776dd85209631f08b6cd31c58265b95f91177c6
answershuto <answershuto@gmail.com>
2021-12-07 11:11:59 +0800

    :sparkles: feat: merge from main.

--- kraken/lib/src/widget/kraken.dart
+++ kraken/lib/src/widget/kraken.dart
@@ -4,6 +4,7 @@
  */
 import 'dart:io';
 import 'dart:ui';
+import 'dart:ffi';
 import 'dart:typed_data';
 
 import 'package:flutter/foundation.dart';
@@ -11,6 +12,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:kraken/kraken.dart';
 import 'package:kraken/rendering.dart';
 import 'package:kraken/dom.dart' as dom;
@@ -19,6 +21,7 @@ import 'package:kraken/gesture.dart';
 import 'package:kraken/css.dart';
 import 'package:kraken/src/dom/element_registry.dart';
 import 'package:kraken/src/dom/element_manager.dart';
+import 'package:kraken/bridge.dart';
 
 /// Get context of current widget.
 typedef GetContext = BuildContext Function();
@@ -66,17 +69,8 @@ class Kraken extends StatefulWidget {
   // the height of krakenWidget
   final double? viewportHeight;
 
-  // The initial URL to load.
-  final String? bundleURL;
-
-  // The initial assets path to load.
-  final String? bundlePath;
-
-  // The initial raw javascript content to load.
-  final String? bundleContent;
-
-  // The initial raw bytecode to load.
-  final Uint8List? bundleByteCode;
+  //  The initial bundle to load.
+  final KrakenBundle? bundle;
 
   // The animationController of Flutter Route object.
   // Pass this object to KrakenWidget to make sure Kraken execute JavaScripts scripts after route transition animation completed.
@@ -128,34 +122,66 @@ class Kraken extends StatefulWidget {
     defineElement(tagName.toUpperCase(), creator);
   }
 
+  loadBundle(KrakenBundle bundle) async {
+    await controller!.unload();
+    await controller!.loadBundle(
+        bundle: bundle
+    );
+    _evalBundle(controller!, animationController);
+  }
+
+  @deprecated
   loadContent(String bundleContent) async {
     await controller!.unload();
     await controller!.loadBundle(
-        bundleContent: bundleContent
+        bundle: KrakenBundle.fromContent(bundleContent)
     );
     _evalBundle(controller!, animationController);
   }
 
-  loadByteCode(Uint8List bytecode) async {
+  @deprecated
+  loadByteCode(Uint8List bundleByteCode) async {
     await controller!.unload();
     await controller!.loadBundle(
-        bundleByteCode: bytecode
+        bundle: KrakenBundle.fromBytecode(bundleByteCode)
     );
     _evalBundle(controller!, animationController);
   }
 
-  loadURL(String bundleURL) async {
+  @deprecated
+  loadURL(String bundleURL, { String? bundleContent, Uint8List? bundleByteCode }) async {
     await controller!.unload();
+
+    KrakenBundle bundle;
+    if (bundleByteCode != null) {
+      bundle = KrakenBundle.fromBytecode(bundleByteCode, url: bundleURL);
+    } else if (bundleContent != null) {
+      bundle = KrakenBundle.fromContent(bundleContent, url: bundleURL);
+    } else {
+      bundle = KrakenBundle.fromUrl(bundleURL);
+    }
+
     await controller!.loadBundle(
-        bundleURL: bundleURL
+        bundle: bundle
     );
     _evalBundle(controller!, animationController);
   }
 
-  loadPath(String bundlePath) async {
+  @deprecated
+  loadPath(String bundlePath, { String? bundleContent, Uint8List? bundleByteCode }) async {
     await controller!.unload();
+
+    KrakenBundle bundle;
+    if (bundleByteCode != null) {
+      bundle = KrakenBundle.fromBytecode(bundleByteCode, url: bundlePath);
+    } else if (bundleContent != null) {
+      bundle = KrakenBundle.fromContent(bundleContent, url: bundlePath);
+    } else {
+      bundle = KrakenBundle.fromUrl(bundlePath);
+    }
+
     await controller!.loadBundle(
-        bundlePath: bundlePath
+        bundle: bundle
     );
     _evalBundle(controller!, animationController);
   }
@@ -168,10 +194,7 @@ class Kraken extends StatefulWidget {
     Key? key,
     this.viewportWidth,
     this.viewportHeight,
-    this.bundleURL,
-    this.bundlePath,
-    this.bundleContent,
-    this.bundleByteCode,
+    this.bundle,
     this.onLoad,
     this.navigationDelegate,
     this.javaScriptChannel,
@@ -793,9 +816,7 @@ This situation often happened when you trying creating kraken when FlutterView n
       viewportHeight,
       background: _krakenWidget.background,
       showPerformanceOverlay: Platform.environment[ENABLE_PERFORMANCE_OVERLAY] != null,
-      bundleContent: _krakenWidget.bundleContent,
-      bundleURL: _krakenWidget.bundleURL,
-      bundlePath: _krakenWidget.bundlePath,
+      bundle: _krakenWidget.bundle,
       onLoad: _krakenWidget.onLoad,
       onLoadError: _krakenWidget.onLoadError,
       onJSError: _krakenWidget.onJSError,
@@ -805,7 +826,7 @@ This situation often happened when you trying creating kraken when FlutterView n
       devToolsService: _krakenWidget.devToolsService,
       httpClientInterceptor: _krakenWidget.httpClientInterceptor,
       widgetDelegate: _widgetDelegate,
-      uriParser: _krakenWidget.uriParser,
+      uriParser: _krakenWidget.uriParser
     );
 
     if (kProfileMode) {
@@ -875,7 +896,7 @@ class _KrakenRenderObjectElement extends SingleChildRenderObjectElement {
     // So we bind _KrakenRenderObjectElement into KrakenController, and widgetElements created by controller can follow this to the root.
     controller.rootKrakenElement = this;
 
-    if (controller.bundleContent == null && controller.bundlePath == null && controller.bundleURL == null) {
+    if (controller.bundle == null || (controller.bundle?.content == null && controller.bundle?.bytecode == null && controller.bundle?.src == null)) {
       return;
     }
 
@@ -909,4 +930,3 @@ void _evalBundle(KrakenController controller, AnimationController? animationCont
     await controller.evalBundle();
   }
 }
-
