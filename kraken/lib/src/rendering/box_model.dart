83a7d7ebc6763299e8e2a179e8a3ce276fbbf0a1
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2021-12-09 19:25:51 +0800

    fix: paint order of fixed element

--- kraken/lib/src/rendering/box_model.dart
+++ kraken/lib/src/rendering/box_model.dart
@@ -226,9 +226,8 @@ class RenderLayoutBox extends RenderBoxModel
           // this logic after Kraken has implemented stacking context tree.
           if(left is RenderBoxModel && left.renderStyle.position == CSSPositionType.fixed &&
             right is RenderBoxModel && right.renderStyle.position == CSSPositionType.fixed) {
-            // Child element who has higher depth always paint after parent element who has lower depth
-            // in the renderObject tree.
-            return left.renderPositionPlaceholder!.depth - right.renderPositionPlaceholder!.depth;
+            // Child element always paint after parent element in the renderObject tree.
+            return right.renderStyle.isAncestorForChild(left.renderStyle) ? 1 : -1;
           }
 
           bool isLeftNeedsStacking = left is RenderBoxModel && left.needsStacking;
