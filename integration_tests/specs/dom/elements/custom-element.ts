382b129d47d6015447140407d20fa80869f41612
answershuto <answershuto@gmail.com>
2021-12-15 18:10:25 +0800

    :white_check_mark: test: add image.

--- integration_tests/specs/dom/elements/custom-element.ts
+++ integration_tests/specs/dom/elements/custom-element.ts
@@ -39,90 +39,24 @@ describe('custom widget element', () => {
 
     simulateClick(20, 20);
   });
-});
 
-describe('custom html element', () => {
-  it('works with document.createElement', async () => {
-    let sampleElement = document.createElement('sample-element');
-    let text = document.createTextNode('helloworld');
-    sampleElement.appendChild(text);
-    document.body.appendChild(sampleElement);
-    await snapshot();
-  });
+  it('flutter widget and dom node should be child of flutter container', () => {
+    const container = document.createElement('flutter-container');
+    document.body.appendChild(container);
 
-  it('support custom properties in dart directly', () => {
-    let sampleElement = document.createElement('sample-element');
-    let text = document.createTextNode('helloworld');
-    sampleElement.appendChild(text);
-    document.body.appendChild(sampleElement);
+    const element = document.createElement('div');
+    element.style.backgroundColor = 'red';
+    element.appendChild(document.createTextNode('div element'));
+    container.appendChild(element);
 
-    // @ts-ignore
-    expect(sampleElement.ping).toBe('pong');
-  });
+    const fluttetText = document.createElement('flutter-text');
+    fluttetText.setAttribute('value', 'text');
+    container.appendChild(fluttetText);
 
-  it('support call js function but defined in dart directly', () => {
-    let sampleElement = document.createElement('sample-element');
-    let text = document.createTextNode('helloworld');
-    sampleElement.appendChild(text);
-    document.body.appendChild(sampleElement);
+    const text = document.createTextNode('text');
+    container.appendChild(text);
 
-    let arrs = [1, 2, 4, 8, 16];
-    // @ts-ignore
-    expect(sampleElement.fn.apply(sampleElement, arrs)).toEqual([2, 4, 8, 16, 32]);
-  });
-
-  it('return promise when dart return future async function', async () => {
-    let sampleElement = document.createElement('sample-element');
-    let text = document.createTextNode('helloworld');
-    sampleElement.appendChild(text);
-    document.body.appendChild(sampleElement);
-    // @ts-ignore
-    let p = sampleElement.asyncFn(1);
-    expect(p instanceof Promise);
-    let result = await p;
-    expect(result).toBe(1);
-    // @ts-ignore
-    let p2 = sampleElement.asyncFn('abc');
-    expect(await p2).toBe('abc');
-
-    // @ts-ignore
-    let p3 = sampleElement.asyncFn([1, 2, 3, 4]);
-    expect(await p3).toEqual([1, 2, 3, 4]);
-
-    // @ts-ignore
-    let p4 = sampleElement.asyncFn([{ name: 1 }]);
-    expect(await p4).toEqual([{ name: 1 }]);
-  });
-
-  it('return promise error when dart async function throw error', async () => {
-    let sampleElement = document.createElement('sample-element');
-    let text = document.createTextNode('helloworld');
-    sampleElement.appendChild(text);
-    document.body.appendChild(sampleElement);
-    // @ts-ignore
-    let p = sampleElement.asyncFnFailed();
-    expect(p instanceof Promise);
-    try {
-      let result = await p;
-      throw new Error('should throw');
-    } catch (e) {
-      expect(e.message).toBe('Assertion failed: "Asset error"');
-    }
-  });
-
-  it('property with underscore have no effect', () => {
-    let sampleElement = document.createElement('sample-element');
-    let text = document.createTextNode('helloworld');
-    sampleElement.appendChild(text);
-    document.body.appendChild(sampleElement);
-
-    // @ts-ignore
-    expect(sampleElement._fake).toBe(undefined);
-
-    // @ts-ignore
-    sampleElement._fake = [1, 2, 3, 4, 5];
-    // @ts-ignore
-    expect(sampleElement._fake).toEqual([1, 2, 3, 4, 5]);
+    snapshot();
   });
 
   it('text node should be child of flutter container', () => {
@@ -154,25 +88,6 @@ describe('custom html element', () => {
     snapshot();
   });
 
-  it('flutter widget and dom node should be child of flutter container', () => {
-    const container = document.createElement('flutter-container');
-    document.body.appendChild(container);
-
-    const element = document.createElement('div');
-    element.style.backgroundColor = 'red';
-    element.appendChild(document.createTextNode('div element'));
-    container.appendChild(element);
-
-    const fluttetText = document.createElement('flutter-text');
-    fluttetText.setAttribute('value', 'text');
-    container.appendChild(fluttetText);
-
-    const text = document.createTextNode('text');
-    container.appendChild(text);
-
-    snapshot();
-  });
-
   it('flutter widget should be child of element', () => {
     const container = document.createElement('div');
     container.style.width = '100px';
@@ -199,4 +114,88 @@ describe('custom html element', () => {
 
     snapshot();
   });
-});
+
+  describe('custom html element', () => {
+    it('works with document.createElement', async () => {
+      let sampleElement = document.createElement('sample-element');
+      let text = document.createTextNode('helloworld');
+      sampleElement.appendChild(text);
+      document.body.appendChild(sampleElement);
+      await snapshot();
+    });
+
+    it('support custom properties in dart directly', () => {
+      let sampleElement = document.createElement('sample-element');
+      let text = document.createTextNode('helloworld');
+      sampleElement.appendChild(text);
+      document.body.appendChild(sampleElement);
+
+      // @ts-ignore
+      expect(sampleElement.ping).toBe('pong');
+    });
+
+    it('support call js function but defined in dart directly', () => {
+      let sampleElement = document.createElement('sample-element');
+      let text = document.createTextNode('helloworld');
+      sampleElement.appendChild(text);
+      document.body.appendChild(sampleElement);
+
+      let arrs = [1, 2, 4, 8, 16];
+      // @ts-ignore
+      expect(sampleElement.fn.apply(sampleElement, arrs)).toEqual([2, 4, 8, 16, 32]);
+    });
+
+    it('return promise when dart return future async function', async () => {
+      let sampleElement = document.createElement('sample-element');
+      let text = document.createTextNode('helloworld');
+      sampleElement.appendChild(text);
+      document.body.appendChild(sampleElement);
+      // @ts-ignore
+      let p = sampleElement.asyncFn(1);
+      expect(p instanceof Promise);
+      let result = await p;
+      expect(result).toBe(1);
+      // @ts-ignore
+      let p2 = sampleElement.asyncFn('abc');
+      expect(await p2).toBe('abc');
+
+      // @ts-ignore
+      let p3 = sampleElement.asyncFn([1, 2, 3, 4]);
+      expect(await p3).toEqual([1, 2, 3, 4]);
+
+      // @ts-ignore
+      let p4 = sampleElement.asyncFn([{ name: 1 }]);
+      expect(await p4).toEqual([{ name: 1 }]);
+    });
+
+    it('return promise error when dart async function throw error', async () => {
+      let sampleElement = document.createElement('sample-element');
+      let text = document.createTextNode('helloworld');
+      sampleElement.appendChild(text);
+      document.body.appendChild(sampleElement);
+      // @ts-ignore
+      let p = sampleElement.asyncFnFailed();
+      expect(p instanceof Promise);
+      try {
+        let result = await p;
+        throw new Error('should throw');
+      } catch (e) {
+        expect(e.message).toBe('Assertion failed: "Asset error"');
+      }
+    });
+
+    it('property with underscore have no effect', () => {
+      let sampleElement = document.createElement('sample-element');
+      let text = document.createTextNode('helloworld');
+      sampleElement.appendChild(text);
+      document.body.appendChild(sampleElement);
+
+      // @ts-ignore
+      expect(sampleElement._fake).toBe(undefined);
+
+      // @ts-ignore
+      sampleElement._fake = [1, 2, 3, 4, 5];
+      // @ts-ignore
+      expect(sampleElement._fake).toEqual([1, 2, 3, 4, 5]);
+    });
+  });
