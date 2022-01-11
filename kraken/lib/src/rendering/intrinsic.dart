fff3044b8affb177f87dcf14c31e3bbd61d5e089
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2022-01-11 12:19:35 +0800

    fix: remove clip for renderIntrinsic

--- kraken/lib/src/rendering/intrinsic.dart
+++ kraken/lib/src/rendering/intrinsic.dart
@@ -26,13 +26,6 @@ class RenderIntrinsic extends RenderBoxModel
     return heightDefined ? BoxSizeType.specified : BoxSizeType.intrinsic;
   }
 
-  // Set clipX and clipY to true for background cannot overflow beyond the boundary of replaced element
-  @override
-  bool get clipX => true;
-
-  @override
-  bool get clipY => true;
-
   @override
   void setupParentData(RenderBox child) {
     if (child.parentData is! RenderLayoutParentData) {
