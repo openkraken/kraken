9fa7ac664332aa7a0e242f9cdb610d07939babc2
answershuto <answershuto@gmail.com>
2022-01-19 12:35:55 +0800

    :sparkles: feat: add HTMLView.

--- kraken/lib/src/widget/html.dart
+++ kraken/lib/src/widget/html.dart
@@ -2,5 +2,21 @@
  * Copyright (C) 2022-present Alibaba Inc. All rights reserved.
  * Author: Kraken Team.
  */
+import 'package:flutter/material.dart';
+import 'package:kraken/kraken.dart';
+import 'package:kraken/widget.dart';
 
+class HTMLView extends StatelessWidget {
+  final String data;
 
+  HTMLView(
+    this.data,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Kraken(
+      bundle: KrakenBundle.fromContent(data),
+    );
+  }
+}
