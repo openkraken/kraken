2da945bc5d810fa0e4b248a8f7f57a73676c58d9
answershuto <answershuto@gmail.com>
2022-01-21 19:27:23 +0800

    :pencil2: fix: HTMLView need not build because it is StatefulWidiget.

--- kraken/lib/src/widget/html.dart
+++ kraken/lib/src/widget/html.dart
@@ -6,8 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:kraken/kraken.dart';
 import 'package:kraken/widget.dart';
 
-
-
 class HTMLView extends StatefulWidget {
   final String data;
 
@@ -17,13 +15,4 @@ class HTMLView extends StatefulWidget {
 
   @override
   HTMLViewState<HTMLView> createState() => HTMLViewState<HTMLView>();
-
-  Widget build(BuildContext context) {
-    // dom.Document document = parse(data);
-
-    return Kraken(
-      bundle: KrakenBundle.fromHTML('<div>aaa</div>'),
-      disabledJavaScript: true,
-    );
-  }
 }
