e4065100710bdcd962a7ad895c0d83daf7c1d76f
answershuto <answershuto@gmail.com>
2022-01-24 19:30:59 +0800

    :truck: chore: rename html to html_view.

--- kraken/lib/widget.dart
+++ kraken/lib/widget.dart
@@ -6,4 +6,4 @@
 export 'src/widget/kraken.dart';
 export 'src/widget/widget_to_element_adaptor.dart';
 export 'src/widget/element_to_widget_adaptor.dart';
-export 'src/widget/html.dart';
+export 'src/widget/html_view.dart';
