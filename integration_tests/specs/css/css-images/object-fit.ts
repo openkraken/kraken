fed03ff5aa65365b012898b0c093514bbc6cd216
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2022-02-17 17:42:55 +0800

    test: add object-fit none and scale-down tests

--- integration_tests/specs/css/css-images/object-fit.ts
+++ integration_tests/specs/css/css-images/object-fit.ts
@@ -199,4 +199,65 @@ describe('object-fit', () => {
     await snapshot(0.1);
   });
 
+
+  it('should works with none', async () => {
+    let image;
+    image = createElement(
+      'img',
+      {
+        src: 'assets/ruler-v-100px.png',
+        style: {
+          display: 'block',
+          'object-fit': 'none',
+          width: '40px',
+          height: '100px',
+          backgroundColor: 'yellow'
+        },
+      },
+    );
+    BODY.appendChild(image);
+ 
+    await snapshot(0.1);
+  });
+
+  it('should works with scale-down when it behaves as none', async () => {
+    let image;
+    image = createElement(
+      'img',
+      {
+        src: 'assets/ruler-v-100px.png',
+        style: {
+          display: 'block',
+          'object-fit': 'scale-down',
+          width: '100px',
+          height: '250px',
+          backgroundColor: 'yellow'
+        },
+      },
+    );
+    BODY.appendChild(image);
+ 
+    await snapshot(0.1);
+  });
+
+  it('should works with scale-down when it behaves as contain', async () => {
+    let image;
+    image = createElement(
+      'img',
+      {
+        src: 'assets/ruler-v-100px.png',
+        style: {
+          display: 'block',
+          'object-fit': 'scale-down',
+          width: '40px',
+          height: '100px',
+          backgroundColor: 'yellow'
+        },
+      },
+    );
+    BODY.appendChild(image);
+ 
+    await snapshot(0.1);
+  });
+
 });
