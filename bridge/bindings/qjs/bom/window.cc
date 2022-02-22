1eb0830a3a36c84cd070d982ad5b0ca24fa085ea
answershuto <answershuto@gmail.com>
2022-02-22 17:57:08 +0800

    :bug: fix: scrollBy should flushUICommand.

--- bridge/bindings/qjs/bom/window.cc
+++ bridge/bindings/qjs/bom/window.cc
@@ -59,6 +59,7 @@ JSValue Window::scrollTo(JSContext* ctx, JSValue this_val, int argc, JSValue* ar
 #endif
 }
 JSValue Window::scrollBy(JSContext* ctx, JSValue this_val, int argc, JSValue* argv) {
+  getDartMethod()->flushUICommand();
   auto window = static_cast<WindowInstance*>(JS_GetOpaque(this_val, Window::classId()));
   NativeValue arguments[] = {jsValueToNativeValue(ctx, argv[0]), jsValueToNativeValue(ctx, argv[1])};
   return window->callNativeMethods("scrollBy", 2, arguments);
