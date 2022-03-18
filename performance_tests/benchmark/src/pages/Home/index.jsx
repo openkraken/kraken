21b6bd89e41192803b51c09bec0343bccc529cc8
answershuto <answershuto@gmail.com>
2022-03-18 16:49:23 +0800

    :sparkles: feat: add kraken performance benchmark.

---
+++ performance_tests/benchmark/src/pages/Home/index.jsx
@@ -0,0 +1,51 @@
+import { createElement, useState, useEffect } from 'rax';
+import View from 'rax-view';
+import Text from 'rax-text';
+import originDatas from '../../mock.js'
+
+import styles from './index.module.css';
+import Price from '../../components/Price';
+import Card from '../../components/Card';
+
+export default function Home() {
+  const calcRatio = (item) => {
+    const h = item.imageInfos[0].heightSize;
+    const w = item.imageInfos[0].widthSize;
+
+    return Math.min(4 / 3, Math.max(h / w, 3 / 4));
+  };
+
+  const renderElement = (dataList) => {
+    return dataList.map((item, index) => {
+      const props = {
+        key: index,
+        title: item.title,
+        bottom: <Price price={item.price} rightText={`${item.wantNum}人想要`} />,
+        onClick: () => {},
+      };
+      const ratio = calcRatio(item);
+      props.image = {
+        src: item.imageInfos[0].url,
+        ratio,
+      };
+
+      return (<div key={index} style={{ display: 'inline-block', padding: '2px' }}>
+        <Card key={index} {...props} />
+      </div>);
+    });
+  };
+
+  return (
+    <div>
+      <View className={styles.homeContainer}>
+        <View
+          style={{ height: '100vh', display: 'block' }}
+        >
+          {
+            renderElement(originDatas)
+          }
+        </View>
+      </View>
+    </div>
+  );
+}
