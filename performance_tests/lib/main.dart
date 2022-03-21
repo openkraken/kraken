f4cc548b70733695ff78d2ed388d388c13c53b6b
answershuto <answershuto@gmail.com>
2022-03-21 15:52:43 +0800

    :sparkles: feat: add assets path and modify bundle of main.

--- performance_tests/lib/main.dart
+++ performance_tests/lib/main.dart
@@ -90,7 +90,7 @@ class _MyHomePageState extends State<MyBrowser> {
           child: _kraken = Kraken(
             viewportWidth: viewportSize.width - queryData.padding.horizontal,
             viewportHeight: viewportSize.height - appBar.preferredSize.height - queryData.padding.vertical,
-            bundle: KrakenBundle.fromUrl('https://kraken.oss-cn-hangzhou.aliyuncs.com/data/cvd3r6f068.js'),
+            bundle: KrakenBundle.fromUrl('assets://benchmark/build/kraken/home.kbc1'),
             onLoad: (KrakenController controller) {
               Timer(Duration(seconds: 4), () {
                 exit(0);
