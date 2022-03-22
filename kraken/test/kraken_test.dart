772f9715d1b680dc57f84d38d1df7fe2a3cff6ea
ZeroLing <zhuoling.lcl@alibaba-inc.com>
2022-03-22 16:16:25 +0800

    test: for test

--- kraken/test/kraken_test.dart
+++ kraken/test/kraken_test.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:kraken/bridge.dart';
 import 'package:kraken/foundation.dart';
 
 import 'local_http_server.dart';
@@ -34,9 +33,6 @@ void main() {
   LocalHttpServer.basePath = 'test/fixtures';
   var httpServer = LocalHttpServer.getInstance();
   print('Local HTTP Server started at ${httpServer.getUri()}');
-
-  // Work with bridge path.
-  KrakenDynamicLibrary.dynamicLibraryPath = '${Directory.current.path}/macos';
 
   // Work around with path_provider.
   Directory tempDirectory = Directory('./temp');
