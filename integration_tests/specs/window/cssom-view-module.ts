2c5335e9e9febfa9c5394f970240fc24cff94bbe
ZeroLing <zhuoling.lcl@alibaba-inc.com>
2022-03-29 12:24:21 +0800

    test: add dpr test case

--- integration_tests/specs/window/cssom-view-module.ts
+++ integration_tests/specs/window/cssom-view-module.ts
@@ -1,5 +1,9 @@
 // https://drafts.csswg.org/cssom-view
 describe('CSSOM View Module', () => {
+  it('devicePixelRatio', () => {
+    expect(window.devicePixelRatio >= 1).toEqual(true);
+  });
+
   it('innerWidth', () => {
     expect(window.innerWidth > 0).toEqual(true);
   });
