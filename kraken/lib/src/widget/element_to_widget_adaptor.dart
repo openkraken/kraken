1d96587cf9112bbd64d86e7ae62f6731eab7ae4d
answershuto <answershuto@gmail.com>
2022-05-12 15:00:03 +0800

    :art: chore: add comment.

--- kraken/lib/src/widget/element_to_widget_adaptor.dart
+++ kraken/lib/src/widget/element_to_widget_adaptor.dart
@@ -21,6 +21,8 @@ class KrakenElementToWidgetAdaptor extends RenderObjectWidget {
 
   @override
   RenderObject createRenderObject(BuildContext context) {
+    // Children of custom element need RenderFlowLayout nesting,
+    // otherwise the parent render layout will not be called when setting properties.
     if (_krakenNode is dom.Element) {
       RenderFlowLayout renderFlowLayout = RenderFlowLayout(renderStyle: CSSRenderStyle(target: _krakenNode as dom.Element));
       renderFlowLayout.insert(_krakenNode.renderer!);
