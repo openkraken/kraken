fe9cb80ab038d42781a5654ee6fa07e663e12f87
andycall <dongtiangche@outlook.com>
2022-07-05 23:30:00 +0800

    test: add snapshots

--- integration_tests/specs/dom/elements/link.ts
+++ integration_tests/specs/dom/elements/link.ts
@@ -2,13 +2,14 @@ describe('Link Element', () => {
   it('should work with remote css', async (done) => {
     let link = document.createElement('link');
     link.setAttribute('href', 'https://andycall.oss-cn-beijing.aliyuncs.com/css/a-green.css');
-    link.setAttribute('ref', 'stylesheet');
-    document.head.appendChild(link);
+    link.setAttribute('rel', 'stylesheet');
 
     link.addEventListener('load', async () => {
       await snapshot();
       done();
     });
+
+    document.head.appendChild(link);
 
     let div = document.createElement('div');
     div.className = 'a';
